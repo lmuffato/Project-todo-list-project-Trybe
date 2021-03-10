800aaf56316b3f376f2171c18ed2bd5f7f039bdf
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-10 19:09:14 -0300

    1 a 5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Cria Botao
-let botao = document.createElement('button');
+const botao = document.createElement('button');
+botao.id = 'criar-tarefa'
 botao.innerHTML = 'Criar Tarefa';
 document.getElementById('espacoDoBotao').appendChild(botao);
 botao.addEventListener('click', clickDoBotao);
@@ -7,7 +8,18 @@ botao.addEventListener('click', clickDoBotao);
 function clickDoBotao() {
     let pegaTexto = document.getElementById('texto-tarefa').value;
     let criaLista = document.createElement('li');
+    criaLista.className = 'listasCriadas'
     criaLista.innerText = pegaTexto;
     document.getElementById('lista-tarefas').appendChild(criaLista);
     document.getElementById('texto-tarefa').value = '';
+    criaLista.addEventListener('click', mudaCorDaLista);
 }
+
+function mudaCorDaLista() {
+    for (let index = 0; index < document.getElementsByClassName('listasCriadas').length; index += 1) {
+        document.getElementsByClassName('listasCriadas')[index].addEventListener('click', () => {
+            document.getElementsByClassName('listasCriadas')[index].style.backgroundColor = 'gray';
+        } );
+    }
+}
+mudaCorDaLista();
