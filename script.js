f7d29ae8e8b4ce404b6b74fec1f24f28886b90eb
Giovanni <giovannitadeu08@gmail.com>
2021-03-10 17:43:47 -0300

    Requisitos 8 e 9 - Feitos

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const inputItens = document.querySelector('#criar-tarefa'); // Variaveis Gerais.
+const inputItems = document.querySelector('#criar-tarefa'); // Variaveis Gerais.
 const listOfTasks = document.getElementById('lista-tarefas');
+const deleteItems = document.getElementById('apaga-tudo');
 
 // Funções Gerais e Eventos.
-inputItens.addEventListener('click', function () { // Cria os items na lista ol.
+inputItems.addEventListener('click', function () { // Cria as tarefas digitadas colocando na lista ol.
   const taskInput = document.getElementById('texto-tarefa').value;
   if (taskInput !== '') {
     const newLine = document.createElement('li');
@@ -14,7 +15,7 @@ inputItens.addEventListener('click', function () { // Cria os items na lista ol.
   }
 });
 
-listOfTasks.addEventListener('click', function (event) { // Ao selecionar algum item da li marca em cinza
+listOfTasks.addEventListener('click', function (event) { // Ao selecionar algum item da li o marca em cinza
   const listItem = document.getElementsByTagName('li');
   event.target.style.backgroundColor = 'rgb(128,128,128)';
   event.target.id = 'isSelected';
@@ -23,5 +24,20 @@ listOfTasks.addEventListener('click', function (event) { // Ao selecionar algum 
       listItem[i].style.backgroundColor = 'transparent';
       listItem[i].id = 'none';
     }
+  }
+});
+
+listOfTasks.addEventListener('dblclick', function (event) { // Ao dar doubleclick em um item a função "risca", selecionando o mesmo.
+  if (event.target.className !== 'completed') {
+    event.target.className = 'completed';
+  } else {
+    event.target.className = '';
+  }
+});
+
+deleteItems.addEventListener('click', function () {
+  const lines = document.getElementsByTagName('li');
+  for (let i = 0; i < lines.length; i += 0) {
+    lines[i].remove();
   }
 });
