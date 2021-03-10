21b0afcec4fd11ef81bc2b34c434f6bb4acb4f81
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-03-10 17:11:03 -0300

    Requisito 8 concluído é removido o background do item após clcar no seguinte

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ const $taskInput = document.getElementById('texto-tarefa');
 const $taskList = document.getElementById('lista-tarefas');
 
 function addTask() {
-  const $newTaskItem = document.createElement('li');
-  $taskInput.classList.add('tasks__item');
-  $newTaskItem.textContent = $taskInput.value;
-  $taskList.appendChild($newTaskItem);
-  $taskInput.value = '';
+  if ($taskInput.value !== '') {
+    const $newTaskItem = document.createElement('li');
+    $newTaskItem.classList.add('tasks__item');
+    $newTaskItem.textContent = $taskInput.value;
+    $taskList.appendChild($newTaskItem);
+    $taskInput.value = '';
+  }
 }
 
 $addTaskBtn.addEventListener('click', addTask);
 
 function changeColorItemList(event) {
   const $target = event.target;
+  const $tasksItens = document.querySelectorAll('.tasks__item');
+  $tasksItens.forEach((itemList) => {
+    const element = itemList;
+    element.style.cssText = 'background-color: rgb(255, 255, 255);';
+  });
   $target.style.cssText = 'background-color: rgb(128, 128, 128); ';
 }
 
