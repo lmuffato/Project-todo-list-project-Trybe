64772e11d4ebae776e4600700c14c597b71f3a99
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-03-10 15:56:19 -0300

    Requisito 7 concluído - itens da lista mudam de cor quando clicados

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const $taskInput = document.getElementById('texto-tarefa');
 const $taskList = document.getElementById('lista-tarefas');
 
 function addTask() {
-  const $taskItem = document.createElement('li');
-  $taskItem.textContent = $taskInput.value;
-  $taskList.appendChild($taskItem);
+  const $newTaskItem = document.createElement('li');
+  $taskInput.classList.add('tasks__item');
+  $newTaskItem.textContent = $taskInput.value;
+  $taskList.appendChild($newTaskItem);
   $taskInput.value = '';
 }
 
 $addTaskBtn.addEventListener('click', addTask);
+
+function changeColorItemList(event) {
+  const $target = event.target;
+  $target.style.cssText = 'background-color: rgb(128, 128, 128); ';
+}
+
+$taskList.addEventListener('click', changeColorItemList);
