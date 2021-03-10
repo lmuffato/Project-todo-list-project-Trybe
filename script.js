f28119d7602e6f26380944cc9032d9075c61c517
Bruno Mendes <mendessbruno@gmail.com>
2021-03-10 18:26:10 -0300

    resolvido exercicio 7

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function createLi(text) {
   const listItem = document.createElement('li');
   listItem.innerText = text;
+  listItem.className = 'task';
   document.getElementById('lista-tarefas').appendChild(listItem);
+}
+
+function changeColor() {
+  const list = document.getElementsByClassName('task');
+  for (let index = 0; index < list.length; index += 1) {
+    const element = list[index];
+    element.addEventListener('click', () => {
+      element.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
 }
 
 function addItem() {
@@ -11,8 +22,10 @@ function addItem() {
     const text = textBox.value;
     createLi(text);
     textBox.value = '';
+    changeColor(); // declarado aqui para toda vez que um item for adicionado rodar a função de mudar de cor e atualizar o array;
   });
 }
+
 window.onload = function run() {
   addItem();
 };
