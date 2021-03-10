9dbc6e61b9de85919d3e643dbc1028c8c7984ad0
Edrei <edreimateus@gmail.com>
2021-03-10 16:45:00 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function createTask() {
 
 const task = document.querySelector('#criar-tarefa');
 task.addEventListener('click', createTask);
+
+function addSelect(evt) {
+  const select = document.querySelector('.selected');
+  if (select) {
+    select.classList.remove('selected');
+    evt.target.classList.add('selected');
+  } else {
+    evt.target.classList.add('selected');
+  }
+}
+const addBackground = document.getElementById('lista-tarefas');
+addBackground.addEventListener('click', addSelect);
+
+// EXERCICIO RESOLVIDO BASEADO EM EXERCICIO COURSE
