7172e50a9318be4ea7815751a897af82c88f0de9
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-10 17:31:11 -0300

    :rocket: feat(Requirement - 7): Changes the background color of clicked list element

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const btnAddTask = document.getElementById('tarefa');
 const inputTasks = document.getElementById('texto-tarefa');
+const listOfTasks = document.getElementById('lista-tarefas');
+const listItems = document.getElementsByClassName('item-lista');
 
 function clearInput() {
   inputTasks.value = '';
 }
 
-const listOfTasks = document.getElementById('lista-tarefas');
-
-function addsTaskToList() {
+function createListItem() {
   const listItem = document.createElement('li');
   listItem.classList.add('item-lista');
+  return listItem;
+}
+
+function changeBgColor(event) {
+  event.target.style.setProperty('background-color', 'rgb(128, 128, 128)');
+}
+
+function updateListListeners() {
+  for (let index = 0; index < listItems.length; index += 1) {
+    listItems[index].addEventListener('click', changeBgColor);
+  }
+}
+
+function addInputValuesToListItem(listItem) {
+  const item = listItem;
+  item.innerText = inputTasks.value;
+}
+
+function addsTaskToList() {
+  const listItem = createListItem();
+  addInputValuesToListItem(listItem);
   listOfTasks.appendChild(listItem);
-  listItem.innerText = inputTasks.value;
-  console.log('teste');
+  updateListListeners();
   clearInput();
 }
 
