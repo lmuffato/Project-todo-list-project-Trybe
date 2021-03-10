5b5ab6f79e098501bc2604aa4ac963c3fa64d70e
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-10 17:33:25 -0300

    [REQUISITO][09][FIX][REFATORAÇÃO]

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-// EVENTOS EM VÁRIOS ELEMENTOS
-const eventClickList = () => {
-  document.querySelectorAll('li').forEach((element) => {
-    element.addEventListener('click', (elementTarget) => {
-      const listElement = elementTarget.target;
-      const listElements = document.getElementsByTagName('li');
-
-      for (let index = 0; index < listElements.length; index += 1) {
-        if (listElements[index].id === 'selected-task') {
-          listElements[index].id = '';
-        }
-      }
-      listElement.id = 'selected-task';
-    });
-  });
-};
-
-const eventDoubleClickList = () => {
-  document.querySelectorAll('li').forEach((element) => {
-    element.addEventListener('dblclick', (elementTarget) => {
-      const listElement = elementTarget.target;
-      listElement.className = 'completed';
-    });
-  });
-};
-
-// BOTÃO DE CRIAR
 const btnCreateTask = document.getElementById('criar-tarefa');
 const oListTask = document.getElementById('lista-tarefas');
+
 btnCreateTask.addEventListener('click', () => {
   const listTaskElement = document.createElement('li');
   const inputTextTask = document.getElementById('texto-tarefa');
   listTaskElement.innerText = inputTextTask.value;
   oListTask.appendChild(listTaskElement);
-  eventClickList();
-  eventDoubleClickList();
   inputTextTask.value = '';
 });
+
+oListTask.addEventListener('click', (e) => {
+  const listElement = e.target;
+  const listElements = document.getElementsByTagName('li');
+
+  for (let index = 0; index < listElements.length; index += 1) {
+    if (listElements[index].id === 'selected-task') listElements[index].id = '';
+  }
+
+  listElement.id = 'selected-task';
+});
+
+oListTask.addEventListener('dblclick', (e) => {
+  if (e.target.classList.contains('completed')) e.target.classList.remove('completed');
+  else e.target.classList.add('completed');
+});
+
+/*
+REFERÊNCIAS:
+https://www.w3schools.com/jsref/prop_element_classlist.asp
+*/
