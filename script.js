926860846a71b942cef35715388a2cabdb85a821
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-10 17:21:47 -0300

    Resoluçao do requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// window.onload = ?;
+// window.onload = () => {
+
+// };
 
 const btnAdicionarTarefa = document.getElementById('criar-tarefa');
 const inputText = document.getElementById('texto-tarefa');
-const listOrdenada = document.getElementById('lista-tarefas')
-// const listOrdenadas = document.querySelectorAll('#lista-tarefas li');
-// console.log(listOrdenadas);
+const listOrdenada = document.getElementById('lista-tarefas');
 
 // Função responsavel para adicionar um elemento na lista;
 function addListInput() {
@@ -12,12 +12,22 @@ function addListInput() {
     const list = document.createElement('li');  
     if (inputText.value !== '') {
       list.innerHTML = inputText.value;
+      list.className = 'list';
       listOrdenada.appendChild(list);
+      selectColor(list);
     }      
     clearInput();
   });
 }
 addListInput();
+
+// Função para adicionar um dblclik na lista rgb(128,128,128);
+function selectColor(list) {
+  list.addEventListener('click', () => {
+    list.style.backgroundColor = 'rgb(128,128,128)'
+    console.log('Fui clicado');
+  });
+}
 
 // Função responsavel para limpar o input;
 function clearInput() {
