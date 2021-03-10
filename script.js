78eeaef85848b90bc9edcf9ce56cb6f15dd1b9b1
Felipe Muller <felipeavmuller@gmail.com>
2021-03-10 18:44:30 -0300

    realização do requisito 7

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function addTask() {
 }
 
 addTask();
+
+function selectTask() {
+  let taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', function(event) {
+    event.target.className = 'selected';
+    let allLines = document.querySelectorAll('li');
+    for (let index = 0; index < allLines.length; index += 1) {
+      if (allLines[index] !== event.target) {
+        allLines[index].classList.remove('selected');
+      }
+    }
+  })
+}
+selectTask();
+
+function colorTask() {
+  let taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', function() {
+    let allLines = document.querySelectorAll('li');
+    for (let index = 0; index < allLines.length; index += 1) {
+      if (allLines[index].className === 'selected') {
+        allLines[index].style.backgroundColor = 'rgb(128,128,128)';
+      } else {
+        allLines[index].style.backgroundColor = 'white';
+      }
+    }
+  })
+}
+colorTask();
