9308ecfb6281d47a4a9f8e34c48b81b13edc3edb
Vinicios Fraga <soicinivt@gmail.com>
2021-03-10 17:41:40 -0300

    Requisitos 5 e 6.

---
+++ script.js
@@ -0,0 +1,9 @@
+const maker = document.querySelector('#criar-tarefa');
+maker.addEventListener('click', function newTask() {
+  const task = document.querySelector('#texto-tarefa');
+  const theTask = document.createElement('li');
+  const theList = document.querySelector('#lista-tarefas');
+  theTask.innerHTML = task.value;
+  theList.appendChild(theTask);
+  task.value = '';
+});
