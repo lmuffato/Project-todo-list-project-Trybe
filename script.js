10ec0062bbff8d0379b6badd69927bc904f5a650
Viegas <mvrdgs@gmail.com>
2021-03-10 15:29:27 -0300

    feito requisito 5 e 6 veio  de lambuja

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-window.onload = () => {};
+function getNewTask() {
+  const taskText = document.querySelector('#texto-tarefa');
+  const text = taskText.value;
+  taskText.value = '';
+  return text;
+}
+
+function addTask() {
+  const taskListContainer = document.querySelector('#lista-tarefas');
+  const text = getNewTask();
+
+  let newTask = document.createElement('li');
+  newTask.innerText = text;
+  taskListContainer.appendChild(newTask);
+}
+
+function createTask() {
+  const createTaskButton = document.querySelector('#criar-tarefa');
+
+  createTaskButton.addEventListener('click', addTask)
+}
+
+window.onload = () => {
+  createTask();
+};
