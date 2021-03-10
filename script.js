fd4611df29c7656523329fa7e5170302885d9a11
Viegas <mvrdgs@gmail.com>
2021-03-10 15:38:12 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function changeTaskBgColor(event) {
+  const selectedTask = event.target;
+  selectedTask.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function selectTask() {
+  const taskListContainer = document.querySelector('#lista-tarefas');
+
+  taskListContainer.addEventListener('click', changeTaskBgColor);
+}
+
 function getNewTask() {
   const taskText = document.querySelector('#texto-tarefa');
   const text = taskText.value;
@@ -9,17 +20,19 @@ function addTask() {
   const taskListContainer = document.querySelector('#lista-tarefas');
   const text = getNewTask();
 
-  let newTask = document.createElement('li');
+  const newTask = document.createElement('li');
   newTask.innerText = text;
+  newTask.className = 'task';
   taskListContainer.appendChild(newTask);
 }
 
 function createTask() {
   const createTaskButton = document.querySelector('#criar-tarefa');
 
-  createTaskButton.addEventListener('click', addTask)
+  createTaskButton.addEventListener('click', addTask);
 }
 
 window.onload = () => {
   createTask();
+  selectTask();
 };
