ccd0f32ecd65f546888bc8f30c6ed739af6e812c
Gui-lira <gui.lira.martins@gmail.com>
2021-03-10 15:03:30 -0300

    4

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const textTask = document.createElement('INPUT');
 textTask.id = 'texto-tarefa';
 const taskContainer = document.querySelector('.taskContainer');
 taskContainer.appendChild(textTask);
+const listContainer = document.querySelector('.listContainer');
+const list = document.createElement('ol');
+list.id = 'lista-tarefas';
+listContainer.appendChild(list);
