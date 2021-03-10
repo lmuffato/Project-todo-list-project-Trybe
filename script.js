3780068156a0de5547b48a7f793575ef71026f14
Flavio Franco <flavio.win@gmail.com>
2021-03-10 17:56:26 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function buttonCreateTaks() {
     listHeader.appendChild(itemList);
     document.getElementById('texto-tarefa').value = '';
     changeColorItems();
+    completeTasks();
   }
 
 }
@@ -18,17 +19,35 @@ buttonCreateTaks();
 
 function changeColorItems() {
   let itemLi = document.getElementsByTagName('li');
-  console.log(itemLi);
 
   for (let index = 0; index < itemLi.length; index += 1) {
     itemLi[index].addEventListener('click', paintBrush);
     function paintBrush(event) {
       for (index = 0; index < itemLi.length; index += 1) {
-        itemLi[index].style.backgroundColor = 'white';
+        itemLi[index].style.backgroundColor = '';
       }
       event.target.style.backgroundColor = 'rgb(128,128,128)';
     }
   }
+}
+
+function completeTasks() {
+  let itemL = document.getElementsByTagName('li');
+  
+
+  for (let index = 0; index < itemL.length; index += 1) {
+    itemL[index].addEventListener('dblclick', risc);
+
+    function risc(event) {
+      console.log(itemL);
+      if (event.target.className === 'completed') {
+        event.target.className = '';
+      } else {
+        event.target.className = 'completed';
+      }
+    }
+  }
 
 }
+
 
