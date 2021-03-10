6cdcbef93a31ea965076cd97392913945e1a0eca
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-10 15:45:29 -0300

    Requisito 05 - botao criar tarefa e evento

---
+++ script.js
@@ -0,0 +1,14 @@
+const createTaskBtn = document.getElementById('criar-tarefa');
+const inputTask = document.getElementById('texto-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+
+function addTask(e) {
+  e.preventDefault();
+  const newTaskItem = document.createElement('li');
+  const newTask = inputTask.value;
+  newTaskItem.innerText = newTask;
+  taskList.appendChild(newTaskItem);
+  inputTask.value = '';
+}
+
+createTaskBtn.addEventListener('click', addTask);
