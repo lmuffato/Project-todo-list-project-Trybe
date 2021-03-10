0bdf04b2a97b6c327d7adf2dfbc5c63e125fcd94
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-10 16:12:01 -0300

    Requisito 08 - possivel selecionar apenas 1 item

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const createTaskBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 // const tasks = [];
+// let selectedItem;
 
 function addTask(e) {
   e.preventDefault();
   const newTaskItem = document.createElement('li');
+  newTaskItem.classList.add('task');
   const newTask = inputTask.value;
   // tasks.push(newTask);
   newTaskItem.innerText = newTask;
@@ -13,9 +15,14 @@ function addTask(e) {
   inputTask.value = '';
 }
 
-function changeItemColor(e) {
-  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+function selectItem(e) {
+  const allTasks = document.querySelectorAll('.task');
+  for (let index = 0; index < allTasks.length; index += 1) {
+    allTasks[index].className = 'task';
+  }
+  e.target.classList.add('selected');
+  // selectedItem = tasks.findIndex((item) => item === e.target.innerText);
 }
 
 createTaskBtn.addEventListener('click', addTask);
-taskList.addEventListener('click', changeItemColor);
+taskList.addEventListener('click', selectItem);
