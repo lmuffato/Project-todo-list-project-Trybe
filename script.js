ebb1f033190db205d711e4ab73f50e48949298db
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-10 19:02:59 -0300

    correção requisito 1

--- script.js
+++ script.js
@@ -8,10 +8,10 @@
         
         let ListaTarefas = document.querySelector('#lista-tarefas');
         let TextoDigitado = document.querySelector('input').value;
-        let li = document.createElement('li');
-        li.innerText = TextoDigitado;
-        ListaTarefas.appendChild(li);
-        document.querySelector('input').value = '';
+        let li = document.createElement('li'); //crio a li
+        li.innerText = TextoDigitado; // coloco o valor do input dentro da li
+        ListaTarefas.appendChild(li); //colo a li como filha da ol
+        document.querySelector('input').value = ''; //não pude usar a variavel textodigitado 
 
         // crio o requisito 7 aqui, pois esse evento só acontece depois que criamos a li e criamos a li só quando o evento de cliclar no botão acontee
         li.addEventListener('click', function(event){
