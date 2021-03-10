5d151c22dd55c382ada162b1f7c9dda38c8f16c3
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-10 15:46:35 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const createTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const taskText = document.querySelector('#texto-tarefa');
+const taskListLi = document.querySelector('#lista-tarefas').childNodes;
 
+// Cria um item na "lista-tarefas" quando clicka no botão
 function createLi() {
   let creatingLi = document.createElement('li');
   let textValue = taskText.value;
   creatingLi.innerText = textValue;
 
-  taskList.appendChild(creatingLi);
+  taskList
+    .appendChild(creatingLi)
+    .addEventListener('click', changeBackgroundColor);
   taskText.value = '';
 }
 
 createTask.addEventListener('click', createLi);
+
+// Alterar a cor de fundo do item quando clicado
+function changeBackgroundColor(event) {
+  for (let j = 0; j < taskListLi.length; j += 1) {
+    taskListLi[j].classList.remove('selected');
+  }
+  event.currentTarget.classList.add('selected');
+}
