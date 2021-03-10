b0baae85a464a1a30f7142fe5bdfbf463a251372
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-03-10 17:11:14 -0300

    Requisitos 6-11

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ document.getElementById('criar-tarefa').addEventListener('click', () => {
   taskList.appendChild(listItem);
   inputText.value = '';
 });
+
+taskList.addEventListener('click', (newSelected) => {
+  const selected = document.querySelector('.selected');
+  if (selected) {
+    selected.classList.remove('selected');
+    newSelected.target.classList.add('selected');
+  } else {
+    newSelected.target.classList.add('selected');
+  }
+});
+
+taskList.addEventListener('dblclick', (completed) => {
+  const completedClass = completed.target.classList;
+  if (completedClass.contains('completed')) {
+    completedClass.remove('completed');
+  } else {
+    completedClass.add('completed');
+  }
+});
+
+document.getElementById('apaga-tudo').addEventListener('click', () => {
+  taskList.innerText = '';
+});
+
+document.getElementById('remover-finalizados').addEventListener('click', () => {
+  const completedTasks = document.querySelectorAll('.completed');
+  for (let i = 0; i < completedTasks.length; i += 1) {
+    completedTasks[i].remove();
+  }
+});
