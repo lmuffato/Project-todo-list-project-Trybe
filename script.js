7fbdf6042d0e7330c51700a920a21eeacbb474ca
mallmann02 <leomallmann020302@gmail.com>
2021-03-10 16:29:56 -0300

    Requisito 5 - Cria funcionalidade do botão

---
+++ script.js
@@ -0,0 +1,18 @@
+const listaTarefas = document.querySelector('#lista-tarefas');
+const adicionarTarefaButton = document.querySelector('#criar-tarefa');
+const adicionarTarefaInput = document.querySelector('#texto-tarefa');
+
+function criarElementoHtmlTarefa(nomeDaTarefa) {
+  let tarefaEl = document.createElement('li');
+  tarefaEl.className = 'tarefa';
+  tarefaEl.innerHTML = nomeDaTarefa;
+  return tarefaEl;
+}
+
+function adicionaTarefa() {
+  let tarefaParaAdicionar = adicionarTarefaInput.value;
+  const tarefa = criarElementoHtmlTarefa(tarefaParaAdicionar);
+  listaTarefas.appendChild(tarefa);
+}
+
+adicionarTarefaButton.addEventListener('click', adicionaTarefa);
