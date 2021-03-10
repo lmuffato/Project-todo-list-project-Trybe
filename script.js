3cd695602560a5aaf9dd8e15da01d7525af78d76
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-10 19:01:54 -0300

    9th Requirement done

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const toDoList = document.getElementById('lista-tarefas');
 const listItens = document.getElementsByTagName('li');
 
 function deleteSelectClass() {
-  console.log(listItens);
   for (let index = 0; index < listItens.length; index += 1) {
     listItens[index].classList.remove('selected');
   }
@@ -17,7 +16,28 @@ function deleteSelectClass() {
 
 function addSelectClass(e) {
   deleteSelectClass();
-  e.target.className = 'selected';
+  e.target.className += ' selected';
+  console.log(e);
+}
+
+function deleteCompletedClass() {
+  for (let index = 0; index < listItens.length; index += 1) {
+    listItens[index].classList.remove('completed');
+  }
+}
+
+function addCompletedClass(e) {
+  let sum = 0;
+  for (let index = 0; index < e.target.classList.length; index += 1) {
+    if (e.target.classList[index] !== 'completed') {
+      sum += 1;
+    }
+  }
+  if (sum === e.target.classList.length) {
+    e.target.classList += ' completed';
+  } else {
+    deleteCompletedClass();
+  }
 }
 
 function addTaskToList() {
@@ -25,19 +45,9 @@ function addTaskToList() {
   const taskElement = document.createElement('li');
   taskElement.innerText = task;
   taskElement.addEventListener('click', addSelectClass);
+  taskElement.addEventListener('dblclick', addCompletedClass);
   toDoList.appendChild(taskElement);
   textBox.value = '';
 }
 
 createTaskBtn.addEventListener('click', addTaskToList);
-
-/* function changeBackgroundColor(e) {
-  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
-
-function selectItem() {
-  for (let index = 0; index < listItens.length; index += 1) {
-    listItens[index].addEventListener('click', changeBackgroundColor);
-  }
-}
-selectItem(); */
