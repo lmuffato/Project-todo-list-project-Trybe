9eb266323b41320ef1e786738b52b1408866a39e
Diogo Augusto <diogotrescastro@gmail.com>
2021-03-10 18:23:35 -0300

    Resoluçao do requisito 5

---
+++ script.js
@@ -0,0 +1,9 @@
+const submitItem = document.getElementById('criar-tarefa');
+const inputItem = document.getElementById('texto-tarefa');
+const outputOl = document.querySelector('#lista-tarefas');
+
+submitItem.addEventListener('click', () => {
+  const createLi = document.createElement('li');
+  createLi.innerText = inputItem.value;
+  outputOl.appendChild(createLi);
+});
