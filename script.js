6028f959fe4d7b03f81e0133f5422ce1311f6ed3
Carol <carolinavpinheiro@gmail.com>
2021-03-10 18:20:19 -0300

    Adicionando requisito 8

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ const assignmentList = document.querySelector('#lista-tarefas');
 
 button.addEventListener('click', () => {
   const creatAssignmentListItem = document.createElement('li');
+  creatAssignmentListItem.classList = 'list-item';
   const assignmentListItem = assignmentList.appendChild(creatAssignmentListItem);
   assignmentListItem.innerText = assignmentText.value;
   assignmentText.value = "";
+  console.log(document.querySelectorAll('.list-item'));
 });
+
+function passGrayColor (e) {
+    e.target.style.color = 'rgb(128, 128, 128)';
+}
+
+function passGrayColorToTheList() {
+    assignmentList.addEventListener('click', passGrayColor);
+}
+passGrayColorToTheList();
