66b1a1622fb0560103dee5bd868694965850257b
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-10 15:07:16 -0300

    [REQUISITO][07, 08]

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// EVENTOS EM VÁRIOS ELEMENTOS
+const eventClickList = () => {
+  document.querySelectorAll('li').forEach((element) => {
+    element.addEventListener('click', (elementTarget) => {
+      const listElement = elementTarget.target;
+      const listElements = document.getElementsByTagName('li');
+
+      for (let index = 0; index < listElements.length; index += 1) {
+        if (listElements[index].id === 'selected-task') {
+          listElements[index].id = '';
+        }
+      }
+      listElement.id = 'selected-task';
+    });
+  });
+};
+
 // BOTÃO DE CRIAR
 const btnCreateTask = document.getElementById('criar-tarefa');
 const oListTask = document.getElementById('lista-tarefas');
@@ -6,5 +23,6 @@ btnCreateTask.addEventListener('click', () => {
   const inputTextTask = document.getElementById('texto-tarefa');
   listTaskElement.innerText = inputTextTask.value;
   oListTask.appendChild(listTaskElement);
+  eventClickList();
   inputTextTask.value = '';
 });
