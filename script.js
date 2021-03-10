6ad61daadb5c6af7485bdde1d759b5724509eacc
defauth98 <neto.daniribeiro@gmail.com>
2021-03-10 17:17:47 -0300

    Requisito 07: Selecionando uma tarefa

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function selectTask(item) {
+  const taskItem = item;
+  taskItem.style.backgroundColor = 'rgb(128,128,128)';
+}
+
 function createTask() {
   const createTaskButton = document.getElementById('criar-tarefa');
   createTaskButton.onclick = () => {
@@ -6,6 +11,7 @@ function createTask() {
 
     const taskItem = document.createElement('li');
     taskItem.innerHTML = taskInputName.value;
+    taskItem.onclick = () => selectTask(taskItem);
 
     taskInputName.value = '';
     taskList.appendChild(taskItem);
