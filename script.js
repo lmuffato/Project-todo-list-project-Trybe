cdc6f5084c225c6852fcfe3e5acd1ac6269c540f
Lucas André <lucas.cancado89@gmail.com>
2021-03-10 14:57:32 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function setTask() {
   const tagLi = document.createElement('li');
+  tagLi.className = 'list-item';
   tagLi.innerText = document.getElementById('texto-tarefa').value;
   document.getElementById('lista-tarefas').appendChild(tagLi);
   document.getElementById('texto-tarefa').value = '';
@@ -11,4 +12,14 @@ function clickColor(event) {
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
+function unsetColor() {
+  const items = document.getElementsByClassName('list-item');
+  for (let index = 0; index < items.length; index += 1) {
+    if (items[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+      items[index].style.backgroundColor = 'white';
+    }
+  }
+}
+
+document.getElementById('lista-tarefas').addEventListener('click', unsetColor);
 document.getElementById('lista-tarefas').addEventListener('click', clickColor);
