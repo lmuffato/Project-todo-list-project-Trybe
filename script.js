4cedd585db2661b016b83c6cb6c6525d59e6393c
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-10 18:07:33 -0300

    Nono Requisito

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ function changeColor(item) {
   const targe = item.target;
   for (let index = 0; index < colorize.length; index += 1) {
     if (colorize[index] === targe) {
-      colorize[index].setAttribute('class', 'itens selected');
+      colorize[index].classList.add('selected');
       colorize[index].style.backgroundColor = 'rgb(128, 128, 128)';
     } else {
-      colorize[index].setAttribute('class', 'itens');
+      colorize[index].classList.remove('selected');
       colorize[index].removeAttribute('style');
     }
+  }
+}
+
+function lineTrhough(item) {
+  if (item.target.classList.contains('completed')) {
+    item.target.classList.toggle('completed');
+    console.log('if');
+  } else {
+    item.target.classList.toggle('completed');
+    console.log('else');
   }
 }
 
@@ -21,12 +31,9 @@ function resetInput() {
   if (item !== '') {
     document.getElementById('texto-tarefa').value = '';
   }
+  ol.addEventListener('dblclick', lineTrhough);
   ol.addEventListener('click', changeColor);
-  console.log(ol.length);
 }
-
-
 
 const button = document.querySelector('#criar-tarefa');
 button.addEventListener('click', resetInput);
-// const itemColor = document.querySelector('#lista-tarefas');
