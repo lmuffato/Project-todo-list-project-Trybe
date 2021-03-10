399294244b3672006b823c908e29b6a054c577af
nascjoao <oi@joaonasc.dev>
2021-03-10 15:38:38 -0300

    Requisito 7: Seleciona item quando clicado

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const inputAddTask = document.getElementById('texto-tarefa');
 const buttonAddTask = document.getElementById('criar-tarefa');
 const tasksList = document.getElementById('lista-tarefas');
 
+const colors = {
+  selected: 'rgb(128, 128, 128)',
+};
+
 function addTask() {
   const newTask = document.createElement('li');
   newTask.textContent = inputAddTask.value;
@@ -9,4 +13,18 @@ function addTask() {
   inputAddTask.value = '';
 }
 
+function selectTask(element) {
+  const task = element.target;
+  task.style.backgroundColor = colors.selected;
+}
+
+function getTasks() {
+  const tasks = tasksList.children;
+  for (let index = 0; index < tasks.length; index += 1) {
+    const task = tasks[index];
+    task.addEventListener('click', selectTask);
+  }
+}
+
 buttonAddTask.addEventListener('click', addTask);
+buttonAddTask.addEventListener('click', getTasks);
