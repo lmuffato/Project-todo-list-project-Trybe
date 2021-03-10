c4c42cf5961d6b604a3ab685fa396e1bab4b8df8
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-10 18:46:34 -0300

    Requirement 6, 7, 8 Done

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-// Requirement 5;
+// Requirement 7, 8;
+
+function changeBackgroundColor(Event) {
+  const task = Event.target;
+  const selectLis = document.querySelectorAll('li');
+  for (let index = 0; index < selectLis.length; index += 1) {
+    if (selectLis[index].className === 'selected') {
+      selectLis[index].classList.remove('selected');
+    }
+  }
+  task.classList.add('selected');
+}
+
+// Requirement 5, 6;
 
 function addTask() {
   const selectInputText = document.querySelector('#texto-tarefa');
   const selectOL = document.querySelector('#lista-tarefas');
   const createLI = document.createElement('li');
   createLI.innerHTML = selectInputText.value;
+  createLI.addEventListener('click', changeBackgroundColor);
   selectOL.appendChild(createLI);
   selectInputText.value = '';
 }
