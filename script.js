24c042ca0b0d8b38ddecfebdd7372610bb04ebd0
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-10 16:40:04 -0300

    Oitavo Requisito

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function changeColor(item) {
+  const colorize = document.getElementsByClassName('itens');
+  const targe = item.target;
+  for (let index = 0; index < colorize.length; index += 1) {
+    if (colorize[index] === targe) {
+      colorize[index].setAttribute('class', 'itens selected');
+      colorize[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+      colorize[index].setAttribute('class', 'itens');
+      colorize[index].removeAttribute('style');
+    }
+  }
+}
+
 function resetInput() {
   const ol = document.getElementById('lista-tarefas');
   const item = document.createElement('li');
@@ -7,15 +21,12 @@ function resetInput() {
   if (item !== '') {
     document.getElementById('texto-tarefa').value = '';
   }
-  console.log(itemColor);
-  item.addEventListener('click', changeColor);
+  ol.addEventListener('click', changeColor);
+  console.log(ol.length);
 }
 
-function changeColor(item) {
-  const colorize = item.target;
-  colorize.style.backgroundColor = 'rgb(128, 128, 128)';
-}
+
 
 const button = document.querySelector('#criar-tarefa');
 button.addEventListener('click', resetInput);
-const itemColor = document.querySelector('#lista-tarefas');
+// const itemColor = document.querySelector('#lista-tarefas');
