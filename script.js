40c82d1700722182533491b3f4f4f8ab6022e43a
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-10 15:54:13 -0300

    Requisito 07 - clicar muda cor

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const createTaskBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+// const tasks = [];
 
 function addTask(e) {
   e.preventDefault();
   const newTaskItem = document.createElement('li');
   const newTask = inputTask.value;
+  // tasks.push(newTask);
   newTaskItem.innerText = newTask;
   taskList.appendChild(newTaskItem);
   inputTask.value = '';
 }
 
+function changeItemColor(e) {
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 createTaskBtn.addEventListener('click', addTask);
+taskList.addEventListener('click', changeItemColor);
