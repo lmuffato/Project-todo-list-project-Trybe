ef2d5a909795161985cad5e5d9747e48ec85be1c
maricegalla <maricegalla@gmail.com>
2021-03-10 18:07:10 -0300

    Requisitos 5

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#criar-tarefa');
+
+
+button.addEventListener('click', () => {
+  const addInput = document.querySelector('#texto-tarefa');
+  const olItem = document.querySelector('#lista-tarefas');
+  const liItem = document.createElement('li');
+  liItem.innerText = addInput.value;
+  olItem.appendChild(liItem);
+});
