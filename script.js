033802a951dd6ad34a7a1cb70643c23790d430c1
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-10 18:51:48 -0300

    requisitos 5 6 e 7 juntos

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-function CriarTarefa(){
+//requisito 5 e 6 e 7
+    
+    let botaoCriarTarefa = document.querySelector('#criar-tarefa');
 
-    let TextoDigitado = document.querySelector('input').value;
-    let ListaTarefas = document.querySelector('lista-tarefas');
-    let botaoCriarTarefa = document.querySelector('criar-tarefa');
+    botaoCriarTarefa.addEventListener('click', criarTarefa);
 
-    botaoCriarTarefa.addEventListener('click', clicar);
-
-    function clicar(){
+    function criarTarefa(){
+        
+        let ListaTarefas = document.querySelector('#lista-tarefas');
+        let TextoDigitado = document.querySelector('input').value;
         let li = document.createElement('li');
         li.innerText = TextoDigitado;
-        ListaTarefas.appendChild('li');
-        TextoDigitado.innerText = '';
+        ListaTarefas.appendChild(li);
+        document.querySelector('input').value = '';
 
+        // crio o requisito 7 aqui, pois esse evento só acontece depois que criamos a li e criamos a li só quando o evento de cliclar no botão acontee
+        li.addEventListener('click', function(event){
+            let tarefa = event.target;
+            tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
+        });
     }
 
-}
 
-CriarTarefa()
