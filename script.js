47d991a478c74c3713e46a86209f6243fa5bc3a1
Elisa <elisafranca.carvalho@gmail.com>
2021-03-10 19:31:05 -0300

    Requisito 5: Adiciona botao, conteudo a lista e limpa input

---
+++ script.js
@@ -0,0 +1,25 @@
+function getInput() {
+  const inputValue = document.getElementById('texto-tarefa').value;
+  return inputValue;
+}
+
+function addListItem() {
+  const ol = document.getElementById('lista-tarefas');
+  const listItem = document.createElement('li');
+  listItem.innerHTML = getInput();
+  ol.appendChild(listItem);
+}
+
+function clickButton() {
+  const button = document.getElementById('criar-tarefa');
+  const input = document.getElementById('texto-tarefa');
+
+  button.addEventListener('click', () => {
+    addListItem();
+    input.value = '';
+  });
+}
+
+window.onload = function startSession() {
+  clickButton();
+};
