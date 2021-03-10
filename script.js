8517e9b302cda288ee25872b8d3a6bb0190a9d6c
jonathan <jonathanfernandesjf98@gmail.com>
2021-03-10 18:07:11 -0300

    requisito 07 adicao de background na cor predefinida quando clicar no item da lista

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function backgroundLiColor(e) {
+    e.target.style.backgroundColor= 'rgb(128,128,128)';
+}
 function addLi() {
     const olTasks = document.getElementById('lista-tarefas')
     const taskText = document.getElementById('texto-tarefa')
@@ -5,11 +8,15 @@ function addLi() {
     liCreate.innerText = taskText.value;
     olTasks.appendChild(liCreate);
     taskText.value = '';
+    liCreate.className = 'item';
+    liCreate.addEventListener('click', backgroundLiColor)
 }
+
+
 function createListItems() {
     const taskCreate = document.getElementById('criar-tarefa')
     taskCreate.addEventListener('click', addLi)
-   
+
 }
 
 createListItems()
