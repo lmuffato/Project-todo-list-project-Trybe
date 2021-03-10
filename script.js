968d329ee4000f80e1a178d883fa1d4b0f31b688
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-03-10 19:59:32 -0300

    Requirements 6 and half 7

--- script.js
+++ script.js
@@ -9,21 +9,38 @@ function createButton () {
 }
 createButton();
 
-// pratica 5 - https://stackoverflow.com/questions/26688323/appending-input-value-to-list-using-javascript-to-do-list
+// pratica 5 e 6 - https://stackoverflow.com/questions/26688323/appending-input-value-to-list-using-javascript-to-do-list
 function clickButton (){
     
     let click = document.getElementById('criar-tarefa');
     click.addEventListener('click', function (event){
         let btnClick = event.target;
-        let addLi = document.getElementById('lista-tarefas');
+        let addLi = document.getElementById('lista-tarefas'); //ol
         let li = document.createElement('li');
         let text = document.getElementById('texto-tarefa');
         li.innerHTML = text.value;
+        li.addEventListener('click', function (event){
+            let evt = event.target;
+            evt.style.backgroundColor = 'rgb(128, 128, 128)';
+        });
         text.value = '';
         addLi.appendChild(li);
     })
 }
 clickButton ();
+
+
+// // prática 7
+// function backGrey (){
+//     let backColor = document.querySelectorAll('#lista-tarefas li');
+
+//     for (let i = 0; i < backColor.length; i += 1){
+       
+//         });
+//     }
+// }
+// backGrey ();
+
 
 
 
