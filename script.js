f4661043001f9086196474b2944d4af76da64df1
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-10 20:34:04 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-function addItens () {
-
-  let list = document.querySelector('#lista-tarefas');
-  let button = document.querySelector('#criar-tarefa');
-  let input = document.querySelector('#texto-tarefa');
-  input.addEventListener('input', function (e) {
-    console.log(e)
-    input.value = e.target.value
-  })
-  button.addEventListener('click', function() {
-    let item = document.createElement('li');
+function addItens() {
+  const list = document.querySelector('#lista-tarefas');
+  const button = document.querySelector('#criar-tarefa');
+  const input = document.querySelector('#texto-tarefa');
+  //input.addEventListener('input', function (e) {
+  //input.value = e.target.value;
+  //});
+  button.addEventListener('click', function add() {
+    const item = document.createElement('li');
     item.className = 'to-do-itens';
     list.appendChild(item);
     item.innerText = input.value;
     input.value = ''
-  })
-}
+  });
+};
 
 addItens();
+
+function changeItemColor() {
+  const list = document.querySelector('#lista-tarefas');
+  list.addEventListener('click', function (e) {
+    let itens = e.target;
+    itens.style.backgroundColor = "rgb(128,128,128)";
+  });
+};
+
+changeItemColor();
