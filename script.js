2eba490be40266ae1fbd25c1dce2080b16b88b09
rogerio <rogerio.cl@gmail.com>
2021-03-10 20:12:02 -0300

    atentendendo ate o requisito 7

--- script.js
+++ script.js
@@ -3,16 +3,27 @@
 const buttonAddTask = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+let selectedTask = null;
 
 // bloco de declaração de funções
 
+function markTask(event) {
+  if (selectedTask === null) {
+    selectedTask = event.target;
+  } else if (event.target !== selectedTask) {
+    selectedTask.className = 'task';
+    selectedTask = event.target;
+  }
+  selectedTask.className = 'task selected';
+}
 function addTask() {
   if (inputTask.value !== '') {
     let task = document.createElement('li');
     task.className = 'task';
     task.innerText = inputTask.value;
     inputTask.value = '';
-    taskList.appendChild(task);
+    task.addEventListener('click', markTask);
+    taskList.appendChild(task);    
   } else {
     alert('Insira o conteúdo da tarefa');
   }
@@ -20,6 +31,6 @@ function addTask() {
 
 // Bloco de criação de escutadores de eventos
 
-buttonAddTask.addEventListener('click', addTask)
+buttonAddTask.addEventListener('click', addTask);
 
 // Bloco de ações de inicialização da página
