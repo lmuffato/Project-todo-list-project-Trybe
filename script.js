f6482fcb6541d2d1970165e0f9b918a7c24a7fc2
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-10 20:17:08 -0300

    :rocket: feat(Requirement - 9: Click to check or uncheck the task.

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ function clearInput() {
   inputTasks.value = '';
 }
 
-function createListItem() {
-  const listItem = document.createElement('li');
-  listItem.classList.add('item-lista');
-  return listItem;
-}
-
 function removesBackgroundColor() {
   for (let index = 0; index < listItems.length; index += 1) {
     listItems[index].style.setProperty('background-color', 'rgb(255, 255, 255)');
@@ -24,9 +18,20 @@ function changeBgColor(event) {
   event.target.style.setProperty('background-color', 'rgb(128, 128, 128)');
 }
 
+function streakItem(event) {
+  event.target.classList.toggle('completed');
+}
+
+function createListItem() {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item-lista');
+  return listItem;
+}
+
 function updateListItemsListeners() {
   for (let index = 0; index < listItems.length; index += 1) {
     listItems[index].addEventListener('click', changeBgColor);
+    listItems[index].addEventListener('dblclick', streakItem);
   }
 }
 
