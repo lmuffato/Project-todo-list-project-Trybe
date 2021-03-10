70a9400775441f3bc20b7eaa4d21dde82652caee
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-10 20:53:24 -0300

    Requisito 7,8: adiciona cor de fundo ao item quando ele for clicado, e remove a cor de fundo quando clicar em outro item

--- script.js
+++ script.js
@@ -8,8 +8,28 @@ function tasks (recebeTarefa) {
 }
 
 botao.addEventListener('click', function() {
-    console.log('click aconteceu');
     let recebeTarefa = document.querySelector('#texto-tarefa').value;
     tasks(recebeTarefa);
     document.querySelector('#texto-tarefa').value = '';
 });
+
+function setClass(item){
+    if(document.querySelector(".mystyle")){
+        let itemRemove = document.querySelector(".mystyle");
+        itemRemove.classList.remove("mystyle");
+        itemRemove.removeAttribute("style");
+        itemRemove.removeAttribute("class");
+        item.className = "mystyle";
+        item.style.backgroundColor ='rgb(128, 128, 128)';
+    }else{
+        item.className = "mystyle";
+        item.style.backgroundColor ='rgb(128, 128, 128)';
+    }
+}
+
+const taskList = document.getElementById('lista-tarefas');
+taskList.addEventListener('click',()=>{
+    let clickedItem = event.target;
+        setClass(clickedItem);
+});
+
