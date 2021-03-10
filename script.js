b4cea9eb67e8dbc1f3d8bfcea9360cec1e3da97a
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-10 19:12:19 -0300

    fixed the require 05 to clean the text in the input

--- script.js
+++ script.js
@@ -2,27 +2,14 @@ window.onload = () => {
     const addButton = document.querySelector('#criar-tarefa');
     const list = document.querySelector('#lista-tarefas');
 
-    function addTextToList(e) {
+    function addTextToList() {
         let writedText = document.querySelector('#texto-tarefa').value;
         const createLi = document.createElement('li');
         createLi.innerHTML = writedText;
         list.appendChild(createLi);
+        document.querySelector('#texto-tarefa').value = '';
     }
-    
+
     addButton.addEventListener('click', addTextToList);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+};
