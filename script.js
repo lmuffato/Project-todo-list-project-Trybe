cb8d291c9a26b8d8f95d8869e44bcb24a0f20afa
Orlando Flores <orlando_flores@id.uff.br>
2021-03-10 19:20:21 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function AddTaskToList() {
+function addTaskToList() {
   const ol = document.getElementById('lista-tarefas');
   const btnAddTask = document.getElementById('criar-tarefa');
   const inputText = document.getElementById('texto-tarefa');
   btnAddTask.addEventListener('click', () => {
-    if(inputText.value.length > 0) {
+    if (inputText.value.length > 0) {
       const li = document.createElement('li');
-      li.innerText = inputText.value;
+      li.innerHTML = inputText.value;
+      li.className = 'task-list-item'
       ol.appendChild(li);
       inputText.value = '';
     } else {
@@ -14,6 +15,22 @@ function AddTaskToList() {
   });
 }
 
-window.onload = () => {
-  AddTaskToList();
+function selectedTask() { 
+  const tasksList = document.querySelector('ol');
+  tasksList.addEventListener('click', (event) => {
+      if (event.target.className === 'task-list-item') {
+        const selected = document.querySelector('.selected');
+        if (selected !== null) {
+          selected.className = 'task-list-item'
+          event.target.className = 'task-list-item selected';
+        } else {
+          event.target.className = 'task-list-item selected'
+        }
+    } 
+  });
 }
+
+window.onload = () => {
+  addTaskToList();
+  selectedTask();
+};
