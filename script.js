a14e162805f6004c2c786843982400cb4b941da8
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-10 20:43:42 -0300

    change the window.onload in script to he attribute defer in html

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-window.onload = () => {
-    /* -------- button add --------- */
-    const addButton = document.querySelector('#criar-tarefa');
-    const list = document.querySelector('#lista-tarefas');
+/* -------- button add --------- */
+const addButton = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
 
-    function addTextToList() {
-        let writedText = document.querySelector('#texto-tarefa').value;
-        const createLi = document.createElement('li');
-        createLi.innerHTML = writedText;
-        list.appendChild(createLi);
-        document.querySelector('#texto-tarefa').value = '';
+function addTextToList() {
+    const writedText = document.querySelector('#texto-tarefa').value;
+    const createLi = document.createElement('li');
+    createLi.innerHTML = writedText;
+    list.appendChild(createLi);
+    document.querySelector('#texto-tarefa').value = '';
+}
+
+addButton.addEventListener('click', addTextToList);
+
+/* ------- actived Li -------- */
+function paintBackground(e) {
+    if (document.querySelector('.active') !== null) {
+        document.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
+    } else {
+        e.target.classList.add('active');
     }
+}
 
-    addButton.addEventListener('click', addTextToList);
-
-    /* ------- actived Li -------- */
-    function paintBackground(e) {
-        if (document.querySelector('.active') !== null) {
-            document.querySelector('.active').classList.remove('active');
-            e.target.classList.add('active');
-        } else {
-            e.target.classList.add('active');
-        }
-    }
-
-    list.addEventListener('click', paintBackground);
-};
+list.addEventListener('click', paintBackground);
