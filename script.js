0a4c55fe397135ffcbcf8eb5c293fc610c91b7fc
Jodiel Briesemeister <jodielll29@gmail.com>
2021-03-10 18:25:43 -0300

    corrigido erros linter

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // requisitos 1, 2, 3 e 4 adicionados no index.html
 
 // requisito 5 e 6
+
+// olList!
+const olList = document.querySelector('#lista-tarefas');
+
 function createTask() {
   const addBtn = document.querySelector('#criar-tarefa');
   addBtn.addEventListener('click', () => {
-    const olList = document.querySelector('#lista-tarefas');
+    // const olList = document.querySelector('#lista-tarefas');
     const createLi = document.createElement('li');
     const addText = document.getElementById('texto-tarefa').value;
     createLi.innerHTML = addText;
@@ -16,7 +20,6 @@ createTask();
 
 // requisitos 7 e 8
 function selectOlItem() {
-  const olList = document.querySelector('#lista-tarefas');
   olList.addEventListener('click', (e) => {
     const color = 'rgb(128, 128, 128)';
     const checkNull = document.querySelector('#selected');
@@ -32,7 +35,6 @@ selectOlItem();
 
 // requisito 9
 function completedSelect() {
-  const olList = document.querySelector('#lista-tarefas');
   olList.addEventListener('dblclick', (e) => {
     if (e.target.className === 'completed') {
       e.target.className = '';
@@ -47,7 +49,6 @@ completedSelect();
 function clearList() {
   const clearBtn = document.querySelector('#apaga-tudo');
   clearBtn.addEventListener('click', () => {
-    const olList = document.querySelector('#lista-tarefas');
     olList.innerHTML = '';
   });
 }
@@ -58,7 +59,6 @@ function removeCompleted() {
   const removeBtn = document.querySelector('#remover-finalizados');
   removeBtn.addEventListener('click', () => {
     const list = document.querySelectorAll('#lista-tarefas li');
-    //document.querySelector('#lista-tarefas').childNodes;
     for (let index = 0; index < list.length; index += 1) {
       if (list[index].className === 'completed') {
         document.querySelector('#lista-tarefas').removeChild(list[index]);
