901590dd4923d79988a249eaf789433c10cc2ebf
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-10 19:50:11 -0300

    nono requisito!

--- script.js
+++ script.js
@@ -5,25 +5,47 @@ function add() {
 
   getBtn.addEventListener('click', () => {
     const item = document.createElement('li');
-    item.addEventListener('click', () => {
-      item.className = 'selected';
+    item.addEventListener('dblclick', () => {
     });
+    item.addEventListener('click', () => {
+      item.classList.add('selected');
+    });
+
     getList.appendChild(item).innerText = getInputText.value;
     getInputText.value = '';
   });
 }
 
 function clickEvent(event) {
-  const currentSelected = document.getElementsByClassName('selected')[0];
-  currentSelected.classList.remove('selected');
-  const classSelected = event.target;
-  classSelected.className = 'selected';
+  const currentSelected = document.querySelectorAll('.selected');
+  for (let index = 0; index < currentSelected.length; index += 1) {
+    currentSelected[index].classList.remove('selected');
+    const classSelected = event.target;
+    classSelected.classList.add('selected');
+  }
 }
+
+function clickDel(event) {
+  const getCompleted = event.target;
+  console.log(getCompleted.classList.value)
+  if (getCompleted.classList.value.includes('completed') == true) {
+      getCompleted.classList.remove('completed');
+  } else {
+      getCompleted.classList.add('completed');
+    }
+}
+
 
 function swapSelected() {
   const getSelected = document.getElementById('lista-tarefas');
   getSelected.addEventListener('click', clickEvent);
 }
 
+function swapCompleted() {
+  const getSelected = document.getElementById('lista-tarefas');
+  getSelected.addEventListener('dblclick', clickDel);
+}
+
 add();
 swapSelected();
+swapCompleted();
