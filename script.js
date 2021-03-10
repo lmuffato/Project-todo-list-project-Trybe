42853f4d7feaf55618046198b36a430509af56f4
nascjoao <oi@joaonasc.dev>
2021-03-10 18:40:25 -0300

    Requisito 12: Adiciona funçao para armazenar tarefas

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const buttonAddTask = document.getElementById('criar-tarefa');
 const tasksList = document.getElementById('lista-tarefas');
 const buttonClearTasks = document.getElementById('apaga-tudo');
 const buttonClearCompleted = document.getElementById('remover-finalizados');
+const buttonSaveTasks = document.getElementById('salvar-tarefas');
 
 const colors = {
   selected: 'rgb(128, 128, 128)',
@@ -66,6 +67,53 @@ function clearCompleted() {
   });
 }
 
+function saveTasks() {
+  const tasks = getTasks();
+  const tasksToStorage = [];
+  for (let index = 0; index < tasks.length; index += 1) {
+    let task = tasks[index];
+    const taskContent = task.textContent;
+    const taskIsCompleted = task.classList.contains('completed');
+    task = `${taskContent}/${taskIsCompleted}`;
+    tasksToStorage.push(task);
+  }
+  localStorage.setItem('tasks', tasksToStorage);
+}
+
+function taskToObject(currentTask) {
+  let task = currentTask;
+  task = task.split('/');
+  task = {
+    content: task[0],
+    isCompleted: task[1] === 'true',
+  };
+  return task;
+}
+
+function markIfTaskIsCompleted(task, taskElement) {
+  if (task.isCompleted === true) {
+    taskElement.classList.add('completed');
+  }
+}
+
+function loadTasks() {
+  const storageTasks = localStorage.getItem('tasks');
+  const tasks = storageTasks.split(',');
+  for (let index = 0; index < tasks.length; index += 1) {
+    if (storageTasks !== '') {
+      const task = taskToObject(tasks[index]);
+      console.log(task);
+      const taskElement = document.createElement('li');
+      taskElement.textContent = task.content;
+      markIfTaskIsCompleted(task, taskElement);
+      tasksList.appendChild(taskElement);
+    }
+  }
+  getTasks();
+}
+
+loadTasks();
 buttonActions(buttonAddTask, [addTask, getTasks]);
 buttonActions(buttonClearTasks, [clearTasks]);
 buttonActions(buttonClearCompleted, [clearCompleted]);
+buttonActions(buttonSaveTasks, [saveTasks]);
