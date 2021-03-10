13c897b51ec1177c93b9b8fbb6856f10566be161
Ana Ventura <anaventura1811@gmail.com>
2021-03-10 19:10:58 -0300

    Tentativa requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,22 @@
+function checkEachTask() {
+  const newElement = document.createElement('li');
+  const inputValue = document.getElementById('texto-tarefa').value;
+  const newText = document.createTextNode(inputValue);
+  newElement.appendChild(newText);
+  if (inputValue === '') {
+    alert('Você precisa escrever algo para adicionar à lista!');
+  } else {
+    document.getElementById('lista-tarefas').appendChild(newElement);
+  }
+}
+
+const btn = document.getElementById('criar-tarefa');
+btn.addEventListener('click', checkEachTask);
+
+function alterColorWhenClicked() {
+  const listItem = document.getElementsByTagName('li');
+  listItem.style.backgroundColor = 'rgb(128 , 128 , 128)';
+  listItem.addEventListener('click', alterColorWhenClicked);
+}
+
+alterColorWhenClicked();
