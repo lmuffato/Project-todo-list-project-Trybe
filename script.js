1f272dadb869478f724af4db3688f138d3e71f21
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-03-10 17:32:16 -0300

    Requisito 7 adiciona cor-rgb(128,128,128) ao backgroundColor do item da lista

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const botaoCriaTarefa = document.querySelector('#criar-tarefa');
-let inputTexto = document.querySelector('#texto-tarefa');
-let listaDeTarefas = document.querySelector('#lista-tarefas');
+const inputTexto = document.querySelector('#texto-tarefa');
+const listaDeTarefas = document.querySelector('#lista-tarefas');
 
 botaoCriaTarefa.addEventListener('click', criaTarefa);
 
 function criaTarefa() {
-    if (inputTexto.value !== '') {
-        let lista = document.createElement('li');
-        lista.innerText = inputTexto.value;
-        inputTexto.value = '';
-        listaDeTarefas.appendChild(lista);
-    }
-    
+  if (inputTexto.value !== '') {
+    const lista = document.createElement('li');
+    lista.innerText = inputTexto.value;
+    inputTexto.value = '';
+    listaDeTarefas.appendChild(lista);
+  }
+}
+
+listaDeTarefas.addEventListener('click', trocaCor);
+
+function trocaCor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  console.log(event);
 }
