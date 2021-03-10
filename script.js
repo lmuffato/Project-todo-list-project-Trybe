98fc2a085de868e7cea37be0dca97067a0ad2d3e
Gui-lira <gui.lira.martins@gmail.com>
2021-03-10 14:59:53 -0300

    3

---
+++ script.js
@@ -0,0 +1,4 @@
+const textTask = document.createElement('INPUT');
+textTask.id = 'texto-tarefa';
+const taskContainer = document.querySelector('.taskContainer');
+taskContainer.appendChild(textTask);
