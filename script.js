08ca349e6a89364874a0dc8346403f9600383242
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-10 16:10:52 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function createTask() {
 
   taskItems.innerText = textList;
   taskList.appendChild(taskItems);
+
 }
 
 function clearInput() {
@@ -15,3 +16,12 @@ function clearInput() {
 
 buttonAddTask.addEventListener('click', createTask);
 buttonAddTask.addEventListener('click', clearInput);
+
+function changeBgList(event) {
+  const listSelect = event.target;
+  const colorBackground = 'rgb(128, 128, 128)';
+
+  listSelect.style.backgroundColor = colorBackground;
+}
+
+document.getElementById('lista-tarefas').addEventListener('click', changeBgList);
