640a526955ba7608862ae89bd763d9c7aa8ed9df
rogerio <rogerio.cl@gmail.com>
2021-03-11 16:33:46 -0300

    requisito 9

--- script.js
+++ script.js
@@ -6,28 +6,43 @@ const taskList = document.getElementById('lista-tarefas');
 let selectedTask = null;
 
 // bloco de declaração de funções
-
 function markTask(event) {
   if (selectedTask === null) {
     selectedTask = event.target;
   } else if (event.target !== selectedTask) {
-    selectedTask.className = 'task';
+    console.log(selectedTask.className);
+    selectedTask.classList.remove('selected');
+    console.log(selectedTask.className);
     selectedTask = event.target;
   }
-  selectedTask.className = 'task selected';
+  selectedTask.classList.add('selected');
 }
+
+function markCompleted(event) {
+  const testedTask = event.target;
+  if (testedTask.className.includes('completed')) {
+    testedTask.classList.remove('completed');
+  } else {
+    testedTask.classList.add('completed');
+  }
+  console.log(testedTask);
+}
+
 function addTask() {
   if (inputTask.value !== '') {
-    let task = document.createElement('li');
+    const task = document.createElement('li');
     task.className = 'task';
     task.innerText = inputTask.value;
     inputTask.value = '';
     task.addEventListener('click', markTask);
+    task.addEventListener('dblclick', markCompleted);
     taskList.appendChild(task);    
   } else {
     alert('Insira o conteúdo da tarefa');
   }
 }
+
+
 
 // Bloco de criação de escutadores de eventos
 
