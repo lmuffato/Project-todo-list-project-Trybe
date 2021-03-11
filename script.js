88c847d2a1bf266f758fba29b13ee94262a897b5
Max Rudim <max.hermsdorff@gmail.com>
2021-03-11 16:31:27 -0300

    Requisito 05 pronto

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const text = document.getElementById('texto-tarefa');
+const getClick = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+getClick.addEventListener('click', addText);
 
+function addText () {
+const createItem = document.createElement('li');
+createItem.innerText = text.value;
+text.value = '';
+list.appendChild(createItem);
+}
