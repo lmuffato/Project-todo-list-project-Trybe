6ba9fe695a6dbc9775106b294bb4e787e970a145
Eduardo Seije <eduardoseije@gmail.com>
2021-03-11 18:43:43 -0300

    Requisito 08 - teste

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-function addNewTask(){
-  let textTask = document.querySelector('#texto-tarefa').value;
-  let taskList = document.querySelector('#lista-tarefas')
-  let newList = document.createElement('li');
+function addNewTask() {
+  const textTask = document.querySelector('#texto-tarefa').value;
+  const taskList = document.querySelector('#lista-tarefas');
+  const newList = document.createElement('li');
   taskList.appendChild(newList);
-  newList.innerText = textTask;    
+  newList.innerText = textTask;
+  newList.style.backgroundColor = 'white'
 }
-let addBtn = document.querySelector('#criar-tarefa');
+const addBtn = document.querySelector('#criar-tarefa');
 addBtn.addEventListener('click', addNewTask);
 
 function restartInput() {
-    let textTask = document.querySelector('#texto-tarefa');
-    textTask.value = ('');
+  const textTask = document.querySelector('#texto-tarefa');
+  textTask.value = ('');
 }
 addBtn.addEventListener('click', restartInput);
+
+function changeBackgroundColor() {
+    let textList = document.querySelectorAll('li');
+    for (let index = 0; index < textList.length; index += 1) {
+      textList[index].addEventListener('click', function (event) {
+        let selectedItem = event.target;
+        let selectedColor = 'rgb(128,128,128)';
+        selectedItem.style.backgroundColor = selectedColor;
+      });  
+    }
+  }
+  changeBackgroundColor();
+
+
+
