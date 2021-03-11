91b602338f56b56ff2dc472c02d5d155c1600be8
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-11 18:20:50 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -13,35 +13,36 @@ function criarTarefa(){
     ListaTarefas.appendChild(li); //colo a li como filha da ol
     document.querySelector('input').value = ''; //não pude usar a variavel textodigitado 
 
-    // crio o requisito 7 aqui, pois esse evento só acontece depois que clicamos a li e criamos a li foi criada e está dentro da função criarTarefa
+    // crio o requisito 7 e 8 aqui, pois esse evento só acontece depois que clicamos a li e criamos a li foi criada e está dentro da função criarTarefa
+    //primeiro eu fiz um array para retirar a classe cor lista caso exista em algum lugar da lista, se não existe eu posso adicionar a classe
     li.addEventListener('click', function(event){
-    let tarefa = event.target;
-    tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
+    let tarefa = event.target; 
+    
+    tarefasArray = document.querySelectorAll('.cor-lista');  
+    for (index = 0; index < tarefasArray.length; index += 1){
+        tarefasArray[index].classList.remove('cor-lista')   
+    }
+
+    tarefa.classList.add('cor-lista'); 
+
     }); 
 
-    //requisito 8
-    /*let listaTarefasArray = document.querySelector('#lista-tarefas li');
-
-    for (index = 0; index < listaTarefaArray.length; index += 0){
-        listaTarefaArray[index]
-    } */
-
-    //requisito 9
-    /*li.addEventListener('dblclick', function(event){
-        let tarefa = event.target;
-        tarefa.classList.add('completed');
-        tarefa.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
-    }); 
-    /*li.addEventListener('dblclick', function(event){
-        let tarefa = event.target;
+    //requisito 9 colocar e tirar text decoration  https://www.w3schools.com/jsref/prop_element_classlist.asp
+    // faço um if pois quero que ou a classe seja adicionada ou removida. Primeiro olho se já tem, se tem eu tiro, se não eu coloco
+    li.addEventListener('dblclick', function(event){
+        let tarefa = event.target; 
+        if (tarefa.classList.contains('completed')){
         tarefa.classList.remove('completed');
-        tarefa.style.textDecoration = 'none';
-    });*/
+        //tarefa.style.textDecoration = 'transparent';
+        } else {
+        tarefa.classList.add('completed');
+        //tarefa.style.textDecoration = 'line-through solid rgb(0, 0, 0)'; 
+        }
+    }); 
         
 }
 
-//requisito 10
-
+//requisito 10 apaga a lista
 let botaoApagar = document.querySelector('#apaga-tudo');
 
 botaoApagar.addEventListener('click', function(){
@@ -50,15 +51,11 @@ botaoApagar.addEventListener('click', function(){
     for (index = 0; index < tarefas.length; index += 1){
         ListaTarefas.removeChild(tarefas[index]); 
     } 
-
     //let ListaTarefas = document.querySelector('#lista-tarefas');
     //ListaTarefas.innerHTML =''; // coloco a ol com o conteudo vazio ---> dica do Marcus Cesar tumarma 10-A
 });
 
-
-
-//requisito 11
-
+//requisito 11 apaga os finalizados
 let botaoApagarFinalizadas = document.querySelector('#remover-finalizados');
 
 botaoApagarFinalizadas.addEventListener('click',function(){
@@ -68,21 +65,3 @@ botaoApagarFinalizadas.addEventListener('click',function(){
         ListaTarefas.removeChild(ListaTarefasFinalizadas[index]);
     }       
 });
-
-
-
-
-   
-//requisito 7
-/* 
-
-let listaTarefasArray = document.querySelector('#lista-tarefas li');
-
-for (index = 0; index < listaTarefaArray.length; index += 0){
-    listaTarefaArray[index].addEventListener('click', function(event){
-            let tarefa = event.target;
-            tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
-        });
-}
-
-*/
