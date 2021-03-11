dc7d84a32cb47f71c81dc9337c0a19b23a47df86
Rafael Ribeiro <rafittu@outlook.com>
2021-03-11 17:10:15 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.getElementById('criar-tarefa').addEventListener('click', () => {
-  let btnContainer = document.getElementById('texto-tarefa').value;
-  let li = document.createElement('li');
-  let task = document.createTextNode(btnContainer);
+  const btnContainer = document.getElementById('texto-tarefa').value;
+  const li = document.createElement('li');
+  li.className = 'item';
+  const task = document.createTextNode(btnContainer);
 
   li.appendChild(task);
   document.getElementById('lista-tarefas').appendChild(li);
   document.getElementById('texto-tarefa').value = '';
+
+  li.addEventListener('click', function(event) {
+    const colorTask = event.target;
+    colorTask.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
 });
