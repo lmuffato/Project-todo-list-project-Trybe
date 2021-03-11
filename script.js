41457bd233ef7a87ac71b9901056a75e6a3d7657
Alan <alan.alb.flopes@gmail.com>
2021-03-11 11:00:24 -0400

    add requisito[6,7]

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let taskList = document.getElementById("lista-tarefas");
+let tasks = document.getElementsByTagName('li');
 let addTaskButton =  document.getElementById("criar-tarefa");
     addTaskButton.addEventListener("click",addTask);
 function addTask(){
@@ -12,3 +14,26 @@ function addTask(){
     listContent.value="";
     }
 }
+document.body.addEventListener("click",ElementSelected);
+function ElementSelected(e){
+    console.log(e.target.className);
+    for(let index =0;index<tasks.length;index+=1){
+        if(e.target==tasks[index]){
+            tasks[index].style.backgroundColor="rgb(128,128,128)";
+            
+            if(e.target.className=="selected"){
+                tasks[index].style.backgroundColor="initial";
+                tasks[index].classList.remove("selected");
+            }else{
+                tasks[index].classList.add("selected");
+            }
+        }
+    }
+}
+// if(taskList.children.length > 0 ){
+// taskList.children[0].addEventListener('click',taskSelected)
+// function taskSelected(e){
+//     let event = e
+//     event.target.
+// }
+// }
