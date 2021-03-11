4880316abd4f334107ce0d105b589fd44ee3f58f
Max Rudim <max.hermsdorff@gmail.com>
2021-03-11 19:15:46 -0300

    Requisitos 07 e 08 (talvez)

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const text = document.getElementById('texto-tarefa');
 const getClick = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 getClick.addEventListener('click', addText);
+list.addEventListener('click', clicou);
 
 function addText () {
-const createItem = document.createElement('li');
-createItem.innerText = text.value;
-text.value = '';
-list.appendChild(createItem);
+  const createItem = document.createElement('li');
+  createItem.innerText = text.value;
+  text.value = '';
+  createItem.className = 'listItem';
+  list.appendChild(createItem);
+}
+
+function clicou (event) {
+  const receiveItem = document.getElementsByClassName('listItem');
+  console.log(event);
+  const select = event.target
+  for (index = 0; index < receiveItem.length; index += 1) {
+    if (receiveItem[index] === select) {
+      receiveItem[index].classList.add('selected');
+      receiveItem[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+    else {
+      receiveItem[index].classList.remove('selected');
+      receiveItem[index].removeAttribute('style');
+    }
+  }
+
 }
