2cce54f8d21fd8cfe00f5c33ab608e14944110a4
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-11 18:33:54 -0300

    requisito 7 e 8 - conserta o problema com a cor

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 const button = document.querySelector('#criar-tarefa');
 const boxText = document.querySelector('#texto-tarefa');
 
+// muda a cor de fundo da tarefa ao ser clicada
+function changeColor(e) {
+  // e.target.className = 'tarefa-selecionada';
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  console.log(e.target);
+}
+
+// retira a cor cinza
+function resetColor() {
+  const currentItem = document.getElementsByClassName('itemList');
+  for (let index = 0; index < currentItem.length; index += 1) {
+    if (currentItem[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+      currentItem[index].style.backgroundColor = 'white';
+    }
+  }
+}
+
 // adiciona nova tarefa
 function criaTarefa() {
   const item = document.createElement('li');
@@ -9,27 +26,14 @@ function criaTarefa() {
   item.innerText = boxText.value;
   document.querySelector('#lista-tarefas').appendChild(item);
   boxText.value = '';
+  item.addEventListener('click', resetColor);
+  item.addEventListener('click', changeColor);
 }
 
 button.addEventListener('click', criaTarefa);
 
-function resetColor() {
-  const currentItem = document.getElementsByClassName('itemList');
-  for (let index = 0; index < currentItem.length; index += 1) {
-    if (currentItem[index].style.backgroundColor === 'red') {
-      currentItem[index].style.backgroundColor = 'white';
-    }
-  }
-}
 
 
-// muda a cor de fundo da tarefa ao ser clicada
-function changeColor(e) {
-  // e.target.className = 'tarefa-selecionada';
-  e.target.style.backgroundColor = 'red';
-  console.log(e.target);
-}
 
-document.getElementById('lista-tarefas').addEventListener('click', resetColor);
 
-document.getElementById('lista-tarefas').addEventListener('click', changeColor);
+//document.getElementById('lista-tarefas').addEventListener('click', changeColor);
