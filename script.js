d92b8c260421707ae39b96ff776fc194e76be4ae
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-11 13:26:51 -0300

    requisito sete e oito

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-
 const btnTask = document.querySelector('#criar-tarefa');
 const tasksList = document.querySelector('#lista-tarefas');
 btnTask.addEventListener('click', function () {
-  let textTask = document.getElementById('texto-tarefa').value;
+  const textTask = document.getElementById('texto-tarefa').value;
   document.getElementById('texto-tarefa').value = '';
-  let list = document.createElement('li');
+  const list = document.createElement('li');
   list.innerText = textTask;
   tasksList.appendChild(list);
-})
+  list.classList.add('listItem');
+  list.style.backgroundColor = 'white';
+});
+
+tasksList.addEventListener('click', function (event) {
+  const lists = document.querySelectorAll('li');
+  for (let list = 0; list < lists.length; list += 1) {
+    if (lists[list].style.backgroundColor = 'white') {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      lists[list].classList.remove('selected');
+      event.target.classList.add('selected');
+    } else {
+      event.target.backgroundColor = 'rgb(128, 128, 128)';
+      event.target.classList.add('selected');
+    }
+  }
+});
