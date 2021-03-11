00b927c73776d26a6d089fc90b4b115d4b614cb1
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-11 08:43:29 -0300

    requisito 7 - altera cor do item para cinza

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// botao adiciona nova tarefa
+// recupera o botão e a caixa de texto
 let button = document.querySelector('#criar-tarefa');
 let boxText = document.querySelector('#texto-tarefa');
 
-button.addEventListener('click' , function () {
-  let novaTarefa = document.createElement('li');
-  document.querySelector('#lista-tarefas').appendChild(novaTarefa);
-  novaTarefa.innerText = boxText.value;
+// botao adiciona nova tarefa
+button.addEventListener('click', function () {
+  let item = document.createElement('li');
+  document.querySelector('#lista-tarefas').appendChild(item);
+  item.innerText = boxText.value;
   boxText.value = '';
-})
+  changeColor (item);
+});
+
+// função muda a cor de fundo da tarefa ao ser clicada
+function changeColor (itemList) {
+  itemList.addEventListener('click', function (e) {
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
+}
