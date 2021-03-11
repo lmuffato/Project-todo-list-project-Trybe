d228113853079389605c4e8995dd93b9deaf06da
Gabriel Ferreira <glucas709@gmail.com>
2021-03-11 20:24:40 -0300

    Requisito 7 e 8 - Seleção das tarefas

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
 const inputValue = document.getElementById('texto-tarefa');
+let taskItem = document.querySelectorAll('.task');
+
+function selectedTask() {
+  taskItem.forEach((task, index) => {
+    task.addEventListener('click', () => {
+      for (let i = 0; i < taskItem.length; i += 1) {
+        taskItem[i].classList.remove('selected');
+      }
+      taskItem[index].className = 'task selected';
+    });
+  });
+}
 
 function createTask() {
   if (inputValue.value !== '') {
     const li = document.createElement('li');
     listTasks.appendChild(li);
+    li.className = 'task';
     li.innerHTML = inputValue.value;
     inputValue.value = '';
+    taskItem = document.querySelectorAll('.task');
   }
+  selectedTask();
 }
 
 btnCriarTarefa.addEventListener('click', createTask);
