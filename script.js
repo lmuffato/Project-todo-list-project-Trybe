a930b31e33de405357d2491fecf6b12c790ecb05
Rafael Ribeiro <rafittu@outlook.com>
2021-03-11 20:57:54 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 document.getElementById('criar-tarefa').addEventListener('click', () => {
   const btnContainer = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
-  li.className = 'item';
   const task = document.createTextNode(btnContainer);
 
   li.appendChild(task);
   document.getElementById('lista-tarefas').appendChild(li);
   document.getElementById('texto-tarefa').value = '';
 
-  li.addEventListener('click', function(event) {
+  // li.addEventListener('click', function (event) {
+  //   let taskItem = document.getElementById('lista-tarefas').childNodes;
+  //   let taskColor = event.target;
+
+  //   for (let index = 0; index < taskItem.length; index += 1) {
+  //     if (taskItem.style.backgroundColor === 'rgb(128, 128, 128)') {
+  //       taskItem.style.backgroundColor = 'white';
+  //     } else {
+  //       taskColor.style.backgroundColor = 'rgb(128, 128, 128)';
+  //     }
+  //   }
+  // });
+
+  li.addEventListener('click', function (event) {
     const colorTask = event.target;
     colorTask.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
+  });
+
+  li.addEventListener('dblclick', () => {
+    li.classList.toggle('completed');
+  });
 });
