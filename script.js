913a02c6b06276d1c883dc06df261d7ae2dd131c
luanlsr <luanfswd@gmail.com>
2021-03-11 00:54:52 -0300

    corrigido requisito 9

--- script.js
+++ script.js
@@ -3,50 +3,49 @@ const createAssignment = document.querySelector('#criar-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
 const clear = document.querySelector('#remover-finalizados');
 const clearAll = document.querySelector('#apaga-tudo');
-const clearSelected = document.querySelector('#remover-selecionado')
+const clearSelected = document.querySelector('#remover-selecionado');
 
 createAssignment.addEventListener('click', () => {
   const li = document.createElement('li');
-  li.innerText = input.value
-  todoList.appendChild(li)
+  li.innerText = input.value;
+  todoList.appendChild(li);
 
-  input.value = ''
+  input.value = '';
 });
 
-//resolvido com a ajuda da amiga Clarice Oliveira.
+// resolvido com a ajuda da amiga Clarice Oliveira.
 todoList.addEventListener('click', (e) => {
   const selectedItem = e.target;
   const liColor = document.querySelectorAll('#lista-tarefas li');
   for (const liC of liColor) {
-    liC.classList.remove('gray')
+    liC.classList.remove('gray');
   }
-      selectedItem.classList.toggle('gray');
-}); 
+  selectedItem.classList.toggle('gray');
+});
 
 todoList.addEventListener('dblclick', (e) => {
   const selectedItem = e.target;
-  
-  selectedItem.classList.add('completed');
-}); 
+  selectedItem.classList.toggle('completed');
+});
 
-//código feito baseado no code Review do Lucas Pedroso.
+// código feito baseado no code Review do Lucas Pedroso.
 clear.addEventListener('click', () => {
   const fineshed = document.querySelectorAll('.completed');
-  for (let final of fineshed) {
+  for (const final of fineshed) {
     final.remove();
   }
 });
 
 clearAll.addEventListener('click', () => {
   const allCleaned = document.querySelectorAll('#lista-tarefas li');
-  for (let final of allCleaned) {
+  for (const final of allCleaned) {
     final.remove();
   }
 });
 
 clearSelected.addEventListener('click', () => {
   const fineshed = document.querySelectorAll('.gray');
-  for (let final of fineshed) {
+  for (const final of fineshed) {
     final.remove();
   }
 });
