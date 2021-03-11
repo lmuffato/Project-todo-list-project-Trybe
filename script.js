98f49b69dec3f6527b0e5ebebc4bb864b652d038
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-11 08:47:40 -0300

    corrige erros de lint

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // recupera o botão e a caixa de texto
-let button = document.querySelector('#criar-tarefa');
-let boxText = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+const boxText = document.querySelector('#texto-tarefa');
 
 // botao adiciona nova tarefa
-button.addEventListener('click', function () {
+button.addEventListener('click', function() {
   let item = document.createElement('li');
   document.querySelector('#lista-tarefas').appendChild(item);
   item.innerText = boxText.value;
   boxText.value = '';
-  changeColor (item);
+  changeColor(item);
 });
 
 // função muda a cor de fundo da tarefa ao ser clicada
-function changeColor (itemList) {
+function changeColor(itemList) {
   itemList.addEventListener('click', function (e) {
     e.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
+  });
 }
