b50af0e9397c96b89a013965e5b0edb499fd2890
Vinicios Fraga <soicinivt@gmail.com>
2021-03-10 22:27:02 -0300

    Requisito 8.

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ maker.addEventListener('click', function newTask() {
   const theTask = document.createElement('li');
   const theList = document.querySelector('#lista-tarefas');
   theTask.innerHTML = task.value;
+  theTask.classList.add('task');
   theList.appendChild(theTask);
   task.value = '';
 });
 
 const theList = document.querySelector('#lista-tarefas');
 theList.addEventListener('click', function mark(element) {
-  element.target.classList.add('bkGray');
+  const remove = document.querySelector('.bkGray');
+  if (remove === null) {
+    element.target.classList.add('bkGray');
+  } else {
+    remove.classList.remove('bkGray');
+    element.target.classList.add('bkGray');
+  }
 });
