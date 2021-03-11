ff1ede86a59d4288d5a6fdad04cf3b5f01c6fab6
Bruno Mendes <mendessbruno@gmail.com>
2021-03-10 23:19:18 -0300

    resolvido exercicio 9 e alterado no exercicio 8 para adicionar e remver classes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function change(list) {
   for (let index2 = 0; index2 < list.length; index2 += 1) {
     const element2 = list[index2];
     if (element2.className === selectedClass) {
-      element2.className = 'task';
+      element2.classList.remove('selected');
       result = 1;
     } else {
       result = 1;
@@ -26,7 +26,40 @@ function setColor() {
     const element = list[index];
     element.addEventListener('click', () => {
       if (change(list) === 1) {
-        element.className = 'task selected';
+        element.classList.add('selected');
+      }
+    });
+  }
+}
+
+function doneTaskSelect(list) {
+  let result = 0;
+  let counter = 0;
+  for (let index = 0; index < list.length; index += 1) {
+    const element = list[index];
+    if (element === 'completed') {
+      result = 1;
+    } else {
+      counter += 1;
+    }
+  }
+  if (counter === list.length) {
+    result = 2;
+  }
+  return result;
+}
+
+function doneTask() {
+  const task = document.getElementsByClassName('task');
+  for (let index = 0; index < task.length; index += 1) {
+    const element = task[index];
+    element.addEventListener('dblclick', () =>{
+      const list = element.classList;
+      const result = doneTaskSelect(list);
+      if (result === 1) {
+        element.classList.remove('completed');
+      } else if (result === 2) {
+        element.classList.add('completed');
       }
     });
   }
@@ -40,8 +73,10 @@ function addItem() {
     createLi(text);
     textBox.value = '';
     setColor(); // declarado aqui para toda vez que um item for adicionado rodar a função de mudar de cor e atualizar o array;
+    doneTask();
   });
 }
+
 
 window.onload = function run() {
   addItem();
