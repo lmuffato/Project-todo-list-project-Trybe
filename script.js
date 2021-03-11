35ed30159f74cebf20063b60a5fa56ca7b34c968
maricegalla <maricegalla@gmail.com>
2021-03-11 16:08:06 -0300

    Requisitos 8

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const button = document.querySelector('#criar-tarefa');
-
+const addInput = document.querySelector('#texto-tarefa');
+const olItem = document.querySelector('#lista-tarefas');
 
 button.addEventListener('click', () => {
-  const addInput = document.querySelector('#texto-tarefa');
-  const olItem = document.querySelector('#lista-tarefas');
   const liItem = document.createElement('li');
   liItem.innerText = addInput.value;
+  liItem.style.marginBottom = '10px';
+  liItem.style.fontSize = '15px';
+  liItem.style.marginRight = '75%'
+  liItem.addEventListener('click', (event) => {
+    const selectedItem = document.querySelector('#selected');
+    if (selectedItem) {
+      selectedItem.removeAttribute('id');
+    }
+    let gray = event.target;
+    gray.id = 'selected';
+  });
   olItem.appendChild(liItem);
   addInput.value = '';
 });
+
+ 
+
+
