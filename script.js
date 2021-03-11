728c949a93f0842690eef0ece10f09dbc9e177d4
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-03-11 15:55:25 -0300

    requisito 7 feito

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const taskWriten = document.querySelector('#texto-tarefa');
+const list = document.querySelector('#lista-tarefas');
 
 // Requisitos 5 e 6
 // Lógica para pegar texto do input e apagá-lo ao final vista no repositório
 // do Jodiel https://github.com/tryber/sd-010-a-project-todo-list/pull/70
-function AddToList(){
-    let addButton = document.querySelector('#criar-tarefa');
-    addButton.addEventListener('click', function(){
-        let list = document.querySelector('#lista-tarefas');
-        let elementList = document.createElement('li');
-        let text = taskWriten.value;
-        elementList.innerHTML = text;
-        list.appendChild(elementList);
-        taskWriten.value = '';
-    });
+function AddToList() {
+  let addButton = document.querySelector('#criar-tarefa');
+  addButton.addEventListener('click', function(){
+    const elementList = document.createElement('li');
+    let text = taskWriten.value;
+    elementList.innerHTML = text;
+    list.appendChild(elementList);
+    taskWriten.value = '';
+  });
 }
 
 AddToList();
+
+// Requisito 7
+// Lógica pega no meu código de selecionar cor do projeto Pixel Art
+function SelectingTask() {
+    list.addEventListener('click', (event) =>{
+        let color = 'rgb(128, 128, 128)';
+        const checkingId = document.querySelector('#selected');
+        if (checkingId != null) {
+          document.querySelector('#selected').style.backgroundColor = 'white';
+          document.querySelector('#selected').id = '';
+        }
+        event.target.style.backgroundColor = color;
+        event.target.id = 'selected';
+    });
+}
+
+SelectingTask();
