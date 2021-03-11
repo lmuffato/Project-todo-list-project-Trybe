01f0c8732200119ffa82ea2993c538ab36429bbd
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-11 20:36:54 -0300

    Cria listas

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function (){
+    // Pega o clique do botão 
+    let btn = document.getElementById("criar-tarefa");
+    // cria a variável que irá receber o texto do input
+    let pushInput = ''
+    // cria um contador para adicionar mais elementos na lista
+    let cont = 0;
+
+    btn.addEventListener('click', function (){
+        pushInput = document.getElementById('texto-tarefa').value;
+        addList = document.getElementsByTagName('li');
+        // Cria os elementos 'li'
+        let pushOl = document.getElementById('lista-tarefas');
+        let creatLi = document.createElement('li');
+        pushOl.appendChild(creatLi);
+        //adiciona a lista e acrescenta 1 números        
+        addList[cont].innerText = pushInput;
+        cont += 1;
+                
+    });
+
+}
