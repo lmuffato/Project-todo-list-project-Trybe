609a1b837f53d79eeead7e15e5b4c2db653f0bf9
rogerio <rogerio.cl@gmail.com>
2021-03-11 18:05:38 -0300

    requisito 10 e 12

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Bloco de inicialização de variáveis globais
 
-const buttonAddTask = document.getElementById('criar-tarefa');
+const addTaskButton = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+const clearButton = document.getElementById('apaga-tudo');
+const saveButton = document.getElementById('salvar-tarefas');
+const removeCompletedButton = document.getElementById('remover-finalizados');
+const muveUpButton = document.getElementById('mover-cima');
+const moveDownButton = document.getElementById('mover-baixo');
 let selectedTask = null;
 
 // bloco de declaração de funções
@@ -10,9 +15,7 @@ function markTask(event) {
   if (selectedTask === null) {
     selectedTask = event.target;
   } else if (event.target !== selectedTask) {
-    console.log(selectedTask.className);
     selectedTask.classList.remove('selected');
-    console.log(selectedTask.className);
     selectedTask = event.target;
   }
   selectedTask.classList.add('selected');
@@ -25,9 +28,7 @@ function markCompleted(event) {
   } else {
     testedTask.classList.add('completed');
   }
-  console.log(testedTask);
 }
-
 function addTask() {
   if (inputTask.value !== '') {
     const task = document.createElement('li');
@@ -36,16 +37,51 @@ function addTask() {
     inputTask.value = '';
     task.addEventListener('click', markTask);
     task.addEventListener('dblclick', markCompleted);
-    taskList.appendChild(task);    
+    taskList.appendChild(task);
   } else {
     alert('Insira o conteúdo da tarefa');
   }
 }
 
+function clearTaskList() {
+  document.getElementById('lista-tarefas').innerHTML = '';
+}
 
+function removeCompletedTasks() {
+  const completedTasks = document.getElementsByClassName('completed');
+  completedTasks.clear();
+}
+
+function saveTaskList() {
+  localStorage.setItem('taskList', taskList.innerHTML);
+  alert('A lista foi salva no browser');
+}
+
+function moveTask() {
+
+}
+
+function recoverySavedTaskList() {
+  if (localStorage.getItem('taskList') !== null) {
+    taskList.innerHTML = localStorage.getItem('taskList');
+    const tasks = document.getElementsByClassName('task');
+    for (let index = 0; index < tasks.length; index += 1) {
+      tasks[index].addEventListener('click', markTask);
+      tasks[index].addEventListener('dblclick', markCompleted);
+    }
+    selectedTask = document.querySelector('.selected');
+  }
+}
 
 // Bloco de criação de escutadores de eventos
 
-buttonAddTask.addEventListener('click', addTask);
+addTaskButton.addEventListener('click', addTask);
+clearButton.addEventListener('click', clearTaskList);
+removeCompletedButton.addEventListener('click', removeCompletedTasks);
+saveButton.addEventListener('click', saveTaskList);
+muveUpButton.addEventListener('click', moveTask);
+moveDownButton.addEventListener('click', moveTask);
 
 // Bloco de ações de inicialização da página
+
+recoverySavedTaskList();
