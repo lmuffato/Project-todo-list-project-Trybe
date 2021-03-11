0a22f68baa12be4c2eb3ddf0421e0f74d5ad1a8a
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-11 17:11:58 -0300

    requisito 7 - coloca cor na tarefa

--- script.js
+++ script.js
@@ -2,24 +2,34 @@
 const button = document.querySelector('#criar-tarefa');
 const boxText = document.querySelector('#texto-tarefa');
 
-// função muda a cor de fundo da tarefa ao ser clicada
-function changeColor(e) {
-  e.target.className = 'tarefa-selecionada';
-  e.target.style.backgroundColor = 'red';
-}
 // adiciona nova tarefa
 function criaTarefa() {
-  let item = document.createElement('li');
-  document.querySelector('#lista-tarefas').appendChild(item);
+  const item = document.createElement('li');
+  item.className = 'itemList';
   item.innerText = boxText.value;
+  document.querySelector('#lista-tarefas').appendChild(item);
   boxText.value = '';
-  // resetColor(item);
-  // changeColor(item);
-  item.addEventListener('click', changeColor);
 }
 
 button.addEventListener('click', criaTarefa);
 
-/* function resetColor() {
-  if()
-} */
+function resetColor() {
+  const currentItem = document.getElementsByClassName('itemList');
+  for (let index = 0; index < currentItem.length; index += 1) {
+    if (currentItem[index].style.backgroundColor === 'red') {
+      currentItem[index].style.backgroundColor = 'white';
+    }
+  }
+}
+
+
+// muda a cor de fundo da tarefa ao ser clicada
+function changeColor(e) {
+  // e.target.className = 'tarefa-selecionada';
+  e.target.style.backgroundColor = 'red';
+  console.log(e.target);
+}
+
+document.getElementById('lista-tarefas').addEventListener('click', resetColor);
+
+document.getElementById('lista-tarefas').addEventListener('click', changeColor);
