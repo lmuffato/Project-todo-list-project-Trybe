788f75d54c434cb0b6bc778103af33e155cda0b0
luanlsr <luanfswd@gmail.com>
2021-03-11 00:22:26 -0300

    finzalidado requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let input = document.querySelector('#texto-tarefa');
+const input = document.querySelector('#texto-tarefa');
 const createAssignment = document.querySelector('#criar-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
+const clear = document.querySelector('#remover-finalizados');
 
 createAssignment.addEventListener('click', () => {
   const li = document.createElement('li');
@@ -17,14 +18,20 @@ todoList.addEventListener('click', (e) => {
   for (const liC of liColor) {
     liC.classList.remove('gray')
   }
-      selectedItem.classList.add('gray');
+      selectedItem.classList.toggle('gray');
 }); 
 
 todoList.addEventListener('dblclick', (e) => {
   const selectedItem = e.target;
-  const liColor = document.querySelectorAll('#lista-tarefas li');
   
-  selectedItem.classList.toggle('completed');
+  selectedItem.classList.add('completed');
 }); 
 
+//código feito baseado no code Review do Lucas Pedroso.
+clear.addEventListener('click', () => {
+  const listContainer = document.querySelectorAll('.completed');
+  for (let index = 0; index < listContainer.length; index += 1) {
+    listContainer[index].remove();
+  }
+});
 
