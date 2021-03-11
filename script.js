1d68819639721733de59f95dd14211c8356c545d
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-10 22:16:44 -0300

    [REQUISITO][13]

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const btnRemoveAll = document.getElementById('apaga-tudo');
 const btnRemoveCompleted = document.getElementById('remover-finalizados');
 const btnSaveList = document.getElementById('salvar-tarefas');
 const btnRemoveSelected = document.getElementById('remover-selecionado');
+const btnMoveUp = document.getElementById('mover-cima');
+const btnMoveDown = document.getElementById('mover-baixo');
 const idName = 'selected-task';
 const loadList = () => {
+  btnSaveList.addEventListener('click', () => {
+    localStorage.setItem('lists', oListTask.innerHTML);
+  });
+
   oListTask.innerHTML = localStorage.getItem('lists');
 };
 
@@ -42,12 +48,36 @@ btnRemoveCompleted.addEventListener('click', () => {
   document.querySelectorAll('.completed').forEach((e) => e.remove());
 });
 
-btnSaveList.addEventListener('click', () => {
-  localStorage.setItem('lists', oListTask.innerHTML);
-});
-
 btnRemoveSelected.addEventListener('click', () => {
   oListTask.removeChild(document.getElementById(idName));
+});
+
+btnMoveUp.addEventListener('click', () => {
+  const listSelected = document.getElementById(idName);
+  const eList = document.getElementsByTagName('li');
+
+  if (eList[0] === listSelected) return;
+
+  for (let index = 0; index < eList.length; index += 1) {
+    if (eList[index] === listSelected) {
+      listSelected.outerHTML = eList[index - 1].outerHTML;
+      eList[index - 1].outerHTML = listSelected.outerHTML;
+    }
+  }
+});
+
+btnMoveDown.addEventListener('click', () => {
+  const listSelected = document.getElementById(idName);
+  const eList = document.getElementsByTagName('li');
+
+  if (eList[eList.length - 1] === listSelected) return;
+
+  for (let index = 0; index < eList.length; index += 1) {
+    if (eList[index] === listSelected) {
+      listSelected.outerHTML = eList[index + 1].outerHTML;
+      eList[index + 1].outerHTML = listSelected.outerHTML;
+    }
+  }
 });
 
 loadList();
@@ -56,4 +86,5 @@ loadList();
 REFERÊNCIAS:
 https://www.w3schools.com/jsref/prop_element_classlist.asp
 https://stackoverflow.com/questions/10842471/how-to-remove-all-elements-of-a-certain-class-from-the-dom
+https://stackoverflow.com/questions/1763479/how-to-get-the-html-for-a-dom-element-in-javascript
 */
