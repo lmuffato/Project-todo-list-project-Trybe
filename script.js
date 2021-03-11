18045637fbbf1f9601c1020b451119ac97b42f2c
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-11 19:21:45 -0300

    otimiza funçao 'resetColor'

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const boxText = document.querySelector('#texto-tarefa');
 
 // muda a cor de fundo da tarefa ao ser clicada
 function changeColor(e) {
-  // e.target.className = 'tarefa-selecionada';
   e.target.style.backgroundColor = 'rgb(128, 128, 128)';
   console.log(e.target);
 }
 
 // retira a cor cinza
 function resetColor() {
-  const currentItem = document.getElementsByClassName('itemList');
+  const itens = document.getElementsByClassName('itemList');
+  for (let x = 0; x < itens.length; x += 1) {
+    itens[x].style.backgroundColor = 'white';
+  }
+}
+/*   const currentItem = document.getElementsByClassName('itemList');
   for (let index = 0; index < currentItem.length; index += 1) {
     if (currentItem[index].style.backgroundColor === 'rgb(128, 128, 128)') {
       currentItem[index].style.backgroundColor = 'white';
     }
   }
-}
+} */
 
 // adiciona nova tarefa
 function criaTarefa() {
