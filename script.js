7be2ea01f5c4506a87a88fc067df1c9d5bf6f554
Felipe Muller <felipeavmuller@gmail.com>
2021-03-11 00:46:52 -0300

    requisito 9

--- script.js
+++ script.js
@@ -29,15 +29,36 @@ selectTask();
 
 function colorTask() {
   let taskList = document.querySelector('#lista-tarefas');
-  taskList.addEventListener('click', function() {
-    let allLines = document.querySelectorAll('li');
-    for (let index = 0; index < allLines.length; index += 1) {
-      if (allLines[index].className === 'task selected') {
-        allLines[index].style.backgroundColor = 'rgb(128,128,128)';
+  taskList.addEventListener('click', function(event) {
+    let tasksCounter = document.getElementsByClassName('task');
+    for (let index = 0; index < tasksCounter.length; index += 1) {
+      if (tasksCounter[index].classList.contains('selected')) {
+        tasksCounter[index].style.backgroundColor = 'rgb(128,128,128)';
       } else {
-        allLines[index].style.backgroundColor = 'white';
+        tasksCounter[index].style.backgroundColor = 'white';
       }
     }
   })
 }
 colorTask();
+
+function completeTask() {
+  let taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('dblclick', function(event) {
+    if (event.target.classList.contains('task')) {
+      if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+      } else {
+        event.target.classList.add('completed');
+      }
+    }
+  })
+  let tasksCounter = document.getElementsByClassName('task');
+    for (let index = 0; index < tasksCounter.length; index += 1) {
+      if (tasksCounter[index].classList.contains('completed')) {
+        tasksCounter[index].style.textDecoration = 'line-through solid rgb(0, 0, 0)'
+      }
+    }
+}
+completeTask();
+
