80e14c6e862d29f41deaccc8833da45e6cdbc059
Elisa <elisafranca.carvalho@gmail.com>
2021-03-11 18:10:20 -0300

    Requisito 9: Risca item da lista com o clique duplo

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
+const listItem = document.getElementsByTagName('li');
+
 function getInput() {
   const inputValue = document.getElementById('texto-tarefa').value;
   return inputValue;
 }
 
 function removeBackground() {
-  const listItem = document.querySelectorAll('.selected');
-  Array.from(listItem).forEach((item) => {
+  const listRmItem = document.querySelectorAll('.selected');
+
+  Array.from(listRmItem).forEach((item) => {
     item.classList.remove('selected');
   });
 }
 
 function clickListItem() {
-  const clickItem = document.getElementsByTagName('li');
-
-  Array.from(clickItem).forEach((item) => {
+  Array.from(listItem).forEach((item) => {
     item.addEventListener('click', (event) => {
       const element = event.target;
       removeBackground();
       element.classList.add('selected');
+    });
+  });
+}
+
+function dblClickListItem() {
+  Array.from(listItem).forEach((item) => {
+    item.addEventListener('dblclick', (event) => {
+      const element = event.target;
+      //  Para saber mais sobre toggle acessei o seguinte link:
+      //  https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+      element.classList.toggle('selected-dblClick');
     });
   });
 }
@@ -28,6 +40,7 @@ function addListItem() {
   listElement.innerHTML = getInput();
   ol.appendChild(listElement);
   clickListItem();
+  dblClickListItem();
 }
 
 function clickButton() {
