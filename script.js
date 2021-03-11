88e767d9539cf7351511581733c24209070dd12c
William Willers <w.willers@outlook.com>
2021-03-11 13:05:45 -0400

    Requisito 7 setando classe pra lista

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+const list = document.getElementById('lista-tarefas');
+
 function clearInputField() {
   const clearContent = document.getElementById('texto-tarefa');
   clearContent.value = '';
 }
 
 function insertToDo() {
-  const list = document.getElementById('lista-tarefas');
   const content = document.getElementById('texto-tarefa').value;
   const node = document.createElement('li');
+  // Refatorar condição
   if (content != null) {
     node.innerHTML = content;
     list.appendChild(node);
   }
   clearInputField();
 }
+
+function setBackgroundColor() {
+  list.addEventListener('click', (event) => {
+    const childrenList = list.children;
+    for (let index = 0; index < childrenList.length; index += 1) {
+      childrenList[index].classList.remove('selectedItem');
+    }
+    event.target.classList.add('selectedItem');
+    console.log(event.target);
+  });
+}
+
+window.onload = () => {
+  setBackgroundColor();
+};
