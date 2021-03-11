e0b51ad96799b493c250d4fd1f315954bc1bf990
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-03-11 20:16:17 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-function createListItem() {
-  document.getElementById('criar-tarefa').addEventListener('click', () => {
-    const listItem = document.getElementById('texto-tarefa').value;
-    const node = document.createElement('li');
-    const textNode = document.createTextNode(listItem);
-    node.appendChild(textNode);
-    document.getElementById('lista-tarefas').appendChild(node);
-  });
-}
 function clearInput() {
   document.getElementById('criar-tarefa').addEventListener('click', () => {
     document.getElementById('texto-tarefa').value = '';
   });
 }
-window.onload = () => {
+
+function createListItem() {
+  document.getElementById('criar-tarefa').addEventListener('click', () => {
+    const texto = document.getElementById('texto-tarefa').value;
+    const node = document.createElement('li');
+    node.className = 'tarefa';
+    const textNode = document.createTextNode(texto);
+    node.appendChild(textNode);
+    document.getElementById('lista-tarefas').appendChild(node);
+    const listItem = document.getElementsByTagName('li'); 
+    for (let index = 0; index < listItem.length; index += 1) {
+      listItem[index].addEventListener('click', () => {
+        listItem[index].style.backgroundColor = 'red';
+        listItem[index].classList.add('selected');
+        clearColor();
+      });
+    }
+  });
+}
+function clearColor() {
+
+}
+window.onload = () => { 
   createListItem();
+
   clearInput();
 };
