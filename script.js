bc7ba599f507003c9bb000a68d69b75bf869ac77
andy silva <andssilva.23@gmail.com>
2021-03-11 11:23:48 -0400

    requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Requisito 2
 const paragraph = document.getElementById('funcionamento');
 paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
+// Requisito 5
+const taskList = document.getElementById('lista-tarefas');
+const buttonInsertItem = document.getElementById('criar-tarefa');
+buttonInsertItem.innerText = 'Inserir';
+const inputList = document.getElementById('texto-tarefa');
+
+buttonInsertItem.addEventListener('click', () => {
+  const listItem = document.createElement('li');
+  listItem.innerText = inputList.value;
+  taskList.appendChild(listItem);
+});
