016168892db0536234e13d7cdb74a565139fd6a6
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-11 19:13:09 -0300

    Requisito 5 - Js ESlint revisado(1)

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Requisito 5 - Clicando no Botão e adicionando uma nova lista
 function newList() {
-  let getTextoTarefa = document.querySelector('#texto-tarefa');
-  let getCriarTarefa = document.querySelector('#criar-tarefa');
-  let getListaTarefa = document.querySelector('#lista-tarefas');
+  const getTextoTarefa = document.querySelector('#texto-tarefa');
+  const getCriarTarefa = document.querySelector('#criar-tarefa');
+  const getListaTarefa = document.querySelector('#lista-tarefas');
 
   getCriarTarefa.addEventListener('click', () => {
-   if (getTextoTarefa.value.length > 0) {
-    let novaLista = document.createElement('li');
-    novaLista.innerText = getTextoTarefa.value;
+    if (getTextoTarefa.value.length > 0) {
+      let novaLista = document.createElement('li');
+      novaLista.innerText = getTextoTarefa.value;
 
-    getListaTarefa.appendChild(novaLista);
-    getTextoTarefa.value = '';
+      getListaTarefa.appendChild(novaLista);
+      getTextoTarefa.value = '';
     }
   });
 }
