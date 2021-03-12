43ce095a9a6f4e4027fdd76d9d8204eab1661fad
lucas-paz <lucas.hpaz@outlook.com>
2021-03-12 11:39:49 -0300

    Iniciando desafio 7

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-// Desafio 4
-let createTask = document.getElementById('criar-tarefa');
+// Desafio 5
+const createTask = document.getElementById('criar-tarefa');
 createTask.addEventListener('click', addTask);
 
 function addTask() {
     // Captura informação de texto
-    let actualizeText = document.getElementById('texto-tarefa');
-    let saveText = actualizeText.value;
+    const actualizeText = document.getElementById('texto-tarefa');
+    const saveText = actualizeText.value;
     // Cria o elemento da lista
-    let accessList = document.getElementById('lista-tarefas');
-    let creatListElement = document.createElement('li');
+    const accessList = document.getElementById('lista-tarefas');
+    const creatListElement = document.createElement('li');
     // preenche com o texto
     creatListElement.innerHTML = saveText;
-    let print = accessList.appendChild(creatListElement);
+    const print = accessList.appendChild(creatListElement);
     // Apaga a entrada na caixa de digitação
     actualizeText.value = '';
     return print;
+}
+
+//Desafio 7
+function selectItem (position) {
+    let tasks = document.getElementsByTagName('li');
+    tasks[position].style.background = 'rgb(128,128,128)';
+}
+
+for (let index7 = 0; index7 < tasks.length; index7 +=1){
+    document.getElementsByTagName('li')[index7].addEventListener('click', selectItem(index7))
 }
