d0478975fc9d6e9e52eaf09aeef03e6f069fd57e
Rafael Ribeiro <rafittu@outlook.com>
2021-03-12 13:41:48 -0300

    requisito 8

--- script.js
+++ script.js
@@ -2,38 +2,23 @@ document.getElementById('criar-tarefa').addEventListener('click', () => {
   const btnContainer = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
   const task = document.createTextNode(btnContainer);
-
   li.appendChild(task);
   document.getElementById('lista-tarefas').appendChild(li);
   document.getElementById('texto-tarefa').value = '';
 
   li.addEventListener('click', function (event) {
-    const colorTask = event.target;
-    colorTask.style.backgroundColor = 'rgb(128, 128, 128)';
+    let taskItem = document.getElementById('lista-tarefas').childNodes;
+    let taskColor = event.target;
+    for (let index = 0; index < taskItem.length; index += 1) {
+      if (taskItem[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+        taskItem[index].style.backgroundColor = 'white';
+      }
+    }
+    taskColor.style.backgroundColor = 'rgb(128, 128, 128)';
   });
-  // li.addEventListener('click', function (event) {
-  //   let taskItem = document.getElementById('lista-tarefas').childNodes;
-  //   let taskColor = event.target;
-
-  //   for (let index = 0; index < taskItem.length; index += 1) {
-  //     if (taskItem.style.backgroundColor === 'rgb(128, 128, 128)') {
-  //       taskItem.style.backgroundColor = 'white';
-  //     } else {
-  //       taskColor.style.backgroundColor = 'rgb(128, 128, 128)';
-  //     }
-  //   }
-  // });
 
   li.addEventListener('dblclick', () => {
     li.classList.toggle('completed');
-  });
-
-  const clearCompletedTasks = document.querySelector('#remover-finalizados');
-  clearCompletedTasks.addEventListener('click', () => {
-    if (li.className === 'completed') {
-      li.className = '';
-      li.innerHTML = '';
-    }
   });
 });
 
@@ -42,14 +27,3 @@ clearBtn.addEventListener('click', () => {
   const taskList = document.querySelector('#lista-tarefas');
   taskList.innerHTML = '';
 });
-
-// const clearCompletedTasks = document.querySelector('#remover-finalizados');
-// clearCompletedTasks.addEventListener('click', () => {
-//   const taskList = document.querySelector('#lista-tarefas').childNodes;
-
-//   for (let index = 0; index < taskList.length; index += 1) {
-//     if (taskList.className === 'completed') {
-//       taskList.classList.remove('completed');
-//     }
-//   }
-// });
