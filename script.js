6a2430c6ed3fe08a5ecc381335693c51349eef0a
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-12 07:54:50 -0300

    tentando requesito 5

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('lets start');
+/* informações referentes ao input e ao button foram retiradas do
+https://www.w3schools.com/html/html_form_input_types.asp
+https://www.w3schools.com/jsref/dom_obj_pushbutton.asp */
+// vamos criar o button do requesito 5
+const myButton = document.createElement('button');
+myButton.innerText = 'adicionar';
+myButton.type = 'reset';
+myButton.id = 'criar-tarefa';
+document.getElementsByTagName('form')[0].appendChild(myButton);
+// após criação do myButton e atribuir input ao ol
+/* utilização da propriedade value do input
+fonte : https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/text */
+function receiveTask() {
+  const taskToDo = document.createElement('li');
+  taskToDo.innerText = document.getElementById('texto-tarefa').value;
+  document.getElementById('lista-tarefas').appendChild(taskToDo);
+}
+myButton.addEventListener('click', receiveTask);
