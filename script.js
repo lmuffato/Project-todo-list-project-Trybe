ee40e61194aba0f97c5f4543e18e2809e94d6633
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-12 11:29:00 -0300

    requisito 8

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 const elementsList = document.querySelector('.elementList');
 
+
+function clean (){
+    const classColor = document.querySelectorAll('.color');
+    for (let i = 0; i < classColor.length; i += 1) {
+    classColor[i].classList.remove('color');
+    }
+} 
 function main () {
     button.addEventListener('click',() => {
-    let listItens = document.createElement('li');
-    listItens.innerText = input.value;
-    list.appendChild(listItens);
+    let liItem = document.createElement('li');
+    liItem.innerText = input.value;
+    
+    liItem.addEventListener('click', () => {
+        clean();
+        liItem.className = 'color';
+        });
+
+    list.appendChild(liItem);
     input.value = '';
     
-        let list2 = document.querySelectorAll('li');
+        /*  let list2 = document.querySelectorAll('li');
         for (let i = 0; i < list2.length; i += 1){
-            list2[i].addEventListener('click', () => {
-            clean();
-            list2[i].className = 'color';
-            });
-        }
-        const classColor = document.querySelectorAll('.color')
-        function clean (){
-            for (let i = 0; i < classColor.length; i += 1) {
-            classColor[i].classList.remove('color');
-            }
-        }
+           
+        } */
+        
     
     });
     
