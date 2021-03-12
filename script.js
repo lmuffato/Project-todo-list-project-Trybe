91fb127bf1684e5fba60ecfc4b0f6c23aedb708a
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-12 09:37:15 -0300

    Requisito 7 - Mudando o Background-Color para Cinza

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 // Requisito 5 e 6 - Clicando no Botão e adicionando uma nova lista ordenada
 function newList() {
-  const getTextoTarefa = document.querySelector('#texto-tarefa');
-  const getCriarTarefa = document.querySelector('#criar-tarefa');
-  const getListaTarefa = document.querySelector('#lista-tarefas');
+    const getTextoTarefa = document.querySelector('#texto-tarefa');
+    const getCriarTarefa = document.querySelector('#criar-tarefa');
+    const getListaTarefa = document.querySelector('#lista-tarefas');
 
-  getCriarTarefa.addEventListener('click', () => {
-    if (getTextoTarefa.value.length > 0) {
-      const novaLista = document.createElement('li');
-      novaLista.className = 'item';
-      novaLista.innerText = getTextoTarefa.value;
-
-      getListaTarefa.appendChild(novaLista);
-      getTextoTarefa.value = '';
-    }
-  });
+    getCriarTarefa.addEventListener('click', () => {
+        if (getTextoTarefa.value.length > 0) {
+            const novaLista = document.createElement('li');
+            novaLista.innerText = getTextoTarefa.value;
+            novaLista.id = 'item';
+            getListaTarefa.appendChild(novaLista);
+            getTextoTarefa.value = '';
+        }
+    });
 }
 newList();
 
-function changingBackground() {
-  let getListasTarefas = document.querySelector('#listas-tarefas');
-  let getClassItem = document.querySelectorAll('.item');
-  let backgroundColor = 'rgb(255, 255, 255)';
-  let setColor = 'rgb(128, 128, 128)';
+// Requisito 7
+// Exerício realizado com a ajuda do código do Guilherme Lira
+// Link: https://github.com/tryber/sd-010-a-project-todo-list/blob/bb5985c0ec0c474bfb90708581a880b9822623a9/script.js
+function changingBackgroundGreen() {
+    const getIDListaTarefas = document.querySelector('#lista-tarefas');
 
-  getListasTarefas.addEventListener('click', () => {
-    for (let index = 0; index < getClassItem.length; index += 1) {
-        if (getClassItem[index].style.backgroundColor === setColor) {
-            getClassItem[index].style.backgroundColor = backgroundColor;
-        } else {
-            getClassItem[index].style.backgroundColor = setColor;
-        }
-    }
-  });
+    getIDListaTarefas.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
 }
-changingBackground();
+changingBackgroundGreen();
