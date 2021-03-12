f4006f31064ccf69ca9f450aec4d629af84476ca
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-12 12:08:25 -0300

    9 ok

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 //Criação das Funções:
 
 function completedTask(event) {
+  if (event.target.className === 'listClass') {
     event.target.classList.add('completed');
-    console.log('completed task entrou')
+  } else if (event.target.className === 'listClass completed') {
+    event.target.className = 'listClass';
   }
+}
 
 function setColor(event) {
-let fullList = document.getElementsByClassName('listClass');
-for (let index = 0; index < fullList.length; index += 1) {
+  let fullList = document.getElementsByClassName('listClass');
+  for (let index = 0; index < fullList.length; index += 1) {
     fullList[index].style.backgroundColor = 'white';
     console.log('pinta li');
-}
-event.target.style.backgroundColor = 'rgb(128,128,128)';    
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
 function createTask() {
-  console.log('create task ok')
-  let inputText = document.getElementById('texto-tarefa').value;
+  console.log('create task ok');
+  const inputText = document.getElementById('texto-tarefa').value;
   if (inputText === '') {
     alert('Digite algo!');
-} else {
-  let createList = document.createElement('li');
-  createList.innerHTML = inputText;
-  createList.className = 'listClass'
-  createList.addEventListener('dbclick', completedTask);
-  createList.addEventListener('click', setColor);
-  document.getElementById('lista-tarefas').appendChild(createList);
-}
+  } else {
+    let createList = document.createElement('li');
+    createList.innerHTML = inputText;
+    createList.className = 'listClass';
+    createList.addEventListener('dblclick', completedTask);
+    createList.addEventListener('click', setColor);
+    document.getElementById('lista-tarefas').appendChild(createList);
+  }
 }
 
 //Programação do Botao
