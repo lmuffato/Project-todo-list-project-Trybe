b820d4055d0f3d9eb7e9013877944e2cb1c71c5d
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-11 21:42:47 -0300

    Requisito 7 - Ao clicar sobre a lista, sua cor de fundo muda

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function newList() {
   getCriarTarefa.addEventListener('click', () => {
     if (getTextoTarefa.value.length > 0) {
       const novaLista = document.createElement('li');
+      novaLista.className = 'item';
       novaLista.innerText = getTextoTarefa.value;
 
       getListaTarefa.appendChild(novaLista);
@@ -15,3 +16,21 @@ function newList() {
   });
 }
 newList();
+
+function changingBackground() {
+  let getListasTarefas = document.querySelector('#listas-tarefas');
+  let getClassItem = document.querySelectorAll('.item');
+  let backgroundColor = 'rgb(255, 255, 255)';
+  let setColor = 'rgb(128, 128, 128)';
+
+  getListasTarefas.addEventListener('click', () => {
+    for (let index = 0; index < getClassItem.length; index += 1) {
+        if (getClassItem[index].style.backgroundColor === setColor) {
+            getClassItem[index].style.backgroundColor = backgroundColor;
+        } else {
+            getClassItem[index].style.backgroundColor = setColor;
+        }
+    }
+  });
+}
+changingBackground();
