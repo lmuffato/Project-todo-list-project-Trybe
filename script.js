4303326ec6c3300379737e4a90f62e03252e4e1e
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-12 10:51:56 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
+const elementsList = document.querySelector('.elementList');
 
-button.addEventListener('click',() => {
+function main () {
+    button.addEventListener('click',() => {
     let listItens = document.createElement('li');
-    listItens.className = 'listTask'
-    listItens.innerText = document.querySelector('#texto-tarefa').value
+    listItens.innerText = input.value;
     list.appendChild(listItens);
-    document.querySelector('#texto-tarefa').value = ''
-});
-
-
-
+    input.value = '';
     
+        let list2 = document.querySelectorAll('li');
+        for (let i = 0; i < list2.length; i += 1){
+            list2[i].addEventListener('click', () => {
+            clean();
+            list2[i].className = 'color';
+            });
+        }
+        const classColor = document.querySelectorAll('.color')
+        function clean (){
+            for (let i = 0; i < classColor.length; i += 1) {
+            classColor[i].classList.remove('color');
+            }
+        }
+    
+    });
+    
+}
+main();
+    
+ 
