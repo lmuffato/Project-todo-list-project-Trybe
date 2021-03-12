517130355f7003984a545fe10dbdcfdbf1823ebc
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-12 00:15:22 -0300

    Requsitos 6, 7 e 8 - Lista em ordem de criacao, cor de fundo ao clicar no item e ao clicar em outro item seleciona apenas um item

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Requesito 5
+//Requesito 5, 6, 7 e 8
 let button = document.getElementById('criar-tarefa');
 button.addEventListener('click', newItem);
 
@@ -6,7 +6,20 @@ function newItem () {
   let textTask = document.getElementById('texto-tarefa');
   let addList = document.getElementById('lista-tarefas').appendChild(document.createElement('li'));
   addList.innerHTML = textTask.value;
-  textTask.value = ' ';
-}
+  textTask.value = '';
 
-//Requesito 6
+  let listTask = document.getElementsByTagName('li');
+
+  for (let index = 0; index < listTask.length; index += 1) {
+    const listAll = listTask[index];
+    listAll.addEventListener('click', paintItem);
+  }
+
+  function paintItem (e) {
+    for (let index = 0; index < listTask.length; index += 1) {
+      const removeClass = listTask[index];
+      removeClass.classList.remove('add-background');
+    }
+    e.target.classList.add('add-background');
+  }
+}
