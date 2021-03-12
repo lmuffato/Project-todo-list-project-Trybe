f8548bd5be158c75be6cd068c022348cd3bac9ba
Eric <ericff07@gmail.com>
2021-03-12 12:23:58 -0300

    Adiciona o passo 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Clicar no botão adiciona o item à lista
 function newTask () {
     let newItem = document.createElement('li');
     let itemValue = document.getElementById('texto-tarefa').value;
@@ -5,11 +6,23 @@ function newTask () {
         alert('Por favor, digite um valor para adicionar à sua lista.')
     }   else {
         newItem.innerText = itemValue;
+        newItem.classList.add('back');
+        newItem.style.width = '200px';
         document.getElementById('lista-tarefas').appendChild(newItem);
         //itemValue = '';
         document.getElementById('texto-tarefa').value = '';
+    }
+    let listItem = document.querySelectorAll('.back')
+    for (let index = 0; index < listItem.length; index += 1) {
+        listItem[index].addEventListener('click', function(e) {
+          //listItem[index].classList.add('selected');
+         e.target.style.backgroundColor = 'rgb(128,128,128)';
+          e.target.classList.add('selected');
+      })
     }
 }
 
 let btnAddTask = document.getElementById('criar-tarefa');
 btnAddTask.addEventListener('click', newTask);
+
+// Clicar no item na lista muda sua cor de fundo
