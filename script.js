7f5dd34a4fc8cff4ae041fa602d3fe40dfac73b8
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-11 21:36:14 -0300

    Décimo segundo requisito

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// window.onload = function () {
-//   getAll();
-// };
+const ol = document.getElementById('lista-tarefas');
+const li = document.querySelectorAll('.itens');
 
-function changeColor(item) {
+function changeColor(itens) {
   const colorize = document.getElementsByClassName('itens');
-  const targe = item.target;
+  const select = itens.target;
   for (let index = 0; index < colorize.length; index += 1) {
-    if (colorize[index] === targe) {
+    if (colorize[index] === select) {
       colorize[index].classList.add('selected');
       colorize[index].style.backgroundColor = 'rgb(128, 128, 128)';
     } else {
@@ -16,16 +15,15 @@ function changeColor(item) {
   }
 }
 
-function lineTrhough(item) {
-  if (item.target.classList.contains('completed')) {
-    item.target.classList.toggle('completed');
+function lineTrhough(itens) {
+  if (itens.target.classList.contains('completed')) {
+    itens.target.classList.toggle('completed');
   } else {
-    item.target.classList.toggle('completed');
+    itens.target.classList.toggle('completed');
   }
 }
 
 function resetInput() {
-  const ol = document.getElementById('lista-tarefas');
   const item = document.createElement('li');
   item.className = 'itens';
   item.innerText = document.getElementById('texto-tarefa').value;
@@ -38,13 +36,10 @@ function resetInput() {
 }
 
 function cleanList() {
-  const ol = document.getElementById('lista-tarefas');
   ol.innerHTML = '';
 }
 
 function removeFinished() {
-  const li = document.querySelectorAll('.itens');
-  const ol = document.querySelector('#lista-tarefas');
   for (let index = 0; index < li.length; index += 1) {
     if (li[index].classList.contains('completed')) {
       ol.removeChild(li[index]);
@@ -52,19 +47,15 @@ function removeFinished() {
   }
 }
 
-// function saveAll() {
-//   const li = document.querySelector('#lista-tarefas').innerText;
-//   let tasks = {};
-//   for (let index = 0; index < li.length; index += 1) {
-//     tasks[index] = ('task', `${li[index]}`);
-//   }
-//   console.log(tasks);
-//   localStorage.setItem('task', JSON.stringify(tasks));
-// }
+function saveAll() {
+  localStorage.setItem('task', ol.innerHTML);
+}
 
-// function getAll() {
-//   console.log(JSON.parse(localStorage.getItem('tasks')));
-// }
+function getAll() {
+  ol.innerHTML = localStorage.getItem('task');
+  ol.addEventListener('dblclick', lineTrhough);
+  ol.addEventListener('click', changeColor);
+}
 
 // function moveUp() {
 //   const li = document.getElementsByClassName('itens');
@@ -91,8 +82,8 @@ function removeFinished() {
 //   }
 // }
 
-const button = document.querySelector('#criar-tarefa');
-button.addEventListener('click', resetInput);
+const sendBtn = document.querySelector('#criar-tarefa');
+sendBtn.addEventListener('click', resetInput);
 
 const cleanButton = document.querySelector('#apaga-tudo');
 cleanButton.addEventListener('click', cleanList);
@@ -100,10 +91,13 @@ cleanButton.addEventListener('click', cleanList);
 const removeFinishedBtn = document.querySelector('#remover-finalizados');
 removeFinishedBtn.addEventListener('click', removeFinished);
 
-// const saveAllBtn = document.querySelector('#salvar-tarefas');
-// saveAllBtn.addEventListener('click', saveAll);
+const saveAllBtn = document.querySelector('#salvar-tarefas');
+saveAllBtn.addEventListener('click', saveAll);
 
 // const upBtn = document.querySelector('#mover-cima');
 // upBtn.addEventListener('click', moveUp);
 
 // const downBtn = document.querySelector('#mover-baixo');
+window.onload = () => {
+  getAll();
+};
