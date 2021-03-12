c295d9c2904d934675ba6e48f5392f3761aed6f9
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-12 13:05:18 -0300

    ajuste identacao

--- script.js
+++ script.js
@@ -4,34 +4,28 @@ const list = document.querySelector('#lista-tarefas');
 const elementsList = document.querySelector('.elementList');
 
 
-function clean (){
+function clean() {
     const classColor = document.querySelectorAll('.color');
     for (let i = 0; i < classColor.length; i += 1) {
-    classColor[i].classList.remove('color');
+        classColor[i].classList.remove('color');
     }
-} 
-function main () {
-    button.addEventListener('click',() => {
-    let liItem = document.createElement('li');
-    liItem.innerText = input.value;
-    
-    liItem.addEventListener('click', () => {
-        clean();
-        liItem.className = 'color';
-        });
+}
+function main() {
+    button.addEventListener('click', () => {
+        let liItem = document.createElement('li');
+        liItem.innerText = input.value;
+        liItem.addEventListener('click', () => {
+            clean();
+            liItem.className = 'color';
 
-    list.appendChild(liItem);
-    input.value = '';
-    
-        /*  let list2 = document.querySelectorAll('li');
-        for (let i = 0; i < list2.length; i += 1){
-           
-        } */
-        
-    
+        });
+        liItem.addEventListener('dblclick', () => {
+            liItem.className = 'completed';
+        })
+
+        list.appendChild(liItem);
+        input.value = '';
     });
-    
 }
 main();
-    
- 
+
