fe15e24232d9576ab4fa2187b5a5f171446d5cd0
igson <igson660@gmail.com>
2021-03-12 20:15:01 -0500

    quesito 07

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ function creatTask() {
   const list = document.createElement('li');
   list.innerText = task.value;
   task.value = '';
+  list.className = 'list-item'
   taskList.appendChild(list);
 }
 creatTaskButton.addEventListener('click', creatTask);
+
+function addBackgroundColor() {
+    const itens = document.getElementsByClassName('li');
+    taskList.addEventListener('click', (event) => {
+    const backgroundColor = event.target;
+    for (index = 0; index < itens.length; index += 1) {
+      if (itens[index].className === 'selected') {
+        itens[index].classList.remove('selected');
+      }
+    }
+    backgroundColor.classList.add('selected');
+  });
+}
+addBackgroundColor()
+
