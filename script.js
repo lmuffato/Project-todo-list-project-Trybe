04c91b975008a2524ec04d2a4d5d179e26bb7e88
guilhermemd <guilherme.md@gmail.com>
2021-03-13 17:24:18 -0300

    requisito 08

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskBtn = document.getElementById('criar-tarefa');
 const inputPlace = document.getElementById('texto-tarefa');
 const listTask = document.getElementById('lista-tarefas');
+// fontes https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values - https://www.youtube.com/watch?v=JBdyASuhq1c além de olhar muuuuitos projetos de colegas
 
 function addTask() {
   taskBtn.addEventListener('click', () => {
@@ -12,3 +13,14 @@ function addTask() {
 }
 
 addTask();
+
+function grayLi() {
+  listTask.addEventListener('click', (event) => {
+    for (let index; index < listTask.children[index]; index += 0) {
+      listTask.children[index].style.background = '';
+    }
+    event.target.style.background = 'rgb(128, 128, 128)';
+  });
+}
+
+grayLi();
