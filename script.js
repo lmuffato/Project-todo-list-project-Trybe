12e5c77d289ef6148b6f36ba4afdd06d0bafedd0
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-12 21:03:23 -0300

    Requisito 7 - ESlint revisado(3)

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function newList() {
 
   getCriarTarefa.addEventListener('click', () => {
     if (getTextoTarefa.value.length > 0) {
-      let novaLista = document.createElement('li');
+      const novaLista = document.createElement('li');
       novaLista.innerText = getTextoTarefa.value;
       novaLista.className = 'item';
       getListaTarefa.appendChild(novaLista);
@@ -17,15 +17,15 @@ function newList() {
 newList();
 
 // Requisito 7
-
+// Exercício feito com a ajuda do código Lucas Andrade
+// Link: https://github.com/tryber/sd-010-a-project-todo-list/blob/cdc6f5084c225c6852fcfe3e5acd1ac6269c540f/script.js
 function changingBackground(event) {
-  let eventTarget = event.target;
+  const eventTarget = event.target;
   eventTarget.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 function eventchangeBackground() {
   const getListaTarefa = document.getElementById('lista-tarefas');
-
   getListaTarefa.addEventListener('click',changingBackground);
 }
 eventchangeBackground();
