0922e792a77bc6f94a9ede31c5ee0cfcda8178dc
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-13 14:23:19 -0300

    requesito 8 com clean do lint

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ myButton.type = 'reset';
 myButton.id = 'criar-tarefa';
 document.getElementsByTagName('form')[0].appendChild(myButton);
 // mudando o backgroundColor dos li
+/* paro o requsito 8, houve a necessidade de mudar todos o back para white
+e assim colorir o targey para grey */
+// ta aqui por causa do chato do lint
 const listItens = document.getElementById('lista-tarefas').children;
 console.log(listItens);
+function allToWhite() {
+  for (let i = 0; i < listItens.length; i += 1) {
+    listItens[i].style.backgroundColor = 'white';
+  }
+}
 function changeToGrey(selectedItem) {
   const toGrey = selectedItem.target;
+  allToWhite();
   toGrey.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 // após criação do myButton e atribuir input ao ol
