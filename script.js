55fdf01182e72922af7e452060e0ea83a4d597e0
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-13 13:53:44 -0300

    requesito 7

--- script.js
+++ script.js
@@ -14,5 +14,15 @@ function receiveTask() {
   const taskToDo = document.createElement('li');
   taskToDo.innerText = document.getElementById('texto-tarefa').value;
   document.getElementById('lista-tarefas').appendChild(taskToDo);
+  taskToDo.addEventListener('click', changeToGrey);
 }
 myButton.addEventListener('click', receiveTask);
+// mudando o backgroundColor dos li 
+let listItens = document.getElementById('lista-tarefas').children;
+function changeToGrey(selectedItem) {
+  const toGrey = selectedItem.target;
+  toGrey.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+for(let i = 0; i < listItens.lenght; i += 1) {
+  listItens[i].addEventListener('click', changeToGrey, returnAllToWhite);
+}
