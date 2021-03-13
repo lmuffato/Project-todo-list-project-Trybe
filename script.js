5ae99fb95ceef25ecfa45166f28372ab05662d18
lucas-paz <lucas.hpaz@outlook.com>
2021-03-13 15:19:47 -0300

    Melhorias propostats pelo lint

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-// Desafio 5 a 8
+// Desafio 5 a 9
 function selectItem(eventoDeOrigem) {
-    let exclueOld = document.getElementsByClassName('selected');
-    if (exclueOld.length > 0) {
-        exclueOld[0].classList.remove('selected');
-        eventoDeOrigem.target.classList.add('selected');
-    } else {
-        eventoDeOrigem.target.classList.add('selected');
-    }
+  const exclueOld = document.getElementsByClassName('selected');
+  if (exclueOld.length > 0) {
+    exclueOld[0].classList.remove('selected');
+    eventoDeOrigem.target.classList.add('selected');
+  } else {
+    eventoDeOrigem.target.classList.add('selected');
+  }
+}
+
+function donetItem(eventoDeOrigem) {
+  eventoDeOrigem.target.classList.toggle('completed');
 }
 
 function addTask() {
-    // Captura informação de texto
-    const actualizeText = document.getElementById('texto-tarefa');
-    const saveText = actualizeText.value;
-    // Cria o elemento da lista
-    const accessList = document.getElementById('lista-tarefas');
-    const creatListElement = document.createElement('li');
-    // preenche com o texto
-    creatListElement.innerHTML = saveText;
-    const print = accessList.appendChild(creatListElement);
-    // Apaga a entrada na caixa de digitação
-    actualizeText.value = '';
-    // Adiciona o escutador de eventos
-    let currentLists = document.getElementsByTagName('li');
-    for (let index = 0; index < currentLists.length; index += 1) {
-        currentLists[index].addEventListener('click', selectItem);
-        currentLists[index].addEventListener('dblclick', donetItem);
-    }
+  // Captura informação de texto
+  const actualizeText = document.getElementById('texto-tarefa');
+  const saveText = actualizeText.value;
+  // Cria o elemento da lista
+  const accessList = document.getElementById('lista-tarefas');
+  const creatListElement = document.createElement('li');
+  // preenche com o texto
+  creatListElement.innerHTML = saveText;
+  const print = accessList.appendChild(creatListElement);
+  // Apaga a entrada na caixa de digitação
+  actualizeText.value = '';
+  // Adiciona o escutador de eventos
+  const currentLists = document.getElementsByTagName('li');
+  for (let index = 0; index < currentLists.length; index += 1) {
+    currentLists[index].addEventListener('click', selectItem);
+    currentLists[index].addEventListener('dblclick', donetItem);
+  }
+  return print;
 }
 
 const createTask = document.getElementById('criar-tarefa');
 createTask.addEventListener('click', addTask);
-
-//Desafio 9
-function donetItem(eventoDeOrigem) {
-    eventoDeOrigem.target.classList.toggle('completed');
-}
-
-
