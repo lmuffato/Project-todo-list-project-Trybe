614cf7a049cc4e0a4ad73059ceeb6af27c489ff8
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-13 15:37:39 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const inputText = document.getElementById('texto-tarefa');
+inputText.value = '';
 const botAdicionar = document.getElementById('criar-tarefa');
 const listaOl = document.getElementById('lista-tarefas');
+let item;
 
 function concatList() {
-  const item = document.createElement('li');
-  item.innerText = inputText.value;
-  listaOl.appendChild(item);
-  inputText.value = null;
+  if (inputText.value !== '') {
+    item = document.createElement('li');
+    item.innerText = inputText.value;
+    item.className = 'listElement';
+    listaOl.appendChild(item);
+    inputText.value = '';
+  }
 }
 
-function addIten() {
+function changeBackgroudColorElement(e) {
+  const li = document.querySelectorAll('.listElement');
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].style.backgroundColor = 'white'
+  }
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function addBackgroudColorInElement() {
+  const li = document.querySelectorAll('.listElement');
+  console.log(li);
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].addEventListener('click', changeBackgroudColorElement);
+  }
+}
+
+function clickBtnAdicionar() {
   botAdicionar.addEventListener('click', concatList);
+  botAdicionar.addEventListener('click', addBackgroudColorInElement);
 }
 
-addIten();
+clickBtnAdicionar();
