4db059715701970f5dce43b212f058bd510dbcc5
lucas-chave <lcchavesr150@gmail.com>
2021-03-13 02:07:46 -0300

    adicionando requisito 5 corrigindo

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ function pressbutton() {
   const inputValue = (input.value);
   const divLi = document.createElement('li');
   divLi.innerText = inputValue;
+	divLi.style.backgroundColor = 'rgb(128, 128, 128)';
   elementOl.appendChild(divLi);
-  input.value = '';
+  
+	
+	input.value = '';
 }
 
 button.addEventListener('click', pressbutton);
 
 // referencias: https://cursos.alura.com.br/forum/topico-pegar-valor-do-input-com-javascript-62528
 // http://devfuria.com.br/javascript/dom-create-element/#:~:text=A%20fun%C3%A7%C3%A3o%20createElement()%20ir%C3%A1,createElement(tagName)%3B
+// https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/
