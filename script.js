7776234c9ee4a1c47be977a94a024924c169f0b9
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-13 16:36:14 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -17,22 +17,34 @@ function concatList() {
 function changeBackgroudColorElement(e) {
   const li = document.querySelectorAll('.listElement');
   for (let i = 0; i < li.length; i += 1) {
-    li[i].style.backgroundColor = 'white'
+    li[i].style.backgroundColor = 'white';
   }
   e.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 function addBackgroudColorInElement() {
   const li = document.querySelectorAll('.listElement');
-  console.log(li);
   for (let i = 0; i < li.length; i += 1) {
     li[i].addEventListener('click', changeBackgroudColorElement);
   }
 }
 
+function riskElement(e) {
+  e.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+  if (!e.target.classList.contains('completed')) {
+    e.target.className += ' completed';
+  }
+}
+function checkElement() {
+  const li = document.querySelectorAll('.listElement');
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].addEventListener('dblclick', riskElement);
+  }
+}
 function clickBtnAdicionar() {
   botAdicionar.addEventListener('click', concatList);
   botAdicionar.addEventListener('click', addBackgroudColorInElement);
+  botAdicionar.addEventListener('click', checkElement);
 }
 
 clickBtnAdicionar();
