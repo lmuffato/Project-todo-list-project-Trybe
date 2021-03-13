2434a44a6490f181062aef1360faf7b1b7d5567a
Flavio Franco <flavio.win@gmail.com>
2021-03-13 14:58:12 -0300

    arrumando lint

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-function buttonCreateTaks() {
-  let button = document.getElementById('criar-tarefa');
-  let listHeader = document.getElementById('lista-tarefas');
-  button.addEventListener('click', ()=>{
-    let itemList = document.createElement('li');
-    itemList.innerText = document.getElementById('texto-tarefa').value;
-      if (itemList.textContent !== '') { 
-        listHeader.appendChild(itemList);
-        document.getElementById('texto-tarefa').value = ''; 
+const listHeader = document.getElementById('lista-tarefas');
+
+function cleanCompletedTasks() {
+  const listItems = document.getElementsByTagName('li');
+  const buttonClearCompleted = document.getElementById('remover-finalizados');
+  buttonClearCompleted.addEventListener('click', () => {
+    for (let index = 0; index < listItems.length; index += 1) {
+      if (listItems[index].classList.contains('completed')) {
+        listItems[index].remove();
       }
-      cleanCompletedTasks();
-    });  
+    }
+  });
+}
+
+function buttonCreateTaks() {
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', () => {
+    const itemList = document.createElement('li');
+    itemList.innerText = document.getElementById('texto-tarefa').value;
+    if (itemList.textContent !== '') {
+      listHeader.appendChild(itemList);
+      document.getElementById('texto-tarefa').value = '';
+    }
+    cleanCompletedTasks();
+  });
 }
 
 function completedTasks() {
-  let myList = document.getElementById('lista-tarefas');
-  
-  myList.addEventListener('dblclick', (e)=>{
+  const myList = document.getElementById('lista-tarefas');
+
+  myList.addEventListener('dblclick', (e) => {
     if (e.target.classList.contains('completed')) {
       e.target.classList.remove('completed');
     } else {
@@ -25,44 +38,24 @@ function completedTasks() {
 }
 
 function cleanAllTasks() {
+  const btnClean = document.getElementById('apaga-tudo');
 
-  let listHeader = document.getElementById('lista-tarefas');
-  let btnClean = document.getElementById('apaga-tudo');
-
-  btnClean.addEventListener('click', ()=>{
+  btnClean.addEventListener('click', () => {
     while (listHeader.lastElementChild) {
       listHeader.removeChild(listHeader.lastElementChild);
     }
   });
 }
 
-function cleanCompletedTasks() {
-  let listItems = document.getElementsByTagName('li');
-  let buttonClearCompleted = document.getElementById('remover-finalizados');
-  buttonClearCompleted.addEventListener('click', ()=>{
-    
-    for (let index = 0; index < listItems.length; index += 1) {
-      if (listItems[index].classList.contains('completed')) {
-        listItems[index].remove();
-      }
-    }
-  });
-
-
-}
-
 function colorItem() {
-  let listHeader = document.getElementById('lista-tarefas');
-
-  listHeader.addEventListener('click',(e)=> {
+  listHeader.addEventListener('click', (e) => {
     e.target.classList.add('selected');
-    let allItems = document.querySelectorAll('li');
+    const allItems = document.querySelectorAll('li');
     for (let index = 0; index < allItems.length; index += 1) {
       if (allItems[index] !== e.target) {
         allItems[index].classList.remove('selected');
       }
     }
-          
   });
 }
 
@@ -70,11 +63,3 @@ buttonCreateTaks();
 completedTasks();
 cleanAllTasks();
 colorItem();
-
-
-
-
-
-
-
-
