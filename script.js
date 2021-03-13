6a843c202538f40e8e9a9eef9ae057377d77f847
Rafael Cardoso Trybe truma 10 A <rafael.vimur@gmail.com>
2021-03-13 14:42:23 -0300

    adiciona requisito 7 e 8

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+// requisito 5 e 6 feito com a ajuda do colega Lucas Pedroso
 function button () {
 	let addButton = document.querySelector('#criar-tarefa');
   addButton.addEventListener('click', function () {
 	let list = document.querySelector('#lista-tarefas');
 	let writedText = document.querySelector('#texto-tarefa')
 	let li = document.createElement('li');
+	li.classList.add('color-li')
 	li.innerHTML = writedText.value;
 	list.appendChild(li);
 	document.querySelector('#texto-tarefa').value = '';
 });
 }
 button ()
-// requisito 5 feito com a ajuda do colega Lucas Pedroso
+
+function colorLi () {
+	let tasks = document.getElementsByClassName('color-li');
+	let tasksList = document.querySelector('#lista-tarefas');
+	tasksList.addEventListener('click', function (event) {
+		let tasksSelect = event.target
+		for (let index = 0; index < tasks.length; index += 1) {
+			if (tasks[index].className.includes('li-select')) {
+				tasks[index].classList.remove('li-select')
+			} else {
+				tasksSelect.classList.add('li-select')
+			}
+		}
+	})
+}
+colorLi ()
