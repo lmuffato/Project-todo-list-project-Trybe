a957b008045190930ad6aa2b1d0896355da494f0
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-03-13 20:55:04 -0400

    ajustando requisito 7

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
-// REQUISITO 5 - Adicionar um botão com ID='criar-tarefa', que ao clicar o novo item deve aparecer no final da lista, e o texto do input deve ser limpo //
+const buttonTask = document.getElementById('criar-tarefa');
+const inputTask = document.getElementById('texto-tarefa');
+const listTask = document.getElementById('lista-tarefas');
 
-const buttonAddTask = document.querySelector('#criar-tarefa');
-const addItem = document.querySelector('#texto-tarefa');
-const addListTask = document.querySelector('#lista-tarefas');
-const createItems = document.createElement('li');
-buttonAddTask.addEventListener('click', addTask);
-addItem.addEventListener('keyup', insertItemsListKeyUp);
+// REQUISITO 7 //
 
 function addTask() {
-  if (addItem.value !== '') {
-    createItems.className = 'task';
-    createItems.innerHTML = addItem.value;
-    addListTask.appendChild(createItems);
-    addItem.value = '';
-  }
-}
-
-function insertItemsListKeyUp(event) { // Resolução baseada no conteúdo do site https://pt.stackoverflow.com/questions/180333/evento-tecla-enter-no-input
-  const key = event.which || event.keyCode;
-
-  if (addItem.value !== '' && key === 13) {
-    createItems.className = 'task';
-    createItems.innerHTML = addItem.value;
-    addListTask.appendChild(createItems);
-    addItem.value = '';
-  }
-}
-
-// REQUISITO 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza //
-
-function changeGray() {
-  addListTask.addEventListener('click', (event) => {
-    for (let index = 0; index < addListTask.children.length; index += 1) {
-      addListTask.children[index].style.backgroudColor = '';
-    }
-    event.target.style.backgroudColor = 'rgb(128, 128, 128)';
+  buttonTask.addEventListener('click', () => {
+    const getList = document.createElement('li');
+    getList.innerText = inputTask.value;
+    listTask.appendChild(getList);
+    inputTask.value = '';
   });
 }
 
 window.onload = function () {
   addTask();
-  changeGray();
 };
