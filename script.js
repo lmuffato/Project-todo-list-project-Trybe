9d4c824c24d7fa0b721c8fa2ae588e642d84c0b9
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-03-14 18:17:22 -0400

    requisito 12

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const buttonTask = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const listTask = document.getElementById('lista-tarefas');
 const deleteAll = document.getElementById('apaga-tudo');
-const finish = document.getElementsByClassName('completed');
-const buttonFinish = document.getElementById('remover-finalizados');
+const saveList = document.getElementById('salvar-tarefas');
 
 // REQUISITO 5 E 6 //
 
@@ -51,7 +50,9 @@ function clearList() {
 // REQUISITO 11 //
 
 function finishedRemove() {
+  const buttonFinish = document.querySelector('#remover-finalizados');
   buttonFinish.addEventListener('click', () => {
+    const finish = document.querySelectorAll('.completed');
     if (finish.length !== 0) {
       for (let index = 0; index < finish.length; index += 1) {
         finish[index].remove();
@@ -60,10 +61,39 @@ function finishedRemove() {
   });
 }
 
+// REQUISITO 12 //
+
+function saveTask() {
+  saveList.addEventListener('click', () => {
+    const taskLi = document.querySelectorAll('.task');
+    for (let index = 0; index < taskLi.length; index += 1) {
+      const saveInformation = {
+        text: taskLi[index].innerText, // salvar as informações digitadas pelo usuário //
+        class: taskLi[index].className, // salvando todas as classes que tem dentro do elemento //
+      };
+      localStorage.setItem(index, JSON.stringify(saveInformation));
+    }
+  });
+}
+
+function recoverList() {
+  const recoverTask = document.getElementById('lista-tarefas');
+  for (let index = 0; index < listTask.length; index += 1) {
+    const recoverItem = document.createElement('li');
+    const recoverSaveInformation = JSON.parse(localStorage.getItem(index));
+    recoverItem.innerText = recoverSaveInformation.text;
+    recoverItem.className = recoverSaveInformation.class;
+    recoverItem.classList.remove('selected');
+    recoverTask.appendChild(recoverItem);
+  }
+}
+
 window.onload = () => {
   addTask();
   getGray();
   completeItem();
   clearList();
   finishedRemove();
+  saveTask();
+  recoverList();
 };
