e6659ca8b0306f75e44e1663e736d42238773ca3
Tiago Santos <tiago881@gmail.com>
2021-03-15 07:13:43 -0300

    5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo

---
+++ script.js
@@ -0,0 +1,13 @@
+/* No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input. */
+function creatTasks() {
+  const button = document.querySelector('#criar-tarefa');
+  button.addEventListener('click', () => {
+    const inputElement = document.querySelector('#texto-tarefa');
+    const getText = inputElement.value;
+    const liElment = document.createElement('li');
+    liElment.innerHTML = getText;
+    const olElment = document.querySelector('#lista-tarefas');
+    olElment.appendChild(liElment);
+  });
+}
+creatTasks();
