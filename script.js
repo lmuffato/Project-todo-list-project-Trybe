91e16906ee241133e1e287e498525f4fee4782ac
luanlsr <luanfswd@gmail.com>
2021-03-15 14:52:54 -0300

    requisitos 13 e 14 finalizados. Iniciado requisito 12

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const todoList = document.querySelector('#lista-tarefas');
 const clear = document.querySelector('#remover-finalizados');
 const clearAll = document.querySelector('#apaga-tudo');
 const clearSelected = document.querySelector('#remover-selecionado');
+const save = document.querySelector('#salvar-tarefas');
+const up = document.querySelector('#mover-cima')
+const down = document.querySelector('#mover-baixo')
 
-createAssignment.addEventListener('click', () => {
+createAssignment.addEventListener('click', function criaLi() {
   const li = document.createElement('li');
   li.innerText = input.value;
   todoList.appendChild(li);
@@ -48,4 +51,70 @@ clearSelected.addEventListener('click', () => {
   for (const final of fineshed) {
     final.remove();
   }
+});
+// Foi feito baseado no site https://pt.stackoverflow.com/questions/65705/como-%C3%A9-poss%C3%ADvel-trocar-li-de-posi%C3%A7%C3%A3o e no code Review do William Willers
+up.addEventListener('click' , () => {
+    const selectedItem = todoList.querySelector('.gray');
+    if (selectedItem === null) {
+      return alert('Selecione um item para mover')
+    }
+    const swapItem = selectedItem.previousElementSibling;
+    if(swapItem === null) {
+      return alert('Não é possível mover o item')
+    }
+    swapItem.parentNode.insertBefore(selectedItem, swapItem);
+})
+
+
+
+down.addEventListener('click' , () => {
+  const selectedItem = todoList.querySelector('.gray');
+  if (selectedItem === null) {
+    return alert('Selecione um item para mover')
+  }
+  const swapItem = selectedItem.nextElementSibling;
+  if(swapItem === null) {
+    return alert('Não é possível mover o item')
+  }
+  swapItem.parentNode.insertBefore(swapItem, selectedItem);
+})
+
+
+
+window.addEventListener('load' , () => {
+
+  save.addEventListener('click' , () => {
+    // const username = document.querySelector('#username').value;
+      // localStorage.userN = username;
+      localStorage.listLi = todoList.innerHTML;
+  })
+  // let nome = prompt('Digite seu nome:')
+  // while(nome !== 'luan') {  
+  //  var nome = prompt('Digite seu nome:');
+  //   console.log(nome);
+  //   // localStorage.setItem('luan')
+  //   if( nome !== 'luan') {
+  //     alert('O nome digitado não consta em nosso banco de dados. Tente novamente!')
+  //   }
+  // } 
+  // alert(`Bem-Vindo ${nome}` )
+  
+    // const user = document.querySelector('#username').value;
+    
+    // user = localStorage.getItem(userN);
+
+    function criaLi2() {
+      const list = localStorage.getItem(listLi);
+      for (let l of list) {
+
+      const li = document.createElement('li');
+      li.innerText = l.value;
+      
+      todoList.appendChild(li);
+      }
+
+    }
+      
+    criaLi2();
+
 });
