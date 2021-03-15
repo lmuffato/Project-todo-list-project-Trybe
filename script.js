7894ace3412bd8437b6f93e0588523f56cb52348
Luiz <luizfvaccarif@gmail.com>
2021-03-15 14:46:55 +0000

    Requirement 1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Requirement 1
+
+const createHeader = document.createElement('header');
+document.body.appendChild(createHeader);
+
+const createH1 = document.createElement('h1');
+createH1.innerHTML = 'Minha Lista de Tarefas';
+createHeader.appendChild(createH1);
+
