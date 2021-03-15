688774f6b1b8b24fe3fe202b325f7a1402cd48ac
André Dantas <aluizcfd@gmail.com>
2021-03-15 04:03:37 -0300

    marking and unmarking tasks is now possible

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+const taskList = document.querySelector('#lista-tarefas');
+
 function addTask() {
   const eachTask = document.createElement('li');
   eachTask.innerText = document.querySelector('#texto-tarefa').value;
   document.querySelector('#texto-tarefa').value = '';
-  document.querySelector('#lista-tarefas').appendChild(eachTask);
+  taskList.appendChild(eachTask);
 }
 
 const buttonAddTask = document.querySelector('#criar-tarefa');
 buttonAddTask.addEventListener('click', addTask);
+
+function removeBackground() {
+  const everyTask = taskList.children;
+  for (let i = 0; i < everyTask.length; i += 1) {
+    everyTask[i].removeAttribute('style');
+  }
+}
+
+function addBackground(event) {
+  removeBackground();
+  const eTarget = event.target;
+  eTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+taskList.addEventListener('click', addBackground);
