8c32889b93e534ab1332848b3b662cf21f098908
Maria Baeta <m.martinsdg@gmail.com>
2021-03-14 22:21:40 -0300

    Atende ao requisito 7

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ function getInputHtml() {
   function add() {
     const valueLi = getInput.value;
     const createLi = document.createElement('li');
+    createLi.classList.add('list');
     createLi.appendChild(document.createTextNode(valueLi));
     getOl.appendChild(createLi);
     getInput.value = '';
   }
-  getButton.onclick = function () {
+  getButton.onclick = function() {
     add();
   };
 }
-window.onload = function () {
+// Requisito 7
+// 7. Clicar em um item da lista de alterar o bgColor para rgb(128, 128, 128)
+function changeBackgroundColor(event) {
+  if (event.target.className === 'list') {
+    const selectedLi = event.target;
+    selectedLi.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
+function managerEvents() {
+// Gerencia os eventos
+// 7. Recuperar o pai dos meus itens de lista
+  const taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', changeBackgroundColor);
+}
+window.onload = function start() {
+// Chama as funções
   getInputHtml();
+  managerEvents();
 };
