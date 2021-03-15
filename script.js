befe1d2652f51c3256c400aa324f976d1601c118
Samuel Melo <samuelmmjunior@gmail.com>
2021-03-15 15:12:46 -0300

    requisito 10

--- script.js
+++ script.js
@@ -11,15 +11,24 @@ function insertList() {
 }
 
 insertList();
-/* o codigo do requisito 7 e 8 foi escrito com base na solução de Guilherme Dornelas guilhermedornelles-todo-list-project*/
+/* o codigo do requisito 7 e 8 foi escrito com base na solução de Guilherme Dornelas guilhermedornelles-todo-list-project */
 function selectList() {
-    const listSelect = document.getElementById('lista-tarefas');
-    listSelect.addEventListener('click', (event) => {
+  const listSelect = document.getElementById('lista-tarefas');
+  listSelect.addEventListener('click', (event) => {
     for (let index = 0; index < listSelect.children.length; index += 1) {
     listSelect.children[index].style.backgroundColor = '';
     }
     event.target.style.background = 'rgb(128, 128, 128)';
-    });
-  }
+  });
+}
 
-  selectList();
+selectList();
+
+function clearList() {
+  const listSelect = document.getElementById('lista-tarefas');
+  document.getElementById('apaga-tudo').addEventListener('click', () => {
+  listSelect.innerText='';
+  });
+}
+
+clearList();
