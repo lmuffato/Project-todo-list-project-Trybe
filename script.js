970e81ae6cd3a5c285e874989228a1fc5117bc20
Eric <ericff07@gmail.com>
2021-03-15 19:24:34 -0300

    Adiciona o requisito 9

--- script.js
+++ script.js
@@ -6,26 +6,45 @@ function newTask () {
         alert('Por favor, digite um valor para adicionar à sua lista.')
     }   else {
         newItem.innerText = itemValue;
+        newItem.addEventListener('dblclick', function(e) {
+            e.target.classList.toggle('completed')
+        })
         newItem.classList.add('back');
         newItem.style.width = '200px';
+        
         document.getElementById('lista-tarefas').appendChild(newItem);
-        //itemValue = '';
         document.getElementById('texto-tarefa').value = '';
     }
     let listItem = document.querySelectorAll('.back')
     for (let index = 0; index < listItem.length; index += 1) {
+        //listItem[index].addEventListener('dblclick', function(e) {
+           // e.target.classList.toggle('completed')
+        //})
         listItem[index].addEventListener('click', function(e) {
-        getSelected = document.querySelector('.selected');
-
-
-
-        getSelected.classList.remove('selected');
-          //listItem[index].classList.add('selected');
-        //e.target.style.backgroundColor = 'rgb(128,128,128)';
-        e.target.classList.add('selected');
-      })
+            getSelected = document.querySelector('.selected');
+            getSelected.classList.remove('selected');
+            e.target.classList.add('selected');
+        })
+        
+      
     }
 }
+
+function removeList() {
+    let fullList = document.querySelector('.back');
+    fullList.parentNode.removeChild(fullList);
+    //return false;
+}
+
+/*
+function removeList() {
+    let fullList = document.querySelectorAll('.back');
+    for (index1 = 0; index1 < fullList[index1]; index1 += 1) {
+    fullList[index1].parentNode.removeChild(fullList[index1]);
+    return false;
+    }
+}
+*/
 
 let btnAddTask = document.getElementById('criar-tarefa');
 btnAddTask.addEventListener('click', newTask);
