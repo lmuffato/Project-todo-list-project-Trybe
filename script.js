be0e2c302d738a5379ed8554d31571da62cce972
Luiz <luizfvaccarif@gmail.com>
2021-03-15 15:08:11 +0000

    Requirement 3

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ const createH1 = document.createElement('h1');
 createH1.innerHTML = 'Minha Lista de Tarefas';
 createHeader.appendChild(createH1);
 
+//Requirement 2
+
 const createP = document.createElement('p');
 createP.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-createP.style.fontSize = '15px'
+createP.style.fontSize = '15px';
 createP.id = 'funcionamento';
 createH1.appendChild(createP);
+
+//Requirement 3
+
+const createInput = document.createElement('input');
+createInput.id = 'texto-tarefa';
+document.body.appendChild(createInput);
