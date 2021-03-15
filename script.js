07bfc4468783432c9bbdb180e2dab27bb50df62f
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-03-14 21:53:26 -0300

    requisito 5, teste

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function addList() {
-  let btnForAdd = document.querySelector('#cria-tarefa');
-  btnForAdd.addEventListener('click' , function () {
-  let list = document.querySelector('#lista-tarefas');
-  let leList = document.querySelector('#texto-tarefa');
-  let li = document.createElement('li');
-  li.innerHTML = leList.value;
-  list.appendChild(li);
-  document.getElementById('texto-tarefa').value = '';
-});
+  const btnForAdd = document.querySelector('#cria-tarefa');
+  btnForAdd.addEventListener('click' , () => {
+    const list = document.querySelector('#lista-tarefas');
+    const leList = document.querySelector('#texto-tarefa');
+    const li = document.createElement('li');
+    li.innerHTML = leList.value;
+    list.appendChild(li);
+    document.getElementById('texto-tarefa').value = '';
+  });
 }
 addList();
