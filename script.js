cb25db3517dac0c009ab43510bdbeba920a3d2a4
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-03-16 08:12:40 -0300

    requisito 7, teste

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function addList() {
   });
 }
 addList();
+
+// precisei analisar o codigo do Gabriel Pereira para conseguir proceguir, link https://github.com/tryber/sd-010-a-project-todo-list/blob/515c53c7f57f62d816719e601efd0b7b9ad37c70/script.js //
+
+function changingBackgroundGreen(event) {
+  const eventTarget = event.target;
+  eventTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function eventchangeBackgroundGreen() {
+  const getIDTarefaLista = document.getElementById('lista-tarefas');
+  const getTarefa = getIDTarefaLista;
+  getTarefa.addEventListener('click', changingBackgroundGreen);
+}
+
+eventchangeBackgroundGreen();
