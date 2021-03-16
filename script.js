d9bbc92d3dd8e1e021df7369fceba31decd370e6
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-16 00:20:14 -0300

    Adiciona funcionalidade de riscar o texto ao clicar duas vezes

--- script.js
+++ script.js
@@ -9,11 +9,23 @@ window.onload = function (){
         let item = document.getElementsByTagName('li');
         for (let index = 0; index < item.length; index += 1) {
             let control = item[index];
-            control.addEventListener('click', (a) => {
+            control.addEventListener('click', (a) => {                
                 a.target.style.backgroundColor = 'rgb(128, 128, 128)';
             })
         }
     }
+
+    function line () {
+        let selected = document.querySelectorAll('li');
+        for (let index = 0; index < selected.length; index += 1) {
+            let varIndex = selected[index];
+            varIndex.addEventListener('dblclick', () => {
+                varIndex.classList.add('completed');
+            })
+        }
+        
+    }
+
     let addList = [];
     // Pega o clique do botão 
     let btn = document.getElementById("criar-tarefa");
@@ -31,12 +43,12 @@ window.onload = function (){
         pushOl.appendChild(creatLi);
         //adiciona a lista e acrescenta 1 números        
         addList[cont].innerText = pushInput;
+        
         cont += 1;
         mostra();
         // Limpa o campo input
         clearInput();
-        //console.log(addList);
-        
+        line();
     });
     
 } 
