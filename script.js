f8a19baff9eaf2ae317115b4410762a93439a420
Victor Ricco <victor.ricco001@gmail.com>
2021-03-16 16:09:56 -0300

    requisito 7 e 8 :roquet:

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ window.onload = function() {
   const addButton = document.querySelector('#criar-tarefa');
 
   addButton.addEventListener('click', function(event){
-    console.log('dbclick')
     createLsHtml();
+  });
+
+  list.addEventListener('dblclick', function(event){
+    
+  });
+
+  list.addEventListener('click', function(event){
+    let targetLi = event.target;
+    let colorWithSelected = document.querySelector('.selected');
+    if(colorWithSelected === null){
+      targetLi.classList.add('selected');
+    }else{
+      colorWithSelected.classList.remove('selected');
+      targetLi.classList.add('selected');
+    }
   });
 
   function createLsHtml(){
     let ls = document.createElement('li');
     let info = document.querySelector('#texto-tarefa');
     ls.innerText = info.value;
-    list.appendChild(ls)
-    info.value = ''
+    list.appendChild(ls);
+    info.value = '';
   }
 }
