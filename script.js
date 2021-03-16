70f03a68abdf4a672f2300cef45661e66f8eb97b
luanlsr <luanfswd@gmail.com>
2021-03-16 00:54:54 -0300

    corrigidos erros de lint

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const clear = document.querySelector('#remover-finalizados');
 const clearAll = document.querySelector('#apaga-tudo');
 const clearSelected = document.querySelector('#remover-selecionado');
 const save = document.querySelector('#salvar-tarefas');
-const up = document.querySelector('#mover-cima')
-const down = document.querySelector('#mover-baixo')
+const up = document.querySelector('#mover-cima');
+const down = document.querySelector('#mover-baixo');
 
-createAssignment.addEventListener('click', function criaLi() {
+createAssignment.addEventListener('click', () => {
   const li = document.createElement('li');
   li.innerText = input.value;
   todoList.appendChild(li);
@@ -53,51 +53,53 @@ clearSelected.addEventListener('click', () => {
   }
 });
 // Foi feito baseado no site https://pt.stackoverflow.com/questions/65705/como-%C3%A9-poss%C3%ADvel-trocar-li-de-posi%C3%A7%C3%A3o e no code Review do William Willers
-up.addEventListener('click' , () => {
-    const selectedItem = todoList.querySelector('.gray');
-    if (selectedItem === null) {
-      return alert('Selecione um item para mover')
-    }
-    const swapItem = selectedItem.previousElementSibling;
-    if(swapItem === null) {
-      return alert('Não é possível mover o item')
-    }
-    swapItem.parentNode.insertBefore(selectedItem, swapItem);
-})
-
-
-
-down.addEventListener('click' , () => {
+up.addEventListener('click', () => {
   const selectedItem = todoList.querySelector('.gray');
   if (selectedItem === null) {
-    return alert('Selecione um item para mover')
+    return alert('Selecione um item para mover');
   }
-  const swapItem = selectedItem.nextElementSibling;
-  if(swapItem === null) {
-    return alert('Não é possível mover o item')
+  const swapItem = selectedItem.previousElementSibling;
+  if (swapItem === null) {
+    return alert('Não é possível mover o item');
   }
-  swapItem.parentNode.insertBefore(swapItem, selectedItem);
+  swapItem.parentNode.insertBefore(selectedItem, swapItem);
 })
 
+down.addEventListener('click', () => {
+  const selectedItem = todoList.querySelector('.gray');
+  if (selectedItem === null) {
+    return alert('Selecione um item para mover');
+  }
+  const swapItem = selectedItem.nextElementSibling;
+  if (swapItem === null) {
+    return alert('Não é possível mover o item');
+  }
+  swapItem.parentNode.insertBefore(swapItem, selectedItem);
+});
 
-  save.addEventListener('click' , () => {
-    const username = document.querySelector('#username').value;
-      localStorage.userN = username;
-      localStorage.listLi = todoList.innerHTML;
-  })
+save.addEventListener('click', () => {
+  const username = document.querySelector('#username').value;
+    localStorage.userN = username;
+    localStorage.listLi = todoList.innerHTML;
+})
 
-  //linhas e 90 e 91 fiz baseadas no code review do pull Resquest do Anderson Silva (Andy)
+//linhas e 90 e 91 fiz baseadas no code review do pull Resquest do Anderson Silva (Andy)
 function getLocalStorageContent() {
   todoList.innerHTML = localStorage.getItem('listLi');
   const userOfLocalStorage = localStorage.getItem('userN');
   const welcome = document.querySelector('#welcome');
-  if(userOfLocalStorage === '' || userOfLocalStorage === undefined){
-    welcome.style.display = 'none'
-  } else{
-    welcome.style.display = 'block'
-    welcome.innerHTML = `Seja bem-vindo ${userOfLocalStorage}`
+  if (userOfLocalStorage === '' || userOfLocalStorage === undefined) {
+    welcome.style.display = 'none';
+  } else {
+    welcome.style.display = 'block';
+      // let radio = document.querySelector('input[name="radio"]:checked').value;
+      // if (radio){
+      //   welcome.innerHTML = `Seja bem-vindo ${userOfLocalStorage}`;
+      // }else{
+    welcome.innerHTML = `Seja bem-vindo ${userOfLocalStorage}`;
+      
 
       
-    }
+  }
 }
 getLocalStorageContent();
