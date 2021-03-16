037732c8e65cda0c68a99356e462d0b39f9014cc
Robertson <robertson.nunes@gmail.com>
2021-03-15 21:45:30 -0300

    requisito 5 ok

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function addTask() {
-    let li = document.createElement('li')
-    let input_task = document.form_main.task.value;
-    let imput_text = document.createTextNode(input_task);
+/* 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo */
+const orderedList = document.getElementById('lista-tarefas');
 
-    li.appendChild(input_task);
-    document.querySelector('ul').appendChild(li);
-    document.form_main.task.value = "";
+const updateStorage = () => { localStorage.setItem('lists', orderedList.innerHTML); };  
 
-    
-}
+const addTask = () => {
+  document.getElementById('criar-tarefa').addEventListener('click', () => {
+    const tasklist = document.createElement('li');
+    const inputTask = document.getElementById('texto-tarefa');
+
+    tasklist.innerText = inputTask.value;
+    orderedList.appendChild(tasklist);
+    inputTask.value = '';
+  });
+};
+
+window.onload = () => {
+updateStorage()
+addTask()
+};
