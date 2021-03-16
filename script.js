68870e6bb2281fd92df86fae3e35f55b7821f996
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-03-15 22:13:28 -0300

    New try requirement 8

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function createButton () {
 }
 createButton();
 
-// pratica 5 e 6 - https://stackoverflow.com/questions/26688323/appending-input-value-to-list-using-javascript-to-do-list
+// pratica 5, 6, 7 - https://stackoverflow.com/questions/26688323/appending-input-value-to-list-using-javascript-to-do-list
 function clickButton (){
     
     let click = document.getElementById('criar-tarefa');
@@ -21,25 +21,30 @@ function clickButton (){
         li.innerHTML = text.value;
         li.addEventListener('click', function (event){
             let evt = event.target;
-            evt.style.backgroundColor = 'rgb(128, 128, 128)';
+            let gray = document.querySelector('#gray');
+            if (gray){
+                gray.removeAttribute('id');
+            }
+            evt.id = 'gray'; 
         });
+        //let liColor = document.getElementsByTagName('li');
+        // add evento
+        // ferificar se algum tem a cor cinza, se tiver remove e se não tiver adiciona
         text.value = '';
         addLi.appendChild(li);
     })
 }
 clickButton ();
 
+function lineThrough(){
+    let list = document.getElementById('lista-tarefas');
+    list.addEventListener('dblclick', function (event){
+            let click = event.target;
+            click.classList.add('completed');     
+     });    
+}
+lineThrough();
 
-// // prática 7
-// function backGrey (){
-//     let backColor = document.querySelectorAll('#lista-tarefas li');
-
-//     for (let i = 0; i < backColor.length; i += 1){
-       
-//         });
-//     }
-// }
-// backGrey ();
 
 
 
