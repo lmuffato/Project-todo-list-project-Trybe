d15bb98bc02120933314e2ee8dcfcedfc4b7eee4
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-15 22:43:03 -0300

    Corrigindo erros

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 window.onload = function (){
-    
+    // Função para zerar input
+    function clearInput () {
+        document.getElementById('texto-tarefa').value = '';
+    }
+
+        
+    function mostra () {
+        let item = document.getElementsByTagName('li');
+        for (let index = 0; index < item.length; index += 1) {
+            let control = item[index];
+            control.addEventListener('click', (a) => {
+                a.target.style.backgroundColor = "black";
+                console.log(a)
+            })
+        }
+    }
+    let addList = [];
     // Pega o clique do botão 
     let btn = document.getElementById("criar-tarefa");
     // cria a variável que irá receber o texto do input
@@ -17,23 +33,13 @@ window.onload = function (){
         //adiciona a lista e acrescenta 1 números        
         addList[cont].innerText = pushInput;
         cont += 1;
-        document.getElementById('texto-tarefa').value = '';
+        mostra();
+        // Limpa o campo input
+        clearInput();
+        //console.log(addList);
+        
     });
-
-    function mouseDown() {
-        document.getElementsByTagName("li").style.color = "green";
-      }
-      
-
-    /* let listClick = document.getElementsByTagName('li');
-    for (let index = 0; index < cont.length; index += 1) {
-        let clickOnList = listClick[index]
-        clickOnList.addEventListener('click', function()
-        {
-           clickOnList.style.backgroundColor = 'rgb(128, 128, 128)';
-        })    
-    } */
-
+    
 } 
 
 
