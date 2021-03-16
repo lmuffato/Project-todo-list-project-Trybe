53970654de9739d1adc0f2553e6b004231255961
walvesb65 <willian.alvesb65@gmail.com>
2021-03-16 00:43:48 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function paragrafo(){
-  let texto1 = document.getElementById('funcionamento')
+  const texto1 = document.getElementById('funcionamento')
   texto1.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
 }
 paragrafo();
+
+function listaOrdenada(){
+  const pai = document.getElementById('lista')
+  const filho = document.createElement('ol')
+  pai.appendChild(filho)
+  filho.id = 'lista-tarefas'
+}
+listaOrdenada();
+
+function button(){
+  const elementoButton = document.querySelector('button')
+  elementoButton.id = 'criar-tarefa';
+  elementoButton.addEventListener('click', () => {
+  const pai = document.querySelector('ol');
+  const filhoDePai = document.createElement('li');
+  pai.appendChild(filhoDePai);
+  const achandoInput = document.querySelector('#texto-tarefa')
+  filhoDePai.innerText = achandoInput.value;
+  achandoInput.value = ''
+  })
+}
+button();
