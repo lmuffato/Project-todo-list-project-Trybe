992e95b19fc1b74ef0fdce32b65a3aca6adfa3f2
Robertson <robertson.nunes@gmail.com>
2021-03-15 21:58:34 -0300

    requisito 6/7 ok

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /* 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo */
+/* Ref.: https://www.w3schools.com/js/js_events.asp */
 const orderedList = document.getElementById('lista-tarefas');
 
-const updateStorage = () => { localStorage.setItem('lists', orderedList.innerHTML); };  
+const updateLocalStorage = () => { localStorage.setItem('lists', orderedList.innerHTML); };  
 
 const addTask = () => {
   document.getElementById('criar-tarefa').addEventListener('click', () => {
@@ -14,7 +15,21 @@ const addTask = () => {
   });
 };
 
-window.onload = () => {
-updateStorage()
+updateLocalStorage()
 addTask()
-};
+
+/* 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128) */
+const list = document.querySelector('#lista-tarefas');
+const lines = document.querySelectorAll('li');
+
+function selectTask() {
+  list.addEventListener('click', function (event) {
+    event.target.classList.add('selected');
+    for (let index = 0; index < lines.length; index += 1) {
+      if (lines[index] !== event.target) {
+        lines[index].classList.remove('selected');
+      }
+    }
+  });
+}
+selectTask();
