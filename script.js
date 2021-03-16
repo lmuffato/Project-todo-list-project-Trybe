65db8c3ff9ba9896a781808ed98bc92e7a5fbc4b
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-16 20:03:28 -0300

    enviando requisitos 7 e 8, otimizando o codigo e corigindo identacao

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 function submitTask() {
-  const taskList = document.getElementById('lista-tarefas');// Recupera o elemento ordened list
-  const newTask = document.createElement('li');// Cria um list item
-  const index = parseInt(localStorage.getItem('index'));// Um índice salvo no localStorage para ser usado na chave dos list itens
-
+  const index = parseInt(localStorage.getItem('index', 10));
   localStorage.setItem('task' + index, document.formTasks.task.value);// Salva no localStorage a nova tarefa 
-  newTask.innerText = localStorage.getItem('task' + index);
-  taskList.appendChild(newTask);
   localStorage.setItem('index', index + 1);// Incrementa o index no localStorage
 }
 
 function addButtomListener() {
-    const button = document.getElementById('criar-tarefa');
-    button.addEventListener('click', submitTask);
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', submitTask);
+}
+
+function selectItem(eventOrigin) {
+  const selected = document.querySelector('.list-item');
+  
+  if (selected != null) {
+    selected.classList.remove('list-item');
   }
+  listItem = eventOrigin.target;
+  listItem.className = 'list-item';
+}
 
 function pullTasks() {
-  const index = parseInt(localStorage.getItem('index'));
+  const index = parseInt(localStorage.getItem('index', 10));
 
-    for (i = 1; i < index; i += 1) {
-      const taskList = document.getElementById('lista-tarefas');
-      const newTask = document.createElement('li');
-      newTask.innerText = localStorage.getItem('task' + i);
-      taskList.appendChild(newTask);
-    }
+  for (let i = 1; i < index; i += 1) {
+    const taskList = document.getElementById('lista-tarefas');
+    const newTask = document.createElement('li');
+    newTask.addEventListener('click', selectItem);
+    newTask.innerText = localStorage.getItem('task' + i);
+    taskList.appendChild(newTask);
+  }
 }
 
 function firstTime() {
