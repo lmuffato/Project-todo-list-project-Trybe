4a88a9dd1fa070b451c4aa9fa2e6f19a615e667d
rcalvs <rafaelcalvette@gmail.com>
2021-03-16 14:38:30 -0300

    Q7

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ botaoInputTarefa.addEventListener('click', () => {
 });
 }
 inputTarefa ();
+
+function selecionaTarefa () {
+    listaDeTarefas.addEventListener('click',(element) => {
+        const selecionado = document.querySelector('#selecionado')
+        if (selecionado !== null) {
+            document.querySelector('#selecionado').style.backgroundColor = 'white';
+            document.querySelector('#selecionado').id = '';
+        } else {
+            element.target.id = 'selecionado';
+            document.querySelector('#selecionado').style.backgroundColor = 'rgb(128, 128, 128)';
+        }
+    });
+}
+selecionaTarefa ();
+// console.log()
