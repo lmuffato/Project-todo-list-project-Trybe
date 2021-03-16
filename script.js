da01b79886f529f4a1e643ed912f0687f1cfe699
José Luiz Fagundes <jlfagundes@gmail.com>
2021-03-16 20:57:06 -0300

    Requirement five done

---
+++ script.js
@@ -0,0 +1,11 @@
+function addTask() {
+    const addButton = document.getElementById('criar-tarefa');
+    addButton.addEventListener('click', () => {
+        const input = document.getElementById('texto-tarefa').value;
+        const itenList = document.createElement('li');
+        if (input !== '') {
+            document.getElementById('lista-tarefas').appendChild(itenList).innerText = input;
+        }
+    })
+}
+addTask()
