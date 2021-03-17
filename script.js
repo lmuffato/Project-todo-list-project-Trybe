39eec13f81e023368a093c7aedfd52481718c9da
Nathalia Zebral <natzebral@gmail.com>
2021-03-17 10:22:58 -0300

    tentativa requisito 8

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttonNewToDo = document.querySelector('#criar-tarefa');
 const buttonCleanAll = document.querySelector('#apaga-tudo');
 const buttonRemoveFinished = document.querySelector('#remover-finalizados');
 const input = document.querySelector('#texto-tarefa');
+const ol = document.querySelector('#lista-tarefas');
+const li = document.querySelectorAll('li');
 
 function createNewTodo() {
   var newLi = document.createElement('li');
@@ -11,20 +13,29 @@ function createNewTodo() {
 }
 
 buttonNewToDo.addEventListener('click',createNewTodo);
-
+// as funções para os requisitos 7 e 8 foram feitas com base no código do colega Lucas Godoi;
 function itemListSelect() {
-  const ol = document.querySelectorAll('#lista-tarefas')[0];
   ol.addEventListener('click', () => {
     const li = document.querySelectorAll('li');
     for(let index = 0; index < li.length; index += 1) {
       li[index].style.backgroundColor = 'rgb(128, 128, 128)';
-      const color ='rgb(128, 128,128)';
-      if (li[index].style.backgroundColor === color){
-        li[index].style.backgroundColor = 'rgb(0, 161, 157)';
-      } else {
+      const color ='rgb(128, 128, 128)';
         li[index].style.backgroundColor = color;
+      }
+  });
+}
+itemListSelect();
+
+function selectItem() {
+  ol.addEventListener('click', (event) => {
+    const color = event.target;
+    const li = document.querySelectorAll('li');
+    for(let index = 0; index < li.length; index += 1) {
+      li[index].style.backgroundColor = document.body.style.backgroundColor;
+      if (color.localName === 'li') {
+        color.style.backgroundColor = 'rgb(128, 128, 128)';
       }
     }
   });
 }
-itemListSelect();
+selectItem();
