9e1bfc82dd29f988e85f3ec473220f1928272640
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-03-17 15:01:57 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function inputElement(){
     const tagInput = document.querySelector('#texto-tarefa');
     const tagLi = document.createElement('li');
     tagLi.innerText = tagInput.value;
+    tagLi.classList.add('elemento')
     const tagOl = document.querySelector('#lista-tarefas');
     tagOl.appendChild(tagLi);
     tagInput.value = null;
@@ -11,4 +12,16 @@ function createAssignment() {
     const button = document.querySelector('#criar-tarefa');
     button.addEventListener('click', inputElement) 
 }
+
 createAssignment();
+
+function selectBackgroundColor (event) {
+    const clickAct = event.target;
+    clickAct.style.backgroundColor = 'rgb(128, 128, 128)'
+  }
+  
+  function changeBackgroundColor () {
+    const olElement = document.getElementById('lista-tarefas');
+    olElement.addEventListener('click',selectBackgroundColor);
+  }
+  changeBackgroundColor();
