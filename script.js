bc74159759ab2737f8de9e7c721f13731b8d3298
Hernanne <hernannedomiciano@gmail.com>
2021-03-17 09:58:54 -0300

    exercicio 8/9

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const button = document.getElementById('criar-tarefa')
+const button = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas'); // Variaveis Gerais
 const textoTarefa = document.getElementById('texto-tarefa');
 
 // criar botão
 function click() {
-  const buttonAction = document.createElement('li')
+  const buttonAction = document.createElement('li');
   buttonAction.innerText = textoTarefa.value;
   listaTarefas.appendChild(buttonAction);
   textoTarefa.value = '';
-  buttonAction.className = 'list'
 }
 button.addEventListener('click', click);
 
-const list = document.querySelector('.list')
-function listColor(event) {
-  event.target.style.backgroundColor = 'rgb(128,128,128)'
+function colorList(e){
+  const list = document.getElementsByTagName('li');
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  e.target.id = 'isSelected'
+  for (let index = 0; index < list.length; index += 1) {
+    if (list[index] !== e.target) {
+      list[index].style.backgroundColor = 'transparent';
+      list[index].id = '';
+    }
+  }
 }
+listaTarefas.addEventListener('click', colorList);
 
-listaTarefas.addEventListener('click', listColor)
+listaTarefas.addEventListener('dblclick', function (e){
+  if (e.target.className !== 'completed') {
+    e.target.className = 'completed'
+  } else {
+    e.target.className = '';
+  }
+});
