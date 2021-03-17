d648aad31014f7ca318656ae70c781885ce6e92c
Villefor <franklincrius@gmail.com>
2021-03-17 01:10:52 -0300

    reconfigurando

--- script.js
+++ script.js
@@ -2,11 +2,32 @@ function liCreation (){
     let li = document.querySelector('#criar-tarefa');
     let getOlList = document.querySelector('#lista-tarefas')
     li.addEventListener('click', () => {
-        let Lcreate = document.createElement('li');
-        let writeText = document.getElementById('texto-tarefa').value;
+        let Lcreate = document.createElement('li')
+        let writeText = document.querySelector('#texto-tarefa').value;
         Lcreate.innerHTML = writeText;
         getOlList.appendChild(Lcreate);
-        writeText = document.getElementById('texto-tarefa').value;('')
+        document.getElementById('texto-tarefa').value = '';
     })
 }
 liCreation()
+
+function grayChange (colorClick){
+    colorClick.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function eraseColor () {
+ let button = document.querySelector(".list-class");
+ button.addEventListener("click", function() {
+    for (index in button) {
+    if (button[index].style.backgroundColor == 'rgb(128, 128, 128)') {
+        button[index].style.backgroundColor = 'white';
+    }
+   }
+  });
+}
+
+
+
+
+
+
