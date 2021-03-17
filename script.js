13f35a728b64addf2a61818b0bdc277903d86929
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-03-16 23:53:50 -0300

    inclusao do quinto requisito

--- script.js
+++ script.js
@@ -3,18 +3,19 @@
 let incluseTask = document.getElementById('criar-tarefa');
 incluseTask.addEventListener('click', taskAdd);
 
-//incluir tarefa na lista
-let taskList = [];
+
 function taskAdd () {
 //recupera o input em uma variavel
-    let valueInput = document.getElementById('texto-tarefa').value;
+    let valueInput = document.createTextNode(document.getElementById('texto-tarefa').value);
 //incluir no html
     let originaList = document.getElementById('lista-tarefas')
     let listAdd = document.createElement('li');
+    
     originaList.appendChild(listAdd);
     listAdd.appendChild(valueInput);
-//apagar input
     
+//apagar input
+    document.getElementById('texto-tarefa').value = '';
 }
 
 
