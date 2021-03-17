74c20c7d44668419470cad692dc7bb4a262ff1da
Nathalia Zebral <natzebral@gmail.com>
2021-03-16 22:35:17 -0300

    tentativa requisito 7

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const buttonNewToDo = document.querySelector('#criar-tarefa');
 const buttonCleanAll = document.querySelector('#apaga-tudo');
 const buttonRemoveFinished = document.querySelector('#remover-finalizados');
 const input = document.querySelector('#texto-tarefa');
-const orderedList = document.querySelectorAll('ol');
 
 function createNewTodo() {
   var newLi = document.createElement('li');
@@ -13,3 +12,19 @@ function createNewTodo() {
 
 buttonNewToDo.addEventListener('click',createNewTodo);
 
+function itemListSelect() {
+  const ol = document.querySelectorAll('#lista-tarefas')[0];
+  ol.addEventListener('click', () => {
+    const li = document.querySelectorAll('li');
+    for(let index = 0; index < li.length; index += 1) {
+      li[index].style.backgroundColor = 'rgb(128, 128, 128)';
+      const color ='rgb(128, 128,128)';
+      if (li[index].style.backgroundColor === color){
+        li[index].style.backgroundColor = 'rgb(0, 161, 157)';
+      } else {
+        li[index].style.backgroundColor = color;
+      }
+    }
+  });
+}
+itemListSelect();
