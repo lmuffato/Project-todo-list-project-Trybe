51bc9f4332ffd5214ed44a3310725f4e5d90e01d
Debora Passos <deborinhapassos@gmail.com>
2021-03-16 22:35:59 -0300

    req 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const inputText = document.getElementById('texto-tarefa');
-let buttonTarefa = document.getElementById('criar-tarefa');
+const buttonTarefa = document.getElementById('criar-tarefa');
 const olTarefas = document.getElementById('lista-tarefas');
 
-function criar () {
+function criar (){
   const lista = document.createElement('li');
   const node = document.createTextNode(inputText.value);
   lista.appendChild(node);
+  lista.classList.add('listItem');
   olTarefas.appendChild(lista);
   inputText.value = '';
 }
-  buttonTarefa.addEventListener('click', criar);
+buttonTarefa.addEventListener('click', criar);
+
+// req 7
+function changeBg(event){
+  const liItem = document.getElementsByTagName('li');
+  for (let i = 0; i < liItem.length; i += 1){
+    liItem[i].style.backgroundColor = 'white';
+  }
+  const itemClick = event.target;
+  itemClick.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+olTarefas.addEventListener('click', changeBg);
