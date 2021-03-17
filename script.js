c80b3aad8b7491232360ec5156301db7f1fc79f7
camiloLelis <lafirmafuerte@gmail.com>
2021-03-16 22:50:00 -0300

    project-todo-list- requisito 8

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ function create() {
 btn.addEventListener('click', create);
 
 
-function marcar(event){
-    const array = document.querySelectorAll('#lista-tarefas');
-    for(let index = 0; index < array.length; index++){
-        const element = array[index];
-        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+function reset() {
+    const array = document.querySelectorAll('#lista-tarefas li');
+    for (let index = 0; index < array.length; index++) {
+        array[index].style.backgroundColor = "";   
+           
     }
 }
 
+function marcar(event) {
+    const array = document.querySelectorAll('#lista-tarefas');
+    reset(); 
+    for (let index = 0; index < array.length; index++) {
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        /* console.log(event.target);
+        console.log(array[index]); */
+    }
+}
 lista.addEventListener('click', marcar);
