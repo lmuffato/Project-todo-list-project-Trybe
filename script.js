e5aa22307fd78746a862e9e3477d6798e1b92a61
Apoc35 <alexv_silveira@outlook.com>
2021-03-17 20:47:16 -0300

    Adição do botão e suas funcionalidades.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function orderedlist(){
-    const ul = document.getElementById('lista-tarefas')
+const taskButton = document.getElementById('criar-tarefa');
+const inputButton = document.getElementById('texto-tarefa');
+const orderedList = document.getElementById('lista-tarefas');
+
+
+
+
+function mainButton() {
+  taskButton.addEventListener('click', () => {
+   let liList = document.createElement('li');
+    liList.innerText = inputButton.value;
+    orderedList.appendChild(liList);
+    inputButton.value = '';
+  });
 }
+mainButton();
