581882bcfb77c6d9ef15489db648b2b0a1fed506
Lucas <lucasml@live.com>
2021-03-17 20:52:48 -0300

    criterio 5

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-1
+const ol = document.querySelector("#lista-tarefa");
+const inputText = document.getElementById('texto-tarefa');
+
+
+
+
+function newTask() {
+  const btnNewTask = document.getElementById('criar-tarefa');
+  btnNewTask.addEventListener('click', () => {
+    const li = document.createElement('li');
+    ol.appendChild(li);
+    li.innerText=inputText.value;
+
+
+  })
+}
+newTask();
