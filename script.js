73177ac11693b40e21cc374b99e0ee5d5dfa2b9c
diogomarin <diogoamarin@gmail.com>
2021-03-18 12:16:21 -0300

    Meeting the mandatory requirements

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
-const createTaskButton = document.getElementById("criar-tarefa");
+const createTaskButton = document.getElementById('criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const clearButton = document.getElementById('apaga-tudo');
+const clearFinishButton = document.getElementById('remover-finalizados');
 
-createTaskButton.addEventListener('click', function() {
+function createTaskList() {
   const input = document.getElementById('texto-tarefa');
   const inputTask = input.value;
   if (inputTask.length > 0) {
-    let taskListItem = document.createElement('li');
+    const taskListItem = document.createElement('li');
     taskListItem.innerText = inputTask;
     taskList.appendChild(taskListItem);
     input.value = '';
   }
-});
+}
 
-taskList.addEventListener('click', (e) => {
+function changeBackgroundColor(e) {
   for (let i = 0; i < taskList.children.length; i += 1) {
     if (taskList.children[i] !== e.target) {
-      taskList.children[i].style.backgroundColor = document.body.style.backgroundColor;
-      e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      taskList.children[i].style.backgroundColor = 'white';
     }
   }
-});
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
 
-taskList.addEventListener('dblclick', (e) => {
+function completedTask(e) {
   const targetEvent = e.target;
   if (targetEvent.className !== 'completed') {
     targetEvent.className = 'completed';
   } else {
-    targetEvent.className = 'no-completed'
+    targetEvent.className = 'no-completed';
   }
-});
+}
 
+function removeAllTasks() {
+  while (taskList.firstChild) {
+    taskList.firstChild.remove();
+  }
+}
 
+function removeFinishTasks() {
+  const taskFinish = document.querySelectorAll('.completed');
+  for (let i = taskFinish.length - 1; i >= 0; i -= 1) {
+    if (taskFinish[i].className === 'completed') {
+      taskFinish[i].remove();
+    }
+  }
+}
 
-
+createTaskButton.addEventListener('click', createTaskList);
+taskList.addEventListener('click', changeBackgroundColor);
+taskList.addEventListener('dblclick', completedTask);
+clearButton.addEventListener('click', removeAllTasks);
+clearFinishButton.addEventListener('click', removeFinishTasks);
