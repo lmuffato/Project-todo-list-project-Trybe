9d777445a4e0921dbfa4b0328b9aa46db6052a08
Priscila <prihscapin@gmail.com>
2021-03-17 21:47:58 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let lista = document.querySelector('#lista-tarefas');
 let input = document.querySelector('#texto-tarefa');
+
 function criaLI() {
   const botao = document.querySelector(".botao");
   botao.addEventListener("click", (evento) => {
@@ -15,22 +16,19 @@ function criaLI() {
 }
 criaLI();
 
-
-
-// Requisito 7 - Pinta tarefas de cinza
-
+// Requisito 7 - Pinta tarefa
 function pintaTarefa (){
-  const lista = document.getElementsByTagName('ol')[0];
-  const tarefa = document.getElementsByTagName('li');
-    lista.addEventListener('click', (event) => {
-        const targetEvent = event.target;
-        for (let index = 0; index < tarefa.length; index += 1) {
-            tarefa[index].style.backgroundColor = document.body.style.backgroundColor;
-            if (targetEvent.localName === 'li') {
-                targetEvent.style.backgroundColor = 'rgb(128, 128, 128)';
-            }
-        }
-    });
+const lista = document.getElementsByTagName('ol')[0];
+const tarefa = document.getElementsByTagName('li');
+  lista.addEventListener('click', (event) => {
+      const targetEvent = event.target;
+      for (let index = 0; index < tarefa.length; index += 1) {
+          tarefa[index].style.backgroundColor = document.body.style.backgroundColor;
+          if (targetEvent.localName === 'li') {
+              targetEvent.style.backgroundColor = 'rgb(128, 128, 128)';
+          }
+      }
+  });
 }
 
 pintaTarefa();
@@ -38,16 +36,27 @@ pintaTarefa();
 // Requisito 8 - Apenas 1 item selecionado por vez.
 
 function selecionaTarefa() {
-  const tarefa = document.getElementsByTagName('li');
-  const lista = document.getElementsByTagName('ol')[0];
-  
+   let lista = document.querySelector('#lista-tarefas');
   for (let index = 0; index < lista.length; index += 1) {
-  lista[index].addEventListener('click',() => {
-     for (let i = 0; i < cores.length; i += 1) {
-     lista[i] .classList.remove('selected');
-     }
-     lista[index].classList.add('selected');
- })
+  lista[index].addEventListener('dbclick',() => {
+    for (let i = 0; i < lista.length; i += 1) {
+    lista[i] .style.textDecoration = line-through;
+     //}
+     //lista[index].textDecoration: none;
+ }
+})
 }
 }
-selecionaTarefa();
+
+// Requisito 9 - Botão apaga tarefas
+
+function limpaTarefas () {
+  const botaoApagar = document.getElementById('apaga-tudo');
+  let lista = document.querySelector('#lista-tarefas');
+  botaoApagar.addEventListener('click', () => {
+  const textContent = document.querySelector('#lista-tarefas');
+  textContent.innerHTML = '';   
+  })
+}    
+  
+  limpaTarefas()
