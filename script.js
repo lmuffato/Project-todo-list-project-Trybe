e04c1d38f83a4658bfe11714e93a5af40a9a4a79
carlos25xrm <carlos25xrm@gmail.com>
2021-03-18 11:32:05 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Faço questão de deixar registrado que
+  /* Faço questão de deixar registrado que
 O aluno Tiago Santos - Turma 10 - Tribo A
 me ajudou a raciocinar sobre essa função criarTarefa().
 Muito obrigado Tiago Santos - Turma 10 - Tribo A*/
@@ -7,7 +7,7 @@ function criarTarefa() {
   const botao = document.getElementById('criar-tarefa');
   botao.addEventListener('click', (evento) => {
     const listaTarefas = document.getElementById('lista-tarefas');
-    let criarItem = document.createElement('li');
+    const criarItem = document.createElement('li');
     criarItem.innerText = document.getElementById('texto-tarefa').value;
     listaTarefas.appendChild(criarItem).innerHTML;
     document.getElementById('texto-tarefa').value;
@@ -15,3 +15,32 @@ function criarTarefa() {
   });
 }
 criarTarefa();
+
+
+
+
+function mudarCor(evento) {
+  const local = document.querySelector('.selected');
+  if (local !== null) 
+    local.classList.remove('selected');
+  
+  if (evento.target.localName === 'li')
+    evento.target.classList.add('selected');
+
+}
+
+const ondeProcurar = document.getElementById('lista-tarefas')
+
+ondeProcurar.addEventListener('click', mudarCor);
+
+
+
+function duploClick(evento) {
+  console.log(evento);
+  if (evento.target.localName === 'li') {
+    evento.target.classList.toggle('completed');
+  }
+  
+}
+
+ondeProcurar.addEventListener('dblclick', duploClick);
