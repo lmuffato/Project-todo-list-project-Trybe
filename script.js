0ae2e474d0f727ee0f6ec8b824c0b70734414faa
Nathalia Zebral <natzebral@gmail.com>
2021-03-18 13:26:13 -0300

    alterar identaçao e mudei a const li de linha no codigo

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ const buttonCleanAll = document.querySelector('#apaga-tudo');
 const buttonRemoveFinished = document.querySelector('#remover-finalizados');
 const input = document.querySelector('#texto-tarefa');
 const ol = document.querySelector('#lista-tarefas');
-const li = document.querySelectorAll('li');
 
 function createNewTodo() {
-  let newLi = document.createElement('li');
+  const newLi = document.createElement('li');
   newLi.innerText = input.value;
   document.getElementById('lista-tarefas').appendChild(newLi);
   input.value = '';
 }
 
 buttonNewToDo.addEventListener('click', createNewTodo);
+
+const li = document.querySelectorAll('li');
+
 // as funções para os requisitos foram feitas com base no código do colega Lucas Godoi;
 function itemListSelect() {
   ol.addEventListener('click', () => {
-    li;
     for (let index = 0; index < li.length; index += 1) {
       li[index].style.backgroundColor = 'rgb(128, 128, 128)';
       const color = 'rgb(128, 128, 128)';
@@ -29,7 +30,6 @@ itemListSelect();
 function selectItem() {
   ol.addEventListener('click', (event) => {
     const color = event.target;
-    const li = document.querySelectorAll('li');
     for (let index = 0; index < li.length; index += 1) {
       li[index].style.backgroundColor = document.body.style.backgroundColor;
       if (color.localName === 'li') {
@@ -63,9 +63,9 @@ clearAll();
 
 function clearToDoDone() {
   buttonRemoveFinished.addEventListener('click', () => {
-  const classDone = document.querySelectorAll('.completed');
-  for (let index = 0; index < classDone.length; index += 1) {
-    ol.removeChild(classDone[index]);
+    const classDone = document.querySelectorAll('.completed');
+    for (let index = 0; index < classDone.length; index += 1) {
+      ol.removeChild(classDone[index]);
     }
   });
 }
