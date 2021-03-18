b3051fba27abc08f8f16f4254c20e6ec42977dd2
Renata Andrade <reapminas@gmail.com>
2021-03-17 21:49:32 -0300

    Requisito 9 - Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function overwriteText(event) {
+  const { target } = event;
+  // Verificando se já tenho a classe dentro dos elementos da lista:  https://pt.stackoverflow.com/questions/9605/detectar-se-um-elemento-cont%C3%A9m-uma-classe-com-javascript-puro
+  if ((target.classList.contains('completed')) === true) {
+    // Removendo a classe do elemento: https://www.w3schools.com/howto/howto_js_remove_class.asp
+    target.classList.remove('completed');
+  } else {
+    target.classList.add('completed');
+  }
+}
+
 function changesColor(event) {
   const { target } = event;
   const listItems = document.getElementsByTagName('li');
@@ -13,6 +24,10 @@ function receiveClick() {
 
   listItem.addEventListener('click', changesColor);
 
+  // Criando um evento com 2 clicks para os itens adicionados na lista: https://www.w3schools.com/jsref/event_ondblclick.asp
+  listItem.addEventListener('dblclick', overwriteText);
+
+  // Colocando o item da lista dentro do ol e adicionando o texto capturado no input dentro do li: https://www.w3schools.com/jsref/met_document_createtextnode.asp
   document.getElementById('lista-tarefas').appendChild(listItem);
   listItem.appendChild(document.createTextNode(textElement));
 
