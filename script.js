dc24b2a4f771e8fc54d5c037ed49c376ed465af7
Apoc35 <alexv_silveira@outlook.com>
2021-03-17 22:58:29 -0300

    Resolução requisito 7.

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const taskButton = document.getElementById('criar-tarefa');
 const inputButton = document.getElementById('texto-tarefa');
 const orderedList = document.getElementById('lista-tarefas');
 
-
-
-
 function mainButton() {
   taskButton.addEventListener('click', () => {
    let liList = document.createElement('li');
+   liList.className = 'list-order';
     liList.innerText = inputButton.value;
     orderedList.appendChild(liList);
     inputButton.value = '';
   });
 }
 mainButton();
+
+function backColor() {
+    const liList = document.getElementsByClassName('list-order');
+    orderedList.addEventListener('click', (event) => {
+        event.target.classList.add('backColor');
+    });
+}
+backColor();
