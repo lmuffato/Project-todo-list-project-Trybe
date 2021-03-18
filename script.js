7aa4d265a094a0ba5b5976dbfc4789c122689840
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-17 21:54:39 -0300

    corrigindo criar tarefa

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
-
-
-function criarTarefa () {
-    
+function criarTarefa () { 
    var criar = document.getElementById("texto-tarefa");
    var tarefa = criar.value;
    var ol = document.getElementById("lista-tarefas");
@@ -11,24 +8,19 @@ function criarTarefa () {
    var texto = document.createTextNode(tarefa);
    li.appendChild(texto);
    ol.appendChild(li);
-
 }
 
 var btAdicionar = document.getElementById("criar-tarefa");
 btAdicionar.addEventListener("click", criarTarefa);
 
-
 function apagar () {
     var ol = document.getElementById("lista-tarefas");
     let li = document.getElementsByClassName("listaDinamica");
     if(ol.childElementCount > 0) { 
-       for (let index = 0; index < li.length; index++) {
-           
+       for (let index = 0; index < li.length; index++) {   
            ol.removeChild(li[index]);
        }
     }
-
 }
-
 var btnApagar = document.getElementById("apaga-tudo");
 btnApagar.addEventListener("click", apagar);
