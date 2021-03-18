98cafc379746f6dd77ef48b65451ff2237a007aa
LucasMLara <lucasml@live.com>
2021-03-18 05:50:45 -0300

    correct id input textotarefa

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ol = document.querySelector("#lista-tarefa");
-const inputText = document.getElementById('texto-tarefas');
+const inputText = document.getElementById('texto-tarefa');
 
 
 
@@ -9,16 +9,21 @@ function newTask() {
   btnNewTask.addEventListener('click', () => {
     const li = document.createElement('li');
     ol.appendChild(li);
-    li.innerText=inputText.value;
-    li.addEventListener('click', () =>{
+    li.innerText = inputText.value;
+
+    li.addEventListener('click', () => {
       const lis = document.querySelectorAll('li');
-      for (index=0; index < lis.length; index+=1) {
+      for (index = 0; index < lis.length; index += 1) {
         if (lis[index].style.backgroundColor = 'rgb(128, 128, 128)') {
           lis[index].style.backgroundColor = '';
         }
       }
       li.style.backgroundColor = 'rgb(128, 128, 128)';
-    })
+    });
+
+    li.addEventListener('dblclick', () => {
+      li.classList.toggle('completed');
+    });
   })
 }
 newTask();
