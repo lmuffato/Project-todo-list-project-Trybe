9b800f0ac9bea7a3d568ee51fff91f512ce584e9
pmaiamateus <pmaiamateus@gmail.com>
2021-03-18 08:52:25 -0300

    conserta script

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // adiciona tarefas
 function addTask() {
   const button = document.querySelector('button');
-  button.addEventListener('click', function () {
+  button.addEventListener('click', function() {
     const textInputted = document.querySelector('input').value;
     const newTask = document.createElement('li');
     newTask.className = 'task';
     newTask.innerText = textInputted;
-    document.querySelector('#lista-tarefas').appendChild(task);
+    document.querySelector('#lista-tarefas').appendChild(newTask);
     document.querySelector('input').value = null;
   });
 }
 
-addTask()
+addTask();
