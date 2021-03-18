fb97f200b671adc965d08a95621190e0315720be
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-18 00:02:44 -0300

    calterando estilo e corrigindo erros linst

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-
-function criarTarefa () { 
+function criarTarefa() { 
    var criar = document.getElementById("texto-tarefa");
    var tarefa = criar.value;
    var ol = document.getElementById("lista-tarefas");
    var li = document.createElement("li");
    li.classList.add("listaDinamica");
    var texto = document.createTextNode(tarefa);
-   li.addEventListener("dblclick", function(e){
-         riscarLista(e);
+   li.addEventListener("dblclick", function(e) {
+         riscarLista(e);    
+   });
+   li.addEventListener("click", function(e) {
+       coloriLista (e);
    });
    li.appendChild(texto);
    ol.appendChild(li);
    criar.value = "";
-
 }
-
 var btAdicionar = document.getElementById("criar-tarefa");
 btAdicionar.addEventListener("click", criarTarefa);
 
-function apagar () {
+function apagar() {
     var ol = document.getElementById("lista-tarefas");
-   
     if(ol.childElementCount > 0) { 
     ol.innerHTML = "";
     }
@@ -28,13 +27,18 @@ function apagar () {
 var btnApagar = document.getElementById("apaga-tudo");
 btnApagar.addEventListener("click", apagar);
 
-function riscarLista (li){
+function riscarLista (li) {
   if(li.target.classList.contains("completed")){
     li.target.classList.remove("completed");
   }
   else{
     li.target.classList.add("completed"); 
-  }
-   
+  }  
 }
-   
+
+function coloriLista (colorir) {
+    if(!colorir.target.classList.contains("backgroudLista")){
+        colorir.target.classList.add("backgroudLista");
+    }    
+}
+ 
