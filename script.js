7fc320365358c0838f9f9709659c4490732cb9a4
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-18 00:11:10 -0300

    corrigindo erros linst

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 function criarTarefa() { 
-   var criar = document.getElementById("texto-tarefa");
-   var tarefa = criar.value;
-   var ol = document.getElementById("lista-tarefas");
-   var li = document.createElement("li");
-   li.classList.add("listaDinamica");
-   var texto = document.createTextNode(tarefa);
-   li.addEventListener("dblclick", function(e) {
-         riscarLista(e);    
+  let criar = document.getElementById("texto-tarefa");
+  let tarefa = criar.value;
+  let ol = document.getElementById("lista-tarefas");
+  let li = document.createElement("li");
+  li.classList.add("listaDinamica");
+  let texto = document.createTextNode(tarefa);
+  li.addEventListener("dblclick", function(e) {
+    riscarLista(e);    
    });
-   li.addEventListener("click", function(e) {
-       coloriLista (e);
+  li.addEventListener("click", function(e) {
+    coloriLista (e);
    });
-   li.appendChild(texto);
-   ol.appendChild(li);
-   criar.value = "";
+  li.appendChild(texto);
+  ol.appendChild(li);
+  criar.value = "";
 }
-var btAdicionar = document.getElementById("criar-tarefa");
+let btAdicionar = document.getElementById("criar-tarefa");
 btAdicionar.addEventListener("click", criarTarefa);
 
 function apagar() {
-    var ol = document.getElementById("lista-tarefas");
-    if(ol.childElementCount > 0) { 
+  let ol = document.getElementById("lista-tarefas");
+   if (ol.childElementCount > 0) {
     ol.innerHTML = "";
-    }
+  }
 }
-var btnApagar = document.getElementById("apaga-tudo");
+let btnApagar = document.getElementById("apaga-tudo");
 btnApagar.addEventListener("click", apagar);
 
 function riscarLista (li) {
-  if(li.target.classList.contains("completed")){
+  if (li.target.classList.contains("completed")){
     li.target.classList.remove("completed");
   }
-  else{
+  else {
     li.target.classList.add("completed"); 
   }  
 }
 
 function coloriLista (colorir) {
-    if(!colorir.target.classList.contains("backgroudLista")){
-        colorir.target.classList.add("backgroudLista");
-    }    
+   if(!colorir.target.classList.contains("backgroudLista")){
+     colorir.target.classList.add("backgroudLista");
+  }    
 }
- 
