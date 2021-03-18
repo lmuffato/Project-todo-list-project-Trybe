def65d5386c2e84309097bf11fc7ff2260b168db
Apoc35 <alexv_silveira@outlook.com>
2021-03-18 01:16:56 -0300

    Resolução requisito 8.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskButton = document.getElementById('criar-tarefa');
 const inputButton = document.getElementById('texto-tarefa');
 const orderedList = document.getElementById('lista-tarefas');
+const liList = document.getElementsByClassName('list-order');
 
 function mainButton() {
   taskButton.addEventListener('click', () => {
@@ -14,9 +15,15 @@ function mainButton() {
 mainButton();
 
 function backColor() {
-    const liList = document.getElementsByClassName('list-order');
+    const liList = document.getElementsByTagName('li');
     orderedList.addEventListener('click', (event) => {
-        event.target.classList.add('backColor');
+        const click = event.target;
+        for (let index = 0; index < liList.length; index += 1){
+            liList[index].style.backgroundColor = document.body.style.backgroundColor;
+        if (click.localName === 'li'){
+            click.style.backgroundColor = 'rgb(128, 128, 128)';
+        }
+        }
     });
 }
 backColor();
