76e7aebb34be5e1a06b420b728cbb9cafa1f2b2f
diegoCorreia <diegomethal@gmail.com>
2021-03-18 21:29:56 -0300

    requisito 7 corrigido

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const button = document.querySelector('#criar-tarefa'); // seleciona o botão  
+const button = document.querySelector('#criar-tarefa'); // seleciona o botão
 const todoList = document.querySelector('#lista-tarefas'); // seleciona a <ol>
-button.addEventListener('click', () => { // cria um evento de botão p/ ligar o texto digitado ao botão 
-  const insertText = document.getElementById('texto-tarefa').value; // pega o valor do input 
+button.addEventListener('click', () => { // cria um evento de botão p/ ligar o texto digitado ao botão
+  const insertText = document.getElementById('texto-tarefa').value; // pega o valor do input
   document.getElementById('texto-tarefa').value = ''; // passa p/ value um valor vazio
   const textList = document.createElement('li'); // cria o elemento <li>
-  textList.innerText = insertText; // vincula o texto da <li> ao input     
-  todoList.appendChild(textList); // add  a <li> com o texto do input a <ol> 
-  if (todoList.contains(textList)){
+  textList.innerText = insertText; // vincula o texto da <li> ao input
+  todoList.appendChild(textList); // add  a <li> com o texto do input a <ol>
+  if (todoList.contains(textList)) {
     textList.addEventListener('click', () => {
-      textList.style.backgroundColor = 'rgb(128,128,128)';
+      textList.style.backgroundColor = 'gray';
       });
-  } else {
-    textList.style.backgroundColor = '';
-  }
+    } 
 });
-
