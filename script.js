f6356f5dec51c5ad580b1ba0a9a1b92a130cf132
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-19 20:14:44 -0300

    enviando o ultimo requisito

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function selectItem(eventOrigin) {
-  const selected = document.querySelector('.list-item');
-  const listItem = eventOrigin.target;
+// Funcao que aplica a classe que altera o background color para 'gray' ao item clicado e
+// verifica se há outro elemento com essa classe e se ouver remove dele
+function selectedTask(eventOrigin) {
+  const selected = document.querySelector('.selected-task');
+  const listItem = eventOrigin.target; // Recupera o elemento que foi clicado (possui um listener de click)
 
   if (selected != null) {
-    selected.classList.remove('list-item');// Remove a classe list-item caso exista em algum elemento
+    selected.classList.remove('selected-task');// Remove a classe selected-task caso exista em algum elemento
   }
-  listItem.classList.add('list-item');// Adiciona a classe list-item ao elemento clicado
+  listItem.classList.add('selected-task');// Adiciona a classe selected-task ao elemento clicado
 }
 
+// Funcao que adiciona um 'risco' na tarefa clicada 2x para indicar que está completa
 function throughItem(eventOrigin) {
   const elementClasses = eventOrigin.target.classList.value;// Recupera uma string com o nome das classes do elemeto
   const listItem = eventOrigin.target;
@@ -18,22 +21,25 @@ function throughItem(eventOrigin) {
   }
 }
 
+// Funcao que serve para adicionar o texto do input como um item da lista de tarefas
 function submitTask() {
   const taskList = document.getElementById('lista-tarefas');
   const newTask = document.createElement('li');
-  const taskText = document.formTasks.task.value;
-  newTask.addEventListener('click', selectItem);
+  const taskText = document.formTasks.task.value;// Recupera o texto dentro do input para colocar na nova li
+  newTask.addEventListener('click', selectedTask);
   newTask.addEventListener('dblclick', throughItem);
   newTask.innerText = taskText;
   taskList.appendChild(newTask);
-  document.formTasks.task.value = '';
+  document.formTasks.task.value = ''; // Apaga o texto no imput apos salvar na lista
 }
 
+// Funcao que remove todos o conteudo do elemento que representa a lista
 function clearTasks() {
   const taskList = document.getElementById('lista-tarefas');
   taskList.innerHTML = '';
 }
 
+// Funcao que remove apenas as tarefas completas que sao os itens com a classe 'completed'
 function removeFinnished() {
   let taskList = document.getElementById('lista-tarefas');
   let completedTasks = document.getElementsByClassName('completed');
@@ -45,6 +51,15 @@ function removeFinnished() {
   }
 }
 
+// Funcao que remove apenas a tarefa selecionada
+function removeSelected() {
+  let taskList = document.getElementById('lista-tarefas');
+  let selectedTask = document.querySelector('.selected-task');
+  taskList.removeChild(selectedTask);
+}
+
+// Funcao que salva as propriedades dos li (texto e classes) no localStorage, utilizando um
+// array de elementos
 function pushTasks() {
   const listItems = document.getElementsByTagName('li');
   let item = {};
@@ -60,57 +75,8 @@ function pushTasks() {
   localStorage.setItem('savedItems', JSON.stringify(itemPropeties));
 }
 
-function moveUp() {
-  const selected = document.querySelector('.list-item');
-  if (selected != null) {
-    const previous = selected.previousElementSibling;
-
-    if (previous != null) {
-      const taskList = document.getElementById('lista-tarefas');
-      const task = document.createElement('li');
-      task.innerText = selected.innerText;
-      task.classList.value = selected.classList.value;
-      task.addEventListener('click', selectItem);
-      task.addEventListener('dblclick', throughItem);
-      taskList.replaceChild(task, previous);
-      taskList.replaceChild(previous, selected);
-    }
-  }
-}
-
-function moveDown() {
-  const selected = document.querySelector('.list-item');
-  if (selected != null) {
-    const next = selected.nextElementSibling;
-  
-    if (next != null) {
-      const taskList = document.getElementById('lista-tarefas');
-      const task = document.createElement('li');
-      task.innerText = selected.innerText;
-      task.classList.value = selected.classList.value;
-      task.addEventListener('click', selectItem);
-      task.addEventListener('dblclick', throughItem);
-      taskList.replaceChild(task, next);
-      taskList.replaceChild(next, selected);
-    }
-  }
-}
-
-function addButtomListener() {
-  const button1 = document.getElementById('criar-tarefa');
-  const button2 = document.getElementById('apaga-tudo');
-  const button3 = document.getElementById('remover-finalizados');
-  const button4 = document.getElementById('salvar-tarefas');
-  const button5 = document.getElementById('mover-cima');
-  const button6 = document.getElementById('mover-baixo');
-  button1.addEventListener('click', submitTask);
-  button2.addEventListener('click', clearTasks);
-  button3.addEventListener('click', removeFinnished);
-  button4.addEventListener('click', pushTasks);
-  button5.addEventListener('click', moveUp);
-  button6.addEventListener('click', moveDown);
-}
-
+// Funcao que recupera as informacoes salvas pela pushTasks() no localStorage e usa para criar
+// os elementos 'li' como eram assim que foram salvos
 function pullTasks() {
   const saved = localStorage.getItem('savedItems');
   if (saved != null) {
@@ -122,11 +88,71 @@ function pullTasks() {
       newItem = document.createElement('li');
       newItem.innerText = itemProperties[i].text;
       newItem.classList.value = itemProperties[i].classes;
-      newItem.addEventListener('click', selectItem);
+      newItem.addEventListener('click', selectedTask);
       newItem.addEventListener('dblclick', throughItem);
       taskList.appendChild(newItem);
     }
   }
+}
+
+// Funcao que inverte a posicao da tarefa de cima com a de baixo (se houver uma acima)
+// A estratégia usada foi salvar o texto e classes da tarefa selecionada, criar uma nova li,
+// atribuir esses elementos nela e sobrescrever a li acima e como foi recuperada uma 'copia'
+// da li de cima em 'previous' antes de se sobrescrita, assim entao foi usada para sobrescrever
+// a de baixo, assim o resltado final foi a 'troca de posicao' das duas
+function moveUp() {
+  const selected = document.querySelector('.selected-task');
+  if (selected != null) {
+    const previous = selected.previousElementSibling;
+
+    if (previous != null) {
+      const taskList = document.getElementById('lista-tarefas');
+      const task = document.createElement('li');
+      task.innerText = selected.innerText;
+      task.classList.value = selected.classList.value;
+      task.addEventListener('click', selectedTask);
+      task.addEventListener('dblclick', throughItem);
+      taskList.replaceChild(task, previous);
+      taskList.replaceChild(previous, selected);
+    }
+  }
+}
+
+// Funcao quase identica a 'moveUp', so que agora o foco e o elemento abaixo do selecionado
+function moveDown() {
+  const selected = document.querySelector('.selected-task');
+  if (selected != null) {
+    const next = selected.nextElementSibling;
+  
+    if (next != null) {
+      const taskList = document.getElementById('lista-tarefas');
+      const task = document.createElement('li');
+      task.innerText = selected.innerText;
+      task.classList.value = selected.classList.value;
+      task.addEventListener('click', selectedTask);
+      task.addEventListener('dblclick', throughItem);
+      taskList.replaceChild(task, next);
+      taskList.replaceChild(next, selected);
+    }
+  }
+}
+
+// Funcao que adiciona 'escutadores' nos botoes
+function addButtomListener() {
+  const button1 = document.getElementById('criar-tarefa');
+  const button2 = document.getElementById('apaga-tudo');
+  const button3 = document.getElementById('remover-finalizados');
+  const button4 = document.getElementById('salvar-tarefas');
+  const button5 = document.getElementById('mover-cima');
+  const button6 = document.getElementById('mover-baixo');
+  const button7 = document.getElementById('remover-selecionado');
+  button1.addEventListener('click', submitTask);
+  button2.addEventListener('click', clearTasks);
+  button3.addEventListener('click', removeFinnished);
+  button4.addEventListener('click', pushTasks);
+  button5.addEventListener('click', moveUp);
+  button6.addEventListener('click', moveDown);
+  button7.addEventListener('click', removeSelected);
 }
 
 // Arrows ↑ ⬆ ↓ ⬇
