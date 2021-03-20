81313b29019eb7948b6473a1ecc66d27d757e323
diegoCorreia <diegomethal@gmail.com>
2021-03-20 18:25:33 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const button = document.querySelector('#criar-tarefa'); // seleciona o botão
-const todoList = document.querySelector('#lista-tarefas'); // seleciona a <ol>
+const button = document.querySelector('#criar-tarefa'); 
+const todoList = document.querySelector('#lista-tarefas'); 
 
 function taskList(){
-button.addEventListener('click', () => { // cria um evento de botão p/ ligar o texto digitado ao botão
-  const insertText = document.getElementById('texto-tarefa').value; // pega o valor do input
+button.addEventListener('click', () => { 
+  const insertText = document.getElementById('texto-tarefa').value; 
   document.getElementById('texto-tarefa').value = '';
-  const textList = document.createElement('li'); // cria o elemento <li>
-  textList.innerText = insertText; // vincula o texto da <li> ao input
+  const textList = document.createElement('li'); 
+  textList.innerText = insertText; 
   todoList.appendChild(textList); 
 });
 }
@@ -24,14 +24,13 @@ function changeColor(){
 }
 changeColor();
 
+function completeTask(){
+  todoList.addEventListener('dblclick', (e) =>{
+  e.target.classList.toggle('completed');
+});
+}
+completeTask();
+
 
  
  
-/*taskList();
-function changeColor(){
-const textList = document.querySelectorAll('li');
-textList.addEventListener('click', () => {
-textList.style.backgroundColor = 'rgb (255,255,255)';
-});
-}
-changeColor();*/
