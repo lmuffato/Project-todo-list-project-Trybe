9a5e0ee5d1bb2f968e799acdc843e2f882698212
Henrique Lima <hnamix@gmail.com>
2021-03-21 17:38:01 -0400

    requisito 06

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const bt = document.getElementById('criar-tarefa');
+const texto = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
 bt.addEventListener('click', addTarefa);
 
+
 function addTarefa () {
-  const texto = document.getElementById('texto-tarefa');
-  const lista = document.getElementById('lista-tarefas');
-  const li = document.createElement('li');
-  lista.appendChild(li);
-  li.innerText = document.getElementById('texto-tarefa').value;
-  console.log(li)
-  document.getElementById('texto-tarefa').innerText = ' ';
+  const itemlist = document.createElement('li');
+  listaTarefas.appendChild(itemlist);
+  itemlist.innerText = texto.value;
+  texto.value = '';
+  
 }
