d5061addc8aeffb0614316c1d17673ce0c38bab3
Junior Portela <portelajr14@gmail.com>
2021-03-21 01:24:02 -0300

    Ajustes requisitos 1 e 2

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const getInput = document.getElementById('texto-tarefa');
 const getList = document.getElementById('lista-tarefas');
 
 function InsertTask() {
+  if (getInput.value !== '') {
   const task = document.createElement('li');
   task.innerText = getInput.value;
   getList.appendChild(task);
+  }
+}
+
+function clearInput() {
+  getInput.value =''
 }
 
 getButton.addEventListener('click', InsertTask);
+getButton.addEventListener('click', clearInput);
