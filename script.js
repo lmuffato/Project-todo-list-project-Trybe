88d281435d0194cc4dfe9fc243a71f54bc36eae3
Junior Portela <portelajr14@gmail.com>
2021-03-21 16:17:33 -0300

    Requisitos 7 e 8: selecao unica ao clique em item da lista

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const getButton = document.querySelector('.botao-tarefa');
 const getInput = document.getElementById('texto-tarefa');
 const getList = document.getElementById('lista-tarefas');
 
-function InsertTask() {
+function clearSelected () {
+  const items = document.querySelectorAll('li');
+  for(let index = 0; index < items.length; index += 1) {
+    items[index].style.backgroundColor = 'transparent';
+  }
+}
+
+function selectItem() {
+  const painter = this.style;
+  painter.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function insertTask() {
   if (getInput.value !== '') {
-  const task = document.createElement('li');
-  task.innerText = getInput.value;
-  getList.appendChild(task);
+    const task = document.createElement('li');
+    task.innerText = getInput.value;
+    getList.appendChild(task);
+    task.addEventListener('click', clearSelected);
+    task.addEventListener('click', selectItem);
   }
 }
 
@@ -14,5 +28,5 @@ function clearInput() {
   getInput.value = '';
 }
 
-getButton.addEventListener('click', InsertTask);
+getButton.addEventListener('click', insertTask);
 getButton.addEventListener('click', clearInput);
