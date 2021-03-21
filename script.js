20323b6022c7747b7e2e3559735d6ef296c365d1
Junior Portela <portelajr14@gmail.com>
2021-03-21 00:56:31 -0300

    Ajuste de lint e lint styles

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const getButton = document.querySelector('.botao-tarefa');
 const getInput = document.getElementById('texto-tarefa');
 const getList = document.getElementById('lista-tarefas');
 
-getButton.addEventListener("click", InsertTask);
-
-function InsertTask () {
-  let task = document.createElement("li")
-  task.innerText = getInput.value; 
+function InsertTask() {
+  const task = document.createElement('li');
+  task.innerText = getInput.value;
   getList.appendChild(task);
-
 }
 
+getButton.addEventListener('click', InsertTask);
