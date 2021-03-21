637082379cf013c35b46509cfd3a33dbcddb71be
Junior Portela <portelajr14@gmail.com>
2021-03-21 17:23:25 -0300

    Requisito 9: dbclick risk/unrisk item list

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const getButton = document.querySelector('.botao-tarefa');
 const getInput = document.getElementById('texto-tarefa');
 const getList = document.getElementById('lista-tarefas');
 
-function clearSelected() {
+function clearSelecteds() {
   const items = document.querySelectorAll('li');
   for (let index = 0; index < items.length; index += 1) {
     items[index].style.backgroundColor = 'transparent';
   }
+}
+
+function riskItem() {
+  if (this.className == '') {
+    this.className = 'completed';
+  } else {
+    this.className = '';
+  } 
 }
 
 function selectItem() {
@@ -14,13 +22,16 @@ function selectItem() {
   painter.backgroundColor = 'rgb(128, 128, 128)';
 }
 
+
+
 function insertTask() {
   if (getInput.value !== '') {
     const task = document.createElement('li');
     task.innerText = getInput.value;
     getList.appendChild(task);
-    task.addEventListener('click', clearSelected);
+    task.addEventListener('click', clearSelecteds);
     task.addEventListener('click', selectItem);
+    task.addEventListener('dblclick', riskItem);
   }
 }
 
