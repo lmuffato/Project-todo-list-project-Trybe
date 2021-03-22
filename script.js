4e28f35c96f7827e75e06caf79d7d4d42751d010
Thiago <thiagodesouza.dev@gmail.com>
2021-03-22 11:05:36 -0300

    correçoes atividade 7

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ criarTarefa.addEventListener('click', criarNovaTarefa);
 function seletorItem() {
   listaTarefas.addEventListener('click', seletor);
   function seletor() {
-    li = document.querySelectorAll('li')
+   const li = document.querySelectorAll('li')
     for (let index = 0; index < li.length; index += 1) {
       li[index].style.backgroundColor = 'rgb(128, 128, 128)';
       const color = 'rgb(128, 128, 128)';
@@ -25,7 +25,7 @@ seletorItem();
 
 function listaConcluida() {
   listaTarefas.addEventListener('dblclick', riscada);
-  function riscada (event) {
+  function riscada(event) {
     if (event.target.classList.contains('completed')) {
       event.target.classList.remove('completed');
       event.target.style.textDecoration = '';
