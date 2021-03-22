2d7b7179c10b273fb72123de70a2bcf8b7deb75a
Thiago <thiagodesouza.dev@gmail.com>
2021-03-22 10:59:16 -0300

    atividade 6,7,9

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function criarNovaTarefa() {
   textoTarefa.value = '';
 }
 criarTarefa.addEventListener('click', criarNovaTarefa);
+
+function seletorItem() {
+  listaTarefas.addEventListener('click', seletor);
+  function seletor() {
+    li = document.querySelectorAll('li')
+    for (let index = 0; index < li.length; index += 1) {
+      li[index].style.backgroundColor = 'rgb(128, 128, 128)';
+      const color = 'rgb(128, 128, 128)';
+      li[index].style.backgroundColor = color;
+    }
+  };
+}
+seletorItem();
+
+function listaConcluida() {
+  listaTarefas.addEventListener('dblclick', riscada);
+  function riscada (event) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+      event.target.style.textDecoration = '';
+    } else {
+      event.target.classList.add('completed');
+      event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+    }
+  }
+}
+listaConcluida();
