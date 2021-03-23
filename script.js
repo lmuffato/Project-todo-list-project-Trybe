9461a32803c5da7d1d682b1ae95a6a5c059d0b48
biazidioti <bzferreira.bio@gmail.com>
2021-03-23 10:09:41 -0300

    Atualizacoes.

---
+++ script.js
@@ -0,0 +1,3 @@
+// No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
+
+// adicionar evento ao clicar no botão
