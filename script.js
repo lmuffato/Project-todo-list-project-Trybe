b8eb899a7d845e2b1df7e23ba2d53e8e7dc3eec3
defauth98 <neto.daniribeiro@gmail.com>
2021-03-24 13:46:50 -0300

    Ajustes nas funçoes dos botoes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const listaTarefas = 'lista-tarefas';
+
 function selectTask(item) {
   const taskItem = item;
   const tasksList = document.getElementsByClassName('task');
@@ -17,47 +18,44 @@ function toggleDoneTask(item) {
 }
 
 function clearTaskButton() {
-  const clearButtonElement = document.getElementById('apaga-tudo');
-
-  clearButtonElement.onclick = () => {
-    const taskList = document.getElementById(listaTarefas);
-    taskList.innerHTML = '';
-  };
+  const taskList = document.getElementById(listaTarefas);
+  taskList.innerHTML = '';
 }
 
 function createTask() {
-  const createTaskButton = document.getElementById('criar-tarefa');
-  createTaskButton.onclick = () => {
-    const taskList = document.getElementById(listaTarefas);
-    const taskInputName = document.getElementById('texto-tarefa');
+  const taskList = document.getElementById(listaTarefas);
+  const taskInputName = document.getElementById('texto-tarefa');
 
-    const taskItem = document.createElement('li');
-    taskItem.innerHTML = taskInputName.value;
-    taskItem.className = 'task';
-    taskItem.onclick = () => selectTask(taskItem);
-    taskItem.ondblclick = () => toggleDoneTask(taskItem);
+  const taskItem = document.createElement('li');
+  taskItem.innerHTML = taskInputName.value;
+  taskItem.className = 'task';
+  taskItem.onclick = () => selectTask(taskItem);
+  taskItem.ondblclick = () => toggleDoneTask(taskItem);
 
-    taskInputName.value = '';
-    taskList.appendChild(taskItem);
-  };
+  taskInputName.value = '';
+  taskList.appendChild(taskItem);
 }
 
 function removeDoneTask() {
-  const removeButtonElement = document.getElementById('remover-finalizados');
+  const taskList = document.getElementById(listaTarefas);
+  const taskItems = document.querySelectorAll('.completed');
+  for (let index = 0; index < taskItems.length; index += 1) {
+    taskList.removeChild(taskItems[index]);
+  }
+}
 
-  removeButtonElement.onclick = () => {
-    console.log('removeDoneTask');
-    const taskList = document.getElementById(listaTarefas);
-    const taskItems = document.querySelectorAll('.completed');
-    console.log(taskItems);
-    for (let index = 0; index < taskItems.length; index += 1) {
-      taskList.removeChild(taskItems[index]);
-    }
-  };
+function saveAllTasks() {
+
 }
 
 window.onload = () => {
-  createTask();
-  clearTaskButton();
-  removeDoneTask();
+  const createTaskButton = document.getElementById('criar-tarefa');
+  const removeButtonElement = document.getElementById('remover-finalizados');
+  const clearButtonElement = document.getElementById('apaga-tudo');
+  const saveTaskButtonElement = document.getElementById('salvar-tarefas');
+
+  removeButtonElement.onclick = removeDoneTask;
+  createTaskButton.onclick = createTask;
+  clearButtonElement.onclick = clearTaskButton;
+  saveTaskButtonElement.onclick = saveAllTasks;
 };
