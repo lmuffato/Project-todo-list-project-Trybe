1d4221661c1e75993ce0e8ed5d8e0b40720e829b
Henrique Lima <hnamix@gmail.com>
2021-03-24 11:06:23 -0400

    requisito 09

--- script.js
+++ script.js
@@ -12,9 +12,20 @@ function mudaCor(evento) {
   evento.target.style.background = 'rgb(128, 128, 128)';
 }
 
+function riscaTexto(evento) {
+  evento.target.classList.toggle('completed');
+  //for (let index = 0; index < listaTarefas.children.length; index += 1) {
+    //if ( listaTarefas.children[index].classList === 'completed') {
+      //listaTarefas.children[index].classList.toggle('completed');
+    //}
+    //listaTarefas.children[index].classList.add('completed');
+  //}
+}
+
 const bt = document.getElementById('criar-tarefa');
 const texto = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 
 bt.addEventListener('click', addTarefa);
 listaTarefas.addEventListener('click', mudaCor);
+listaTarefas.addEventListener('dblclick', riscaTexto);
