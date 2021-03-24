39c03c2b664e8b8428d11751973acf1b7fb1cc27
Henrique Lima <hnamix@gmail.com>
2021-03-24 12:01:28 -0400

    Requisito 10

--- script.js
+++ script.js
@@ -12,20 +12,23 @@ function mudaCor(evento) {
   evento.target.style.background = 'rgb(128, 128, 128)';
 }
 
+// Requisito 09 Contribuição Lucas Pedroso
 function riscaTexto(evento) {
   evento.target.classList.toggle('completed');
-  //for (let index = 0; index < listaTarefas.children.length; index += 1) {
-    //if ( listaTarefas.children[index].classList === 'completed') {
-      //listaTarefas.children[index].classList.toggle('completed');
-    //}
-    //listaTarefas.children[index].classList.add('completed');
-  //}
+}
+
+function apagaLista() {
+  for (let index = 0; index < listaTarefas.children.length; index += 1) {
+    listaTarefas.innerHTML = '';
+  }
 }
 
 const bt = document.getElementById('criar-tarefa');
 const texto = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
+const btal = document.getElementById('apaga-tudo');
 
 bt.addEventListener('click', addTarefa);
 listaTarefas.addEventListener('click', mudaCor);
 listaTarefas.addEventListener('dblclick', riscaTexto);
+btal.addEventListener('click', apagaLista);
