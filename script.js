aa0d00f694f7601fab120207f5bcdf8c5943c458
biazidioti <bzferreira.bio@gmail.com>
2021-03-24 18:57:30 -0300

    Adiciona cor ao clicar no item da lista.

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ function addTask() {
     const inputTask = document.querySelector('#texto-tarefa');
     const li = document.createElement('li');
     li.innerHTML = inputTask.value;
+
+    li.addEventListener('click', function() {
+        addColor(li)
+    })
+
     document.querySelector('#lista-tarefas').appendChild(li);
     inputTask.value = '';
 }
 
 const task = document.querySelector('#criar-tarefa');
 task.addEventListener('click', addTask);
+
+// adicona cor ao clicar no item da listados
+// colocar cor na li clicada
+function addColor(li) {
+    li.style.backgroundColor= 'rgb(128, 128, 128)';
+}
+
 
