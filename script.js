7763d2aa4ab8564f5e3da8581bdfd7915eadb7cc
diegoCorreia <diegomethal@gmail.com>
2021-03-24 17:56:41 -0300

    correçao lint

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const todoList = document.querySelector('#lista-tarefas');
 const deleteButton = document.querySelector('#apaga-tudo');
 const finishedTask = document.querySelector('#remover-finalizados');
 function taskList() {
-button.addEventListener('click', () => {
-  const insertText = document.getElementById('texto-tarefa').value;
+  button.addEventListener('click', () => {
+    const insertText = document.getElementById('texto-tarefa').value;
     document.getElementById('texto-tarefa').value = '';
     const textList = document.createElement('li');
     textList.innerText = insertText;
-    todoList.appendChild(textList); 
+    todoList.appendChild(textList);
   });
 }
 taskList();
-
-function changeColor() { 
+function changeColor() {
   todoList.addEventListener('click', (e) => {
     const itemList = e.target;
     const liClass = document.getElementsByClassName('item')[0];
@@ -32,27 +31,26 @@ function completeTask() {
 }
 completeTask();
 
-function itemFinished(){
+function itemFinished() {
   const finishedItens = document.querySelectorAll('.completed');
-    for(let index = 0; index < finishedItens.length; index +=1){
-      finishedItens[index].remove();
-    }     
+    for (let index = 0; index < finishedItens.length; index +=1) {
+    finishedItens[index].remove();
+  }
 }
 itemFinished();
 
-function removeFinished(){
+function removeFinished() {
   finishedTask.addEventListener('click', itemFinished);
 }
 removeFinished();
 
-function deleteTask(){
-  deleteButton.addEventListener('click', (e) => {
-  const taskItens = document.querySelector('#lista-tarefas');
-  taskItens.innerHTML = '';
+function deleteTask() {
+  deleteButton.addEventListener('click', () => {
+    const taskItens = document.querySelector('#lista-tarefas');
+    taskItens.innerHTML = '';
   });
 }
 deleteTask();
-
 /* Venho aqui através desse comentário prestar meus agradecimento 
 ao aluno Renzo Sevilha, pela ajuda com os requitos 7 e 8 e refatoração 
 do código!!!! Muito obrigado!!!!!*/
