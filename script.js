6904568b2cd0cf13c56b2e5abfd650cbe6b5a0ed
Henrique Lima <hnamix@gmail.com>
2021-03-24 09:11:27 -0400

    requisito 07 e 08 FINALMENTE!!!!

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const bt = document.getElementById('criar-tarefa');
-const texto = document.getElementById('texto-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
-bt.addEventListener('click', addTarefa);
-
-
-function addTarefa () {
+function addTarefa() {
   const itemlist = document.createElement('li');
+  itemlist.classList.add('item');
   listaTarefas.appendChild(itemlist);
   itemlist.innerText = texto.value;
   texto.value = '';
-  
 }
+function mudaCor(evento) {
+  for (let index = 0; index < listaTarefas.children.length; index += 1) {
+    listaTarefas.children[index].style.backgroundColor = '';
+  }
+  evento.target.style.background = 'rgb(128, 128, 128)';
+}
+
+const bt = document.getElementById('criar-tarefa');
+const texto = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+
+bt.addEventListener('click', addTarefa);
+listaTarefas.addEventListener('click', mudaCor);
