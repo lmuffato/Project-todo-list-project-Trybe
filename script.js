9652953f2dfce74d242ff5614d4d9bbd10418e61
Henrique Lima <hnamix@gmail.com>
2021-03-25 17:33:14 -0400

    Requisito 12

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ function addTarefa() {
 function mudaCor(evento) {
   for (let index = 0; index < listaTarefas.children.length; index += 1) {
     listaTarefas.children[index].style.backgroundColor = '';
+    listaTarefas.children[index].classList.remove('selec');
   }
   evento.target.style.background = 'rgb(128, 128, 128)';
+  evento.target.classList.add('selec');
 }
 
 // Requisito 09 Contribuição Lucas Pedroso
@@ -23,16 +25,33 @@ function apagaLista() {
   }
 }
 
-function removeFinalizados(){
+function removeFinalizados() {
   const removeitem = document.getElementsByClassName('completed');
   let cont = removeitem.length;
-  while(cont > 0){
-  for (let index=0; index<removeitem.length; index+=1){
-    listaTarefas.removeChild(removeitem[index]);
-    cont= cont-1;
-    console.log(cont)
+  while (cont > 0) {
+    for (let index = 0; index < removeitem.length; index += 1) {
+      listaTarefas.removeChild(removeitem[index]);
+      cont -= 1;
+      console.log(cont);
+    }
   }
 }
+
+function moverCima() {
+  const itemselec = document.getElementsByClassName('item selec');
+  const listali = document.getElementsByTagName('li');
+  for (let index = 0; index < listali.length; index += 1) {
+  }
+}
+
+function removeSel() {
+  const itemselec = document.getElementsByClassName('item selec');
+  const listali = document.getElementsByTagName('li');
+  for (let index = 0; index < listali.length; index += 1) {
+    if (itemselec == listali[index]) {
+      listaTarefas.children.remove(itemselec);
+    }
+  }
 }
 
 const bt = document.getElementById('criar-tarefa');
@@ -40,9 +59,26 @@ const texto = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const btal = document.getElementById('apaga-tudo');
 const btrf = document.getElementById('remover-finalizados');
+const btup = document.getElementById('mover-cima');
+//const btdw = document.getElementById('mover-baixo');
+const btsv = document.getElementById('salvar-tarefas');
+const btrms = document.getElementById('remover-selecionado');
 
 bt.addEventListener('click', addTarefa);
 listaTarefas.addEventListener('click', mudaCor);
 listaTarefas.addEventListener('dblclick', riscaTexto);
 btal.addEventListener('click', apagaLista);
 btrf.addEventListener('click', removeFinalizados);
+btup.addEventListener('click', moverCima);
+btsv.addEventListener('click', () => {
+  localStorage.setItem('key', listaTarefas.outerHTML);
+});
+
+const pegarItens = () => {
+  if (localStorage.getItem('key') !== undefined) {
+    const pegarItem = localStorage.getItem('key');
+    listaTarefas.innerHTML = pegarItem;
+  }
+};
+pegarItens();
+btrms.addEventListener('click', removeSel);
