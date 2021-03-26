3fd28914d61b2bdb2312d3f807149566ef865670
biazidioti <bzferreira.bio@gmail.com>
2021-03-26 08:50:55 -0300

    Arruma lint.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Item 5. Adiciona evento click no input texto, cria intem dentro da ol e apaga o texto do input
 
 function addTask() {
-    const inputTask = document.querySelector('#texto-tarefa');
-    const li = document.createElement('li');
-    li.innerHTML = inputTask.value;
+  const inputTask = document.querySelector('#texto-tarefa');
+  const li = document.createElement('li');
+  li.innerHTML = inputTask.value;
 
-    li.addEventListener('click', function() {
-        const liArray = document.querySelectorAll('li');
-        for (let index = 0; index < liArray.length; index += 1) {
-            liArray[index].style.backgroundColor = '';
-        }
-        addColor(li)
-    })
+  li.addEventListener('click', function() {
+      const liArray = document.querySelectorAll('li');
+      for (let index = 0; index < liArray.length; index += 1) {
+          liArray[index].style.backgroundColor = '';
+      }
+      addColor(li);
+  }
 
-    document.querySelector('#lista-tarefas').appendChild(li);
-    inputTask.value = '';
+  document.querySelector('#lista-tarefas').appendChild(li);
+  inputTask.value = '';
 }
 
 const task = document.querySelector('#criar-tarefa');
@@ -23,7 +23,7 @@ task.addEventListener('click', addTask);
 // adicona cor ao clicar no item da listados
 // colocar cor na li clicada
 function addColor(li) {
-    li.style.backgroundColor= 'rgb(128, 128, 128)';
+    li.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 // adiciona função que risca item e desfaz a ação tcomb
