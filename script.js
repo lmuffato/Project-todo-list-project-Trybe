95523a50cd764f8ecb9e50eaaffd34852e5ed9ad
Djaniza Vasques <djanizav@gmail.com>
2021-03-26 20:07:57 -0300

    Adiciona requisitos 7 e 9

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const taskbutton = document.getElementById('criar-tarefa');
 const inputTarefa = document.getElementById('texto-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
+const listaTarefas = document.querySelectorAll('lista-tarefas');
+const listaDeTarefas = document.getElementById('lista-tarefas');
 
 taskbutton.addEventListener('click', function() {
-    let newtask = document.createElement('li');
-    newtask.innerText = inputTarefa.value;
-    listaTarefas.appendChild(newtask);
-    inputTarefa.value = null
-})
+  const newtask = document.createElement('li');
+  newtask.innerText = inputTarefa.value;
+  listaDeTarefas.appendChild(newtask);
+  inputTarefa.value = null;
+});
+
+function paintBackground(event) {
+      for( let index = 0; index < listaTarefas.length; index += 1){
+        listaTarefas[index].classList.remove('task-selection')
+        }
+      event.target.classList.add('task-selection');
+}
+
+function completedTAsk(event) {
+  event.target.classList.add('completed');
+}
+
+
+listaDeTarefas.addEventListener('dblclick', completedTAsk);
+listaDeTarefas.addEventListener('click', paintBackground);
