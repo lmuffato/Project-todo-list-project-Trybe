978d07678474f216023e1e47a9da81dc43f45ffe
AlexSmith7192 <alexsmith7192@gmail.com>
2021-03-29 18:48:35 -0300

    7 - Adiciona cor cinza em tarefa selecionada

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const inputTask = document.getElementById('texto-tarefa');
-const listTasks = document.getElementById('lista-tarefas');
+const olTasks = document.getElementById('lista-tarefas');
 const buttonCreateTasks = document.getElementById('criar-tarefa');
 
 const createTasks = () => {
-  const task = document.createElement('li')
-  listTasks.appendChild(task);
+  const task = document.createElement('li');
+  olTasks.appendChild(task);
   task.innerText = inputTask.value;
   inputTask.value = '';
-};
+}
+
+function deselect() {
+  const selected = document.querySelector('.color-gray');
+  selected.classList.remove('color-gray');
+}
 
 buttonCreateTasks.addEventListener('click', createTasks);
+
+function select() {
+  const list = document.getElementsByTagName('li');
+  for (let indexGray = 0; indexGray < list.length; indexGray += 1) {
+    list[indexGray].addEventListener('click', () => {
+      deselect();
+      list[indexGray].classList.add('color-gray');
+    });
+  }
+}
+
+select();
