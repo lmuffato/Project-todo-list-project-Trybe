0679160938a17db0e5c551e08376a3452d9ec0f8
Rodrigo Grande <rogrande0496@gmail.com>
2021-03-29 16:30:50 -0300

    completa requisitos 9

--- script.js
+++ script.js
@@ -21,5 +21,15 @@ function selectList(e) {
   e.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
+function completedTask(e) {
+  const targetTask = e.target;
+  if (targetTask.className !== 'completed') {
+    targetTask.className = 'completed';
+  } else {
+    targetTask.className = 'no-completed';
+  }
+}
+
 creatTaskButton.addEventListener('click', creatList);
 list.addEventListener('click', selectList);
+list.addEventListener('dblclick', completedTask);
