5b1eaf97dc4a411b6899e1e63dd1a7c3c3f72307
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-03-30 20:49:23 -0300

    Refatoração

--- script.js
+++ script.js
@@ -13,27 +13,18 @@ taskList.addEventListener('click', (newSelected) => {
   if (selected) {
     selected.classList.remove('selected');
     newSelected.target.classList.add('selected');
-  } else {
-    newSelected.target.classList.add('selected');
-  }
+  } else newSelected.target.classList.add('selected');
 });
 
 taskList.addEventListener('dblclick', (completed) => {
   const completedClass = completed.target.classList;
-  if (completedClass.contains('completed')) {
-    completedClass.remove('completed');
-  } else {
-    completedClass.add('completed');
-  }
+  if (completedClass.contains('completed')) completedClass.remove('completed');
+  else completedClass.add('completed');
 });
 
-document.querySelector('#apaga-tudo').addEventListener('click', () => {
-  taskList.innerText = '';
-});
+document.querySelector('#apaga-tudo').addEventListener('click', () => { taskList.innerText = ''; });
 
 document.querySelector('#remover-finalizados').addEventListener('click', () => {
   const completedTasks = document.querySelectorAll('.completed');
-  for (let i = 0; i < completedTasks.length; i += 1) {
-    completedTasks[i].remove();
-  }
+  for (let i = 0; i < completedTasks.length; i += 1) completedTasks[i].remove();
 });
