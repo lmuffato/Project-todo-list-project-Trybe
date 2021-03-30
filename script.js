0ba9a308aca326e45e309a071244fed023808a45
Djaniza Vasques <djanizav@gmail.com>
2021-03-29 21:39:10 -0300

    Corrige requisitos 8 e 9

--- script.js
+++ script.js
@@ -9,25 +9,32 @@ taskbutton.addEventListener('click', function() {
   const newtask = document.createElement('li');
   newtask.innerText = inputTarefa.value;
   listaDeTarefas.appendChild(newtask);
+  newtask.addEventListener('click', paintBackground);
+  newtask.addEventListener('dblclick', completedTAsk);
   inputTarefa.value = null;
 });
 
 function paintBackground(event) {
-      for( let index = 0; index < listaTarefas.length; index += 1){
-        listaTarefas[index].classList.remove('task-selection')
+      for( let index = 0; index < listaDeTarefas.childNodes.length; index += 1){
+        listaDeTarefas.childNodes[index].classList.remove('task-selection')
         }
       event.target.classList.add('task-selection');
 }
 
 function completedTAsk(event) {
-  event.target.classList.add('completed');
+  event.target.classList.toggle('completed');
 }
 
-listaDeTarefas.addEventListener('dblclick', completedTAsk);
-listaDeTarefas.addEventListener('click', paintBackground);
 
 eraseAll.addEventListener('click', function() {
   while (listaDeTarefas.firstChild) {
     listaDeTarefas.removeChild(listaDeTarefas.firstChild);
+  }
+})
+
+eraseCompleted.addEventListener('click', function() {
+  for(let index = 0; index < listaTarefas.length; index += 1){
+    if(listaTarefas[index].className = 'selected'){
+    }
   }
 })
