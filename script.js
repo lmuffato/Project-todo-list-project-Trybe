1bba1e4a0ca120d43874769bcde0f10b6536a6d6
lucas-chave <lcchavesr150@gmail.com>
2021-04-25 17:58:48 -0300

    requisito 13

--- script.js
+++ script.js
@@ -80,18 +80,26 @@ removeSelected();
 
 const moved = () => {
   buttonUp.addEventListener('click', () => {
-
+    const elementList = document.querySelector('.selectLi');
+    if (elementList === null) return console.log('null');
+    const previousElement = elementList.previousElementSibling;
+    if (previousElement) elementOl.insertBefore(elementList, previousElement);
   });
-
   buttonDown.addEventListener('click', () => {
-
+    const elementList = document.querySelector('.selectLi');
+    if (elementList === null) return console.log('null');
+    const nextElement = elementList.nextElementSibling;
+    if (nextElement) elementOl.insertBefore(nextElement, elementList);
   });
 };
 moved();
 
-// referencias: https://cursos.alura.com.br/forum/topico-pegar-valor-do-input-com-javascript-62528
+// Minhas Referências:
+// https://cursos.alura.com.br/forum/topico-pegar-valor-do-input-com-javascript-62528
 // http://devfuria.com.br/javascript/dom-create-element/#:~:text=A%20fun%C3%A7%C3%A3o%20createElement()%20ir%C3%A1,createElement(tagName)%3B
 // https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/
 // https://developer.mozilla.org/en-US/docs/Web/CSS/fit-content
 // https://developer.mozilla.org/pt-BR/docs/Web/API/EventTarget/addEventListener
 // Adelino Junior turma 10 tribo A
+// https://github.com/tryber/sd-010-a-project-todo-list/pull/49/commits
+// https://github.com/tryber/sd-010-a-project-todo-list/pull/49/commits/043303141e4d88fd0228d71771adcdc4a216dd88
