7a04a2b20aa183e08bb527f8e09234c5dfce10bf
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-04-27 13:22:57 -0300

    requisitos bonus

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function add() {
     item.addEventListener('click', () => {
       item.classList.add('selected');
     });
-
     getList.appendChild(item).innerText = getInputText.value;
     getInputText.value = '';
   });
@@ -26,16 +25,24 @@ function clickEvent(event) {
 }
 function clickDel(event) {
   const getCompleted = event.target;
-  if (getCompleted.classList.value.includes('completed') == true) {
-      getCompleted.classList.remove('completed');
+  if (getCompleted.classList.value.includes('completed')) {
+    getCompleted.classList.remove('completed');
   } else {
-      getCompleted.classList.add('completed');
-    }
+    getCompleted.classList.add('completed');
+  }
 }
 
 function swapSelected() {
   const getSelected = document.getElementById('lista-tarefas');
   getSelected.addEventListener('click', clickEvent);
+}
+
+function CleanAll() {
+  const getAllList = document.getElementById('lista-tarefas');
+  while (getAllList.firstChild) {
+    getAllList.firstChild.remove();
+  }
+  localStorage.clear();
 }
 
 function swapCompleted() {
@@ -44,36 +51,74 @@ function swapCompleted() {
 }
 
 function cleanAllListener() {
-  const getCleanButton = document.getElementById('apaga-tudo')  
-  getCleanButton.addEventListener('click', CleanAll)
-}
-
-function cleanCompletedListener() {
-  const getCleanButton = document.getElementById('remover-finalizados')  
-  getCleanButton.addEventListener('click', cleanCompleted)
-}
-
-function CleanAll() {
-  const getAllList = document.getElementById('lista-tarefas');
-  while (getAllList.firstChild) {
-    getAllList.firstChild.remove()
-  }
+  const getCleanButton = document.getElementById('apaga-tudo');
+  getCleanButton.addEventListener('click', CleanAll);
 }
 
 function cleanCompleted() {
-  const getAllList = document.querySelectorAll('li')
+  const getAllList = document.querySelectorAll('li');
   for (let index = 0; index < getAllList.length; index += 1) {
-    if (getAllList[index].classList.value.includes('completed') == true) {
-      getAllList[index].remove()
+    if (getAllList[index].classList.value.includes('completed')) {
+      getAllList[index].remove();
     }
   }
 }
 
-add();
-swapSelected();
-swapCompleted();
-cleanAllListener();
-cleanCompletedListener();
+function cleanCompletedListener() {
+  const getCleanButton = document.getElementById('remover-finalizados');
+  getCleanButton.addEventListener('click', cleanCompleted);
+}
 
-//Referências:
-//https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+function getLocalStorage() {
+  document.querySelector('ol').innerHTML = localStorage.getItem('item');
+}
+
+function saveTasks() {
+  document.getElementById('salvar-tarefas').addEventListener('click', () => {
+    localStorage.setItem('item', document.querySelector('ol').innerHTML);
+  });
+}
+function modifyPriority() {
+  document.getElementById('mover-cima').addEventListener('click', () => {
+    const getAllList = document.querySelectorAll('li');
+    const getParent = document.querySelector('ol');
+    for (let index = 1; index < getAllList.length; index += 1) {
+      if (getAllList[index] === document.getElementsByClassName('selected')[0]) {
+        getParent.insertBefore(document
+          .getElementsByClassName('selected')[0], getAllList[index - 1]);
+      }
+    }
+  });
+  document.getElementById('mover-baixo').addEventListener('click', () => {
+    const getAllList = document.querySelectorAll('li');
+    const getParent = document.querySelector('ol');
+    for (let index = 0; index < getAllList.length; index += 1) {
+      if (getAllList[index] === document.getElementsByClassName('selected')[0]) {
+        getParent.insertBefore(document
+          .getElementsByClassName('selected')[0], getAllList[index + 2]);
+      }
+    }
+  });
+}
+
+function removeSelected() {
+  document.getElementById('remover-selecionado').addEventListener('click', () => {
+    const getSelected = document.querySelector('.selected');
+    getSelected.remove();
+  });
+}
+
+window.onload = function () {
+  add();
+  swapSelected();
+  swapCompleted();
+  cleanAllListener();
+  cleanCompletedListener();
+  getLocalStorage();
+  saveTasks();
+  removeSelected();
+  modifyPriority();
+};
+
+// Referências:
+// https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
