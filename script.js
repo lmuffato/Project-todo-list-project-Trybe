7b8fcb6d737d826e8124c1d8d16125283659bcec
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-28 08:54:59 -0300

    Commit bomba com refatorações

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 const list = document.querySelector('#lista-tarefas');
+const addButton = document.querySelector('#criar-tarefa');
+const deleteButton = document.querySelector('#apaga-tudo');
+const saveButton = document.querySelector('#salvar-tarefas');
+const removeButton = document.querySelector('#remover-finalizados');
+const store = localStorage;
 
 function clearInput() {
   const input = document.querySelector('#texto-tarefa');
   input.value = '';
 }
 
-function deleteAll(event) {
+function deleteAll(e) {
   const tasks = document.querySelectorAll('li');
-  const removeButton = document.querySelector('#remover-finalizados');
-  const deleteButton = event.target;
+  const event = e.target;
 
   for (let index = 0; index < tasks.length; index += 1) {
     list.removeChild(tasks[index]);
   }
-  deleteButton.classList.add('disabled');
-  deleteButton.disabled = true;
+  event.classList.add('disabled');
   removeButton.classList.add('disabled');
+  saveButton.classList.add('disabled');
+  event.disabled = true;
   removeButton.disabled = true;
+  saveButton.disabled = true;
 }
 
-function removeCompleted(event) {
+function removeCompleted(e) {
   const completedList = document.querySelectorAll('.completed');
-  const removeButton = event.target;
+  const event = e.target;
 
   for (let index = 0; index < completedList.length; index += 1) {
     list.removeChild(completedList[index]);
   }
-  removeButton.classList.add('disabled');
+  if (!list.childNodes.length) {
+    deleteButton.classList.add('disabled');
+    saveButton.classList.add('disabled');
+    deleteButton.disabled = true;
+    saveButton.disabled = true;
+  }
+  event.classList.add('disabled');
+  event.disabled = true;
 }
 
-function completedTask(event) {
-  const task = event.target;
-  const removeButton = document.querySelector('#remover-finalizados');
+function completedTask(e) {
+  const event = e.target;
+
+  // https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+  if (event.classList.contains('completed')) {
+    event.classList.remove('completed');
+
+    if (!document.querySelectorAll('.completed').length) {
+      removeButton.classList.add('disabled');
+      removeButton.disabled = true;
+    }
+    return;
+  }
 
   removeButton.classList.remove('disabled');
   removeButton.disabled = false;
-  removeButton.addEventListener('click', removeCompleted);
-
-  // https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
-  if (task.classList.contains('completed')) {
-    return task.classList.remove('completed');
-  }
-  return task.classList.add('completed');
+  return event.classList.add('completed');
 }
 
 function backgroundDeselected() {
@@ -52,61 +69,74 @@ function backgroundDeselected() {
   return selectedElement;
 }
 
-function backgroundSelected(event) {
-  const itemToSelect = event.target;
+function backgroundSelected(e) {
+  const event = e.target;
   const selectedItem = backgroundDeselected();
 
-  if (itemToSelect === selectedItem) {
+  if (event === selectedItem) {
     return;
   }
-  itemToSelect.classList.add('selected');
+  return event.classList.add('selected');
 }
 
-function saveList() {
-  const store = window.localStorage;
+function setList() {
   const tasks = list.childNodes;
+  const property = {};
+
+  if (!tasks.length) {
+    return alert('Sua lista de tarefas está vázia!');
+  }
 
   for (let index = 0; index < tasks.length; index += 1) {
-    store.setItem(index + 1, tasks[index].textContent);
+    const task = tasks[index];
+
+    if (task.classList.contains('selected')) {
+      task.classList.remove('selected');
+    }
+    property.textContent = task.textContent;
+    property.class = task.className;
+    store.setItem(index + 1, JSON.stringify(property));
+  }
+}
+
+// https://www.horadecodar.com.br/2020/07/21/como-salvar-um-objeto-na-localstorage/
+function getList() {
+  for (let key = 1; key <= store.length; key += 1) {
+    const task = document.createElement('li');
+    const storedTask = JSON.parse(store[key]);
+
+    task.className = storedTask.class;
+    task.innerText = storedTask.textContent;
+    list.appendChild(task);
   }
 }
 
 function addTask() {
   const task = document.createElement('li');
   const inputValue = document.querySelector('#texto-tarefa').value;
-  const deleteButton = document.querySelector('#apaga-tudo');
-  const saveButton = document.querySelector('#salvar-tarefas');
 
   task.innerText = inputValue;
   task.addEventListener('click', backgroundSelected);
   task.addEventListener('dblclick', completedTask);
+
   list.appendChild(task);
+
   deleteButton.classList.remove('disabled');
   deleteButton.disabled = false;
-  deleteButton.addEventListener('click', deleteAll);
+
   saveButton.classList.remove('disabled');
   saveButton.disabled = false;
-  saveButton.addEventListener('click', saveList);
   clearInput();
 }
 
-function buttonsEvent() {
-  const button = document.querySelector('#criar-tarefa');
-  button.addEventListener('click', addTask);
-}
-
-function getSavedList() {
-  const store = window.localStorage;
-
-  for (let key = 1; key <= store.length; key += 1) {
-    const task = document.createElement('li');
-
-    task.innerText = store[key];
-    list.appendChild(task);
-  }
+function getEvents() {
+  addButton.addEventListener('click', addTask);
+  deleteButton.addEventListener('click', deleteAll);
+  removeButton.addEventListener('click', removeCompleted);
+  saveButton.addEventListener('click', setList);
 }
 
 window.onload = () => {
-  buttonsEvent();
-  getSavedList();
+  getEvents();
+  getList();
 };
